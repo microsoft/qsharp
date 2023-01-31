669fb07857df900b20bed5e5d2406f89003c5ba0
Stefan J. Wernli <swernli@microsoft.com>
2023-01-31 11:53:56 -0800

    Adding basic Rust CI infrastructure (#1)
    * Placeholder crates to start working on infra
    
    * More example infrastructure
    
    * Add basic CI for Rust
    
    * Fix formatting of yaml
    
    * Add manual dispatch to workflow
    
    * Use checkout v3
    
    * Update and simplify
    
    * Switch to warnings for clippy::pedantic for easier local development

---
+++ compiler/qsc/src/main.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#![warn(clippy::pedantic)]
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ compiler/qsc_ast/src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#![warn(clippy::pedantic)]
+
+#[must_use]
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
