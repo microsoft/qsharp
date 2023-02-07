8217d04bdaa80bb216d1c869d95998d5ccf10fb9
Sarah Marshall <sarah.marshall@microsoft.com>
2023-02-07 10:59:02 -0800

    Add more tokens and rename some

--- compiler/qsc_frontend/src/lex.rs
+++ compiler/qsc_frontend/src/lex.rs
@@ -9,68 +9,106 @@ pub(crate) struct Token {
 }
 
 pub(crate) enum TokenKind {
+    /// `@`
+    At,
+    /// `!`
+    Bang,
+    /// `|`
     Bar,
+    /// A closed binary operator followed by an equals sign.
     BinOpEq(ClosedBinOp),
+    /// A closed binary operator.
     ClosedBinOp(ClosedBinOp),
+    /// A closing delimiter.
     CloseDelim(Delim),
+    /// `:`
     Colon,
+    /// `::`
     ColonColon,
+    /// `,`
+    Comma,
+    /// `$"`
     DollarQuote,
+    /// `..`
     DotDot,
+    /// `...`
     DotDotDot,
+    /// `=`
     Eq,
+    /// `==`
     EqEq,
+    /// `=>`
     FatArrow,
+    /// `>`
     Gt,
+    /// `>=`
     Gte,
+    /// An identifier.
     Ident(String),
+    /// `<-`
     LArrow,
+    /// A literal.
     Lit(Lit),
+    /// `<`
     Lt,
+    /// `<=`
     Lte,
-    Neq,
+    /// `!=`
+    Ne,
+    /// An opening delimiter.
     OpenDelim(Delim),
+    /// `?`
     Question,
+    /// `->`
     RArrow,
+    /// `;`
     Semi,
+    /// `'`
     SingleQuote,
+    /// `~~~`
+    TildeTildeTilde,
+    /// `w/`
     WSlash,
+    /// `w/=`
     WSlashEq,
 }
 
 /// Binary operators whose input type is closed under the operation. These are the only binary
-/// operators that can be used for compound assignment, like `set x += y`.
+/// operators that can be used in compound assignment, like `set x += y`.
 pub(crate) enum ClosedBinOp {
-    /// Addition: `+`.
-    Add,
-    /// Bitwise AND: `&&&`.
-    AndB,
-    /// Logical AND: `and`.
-    AndL,
-    /// Division: `/`.
-    Div,
-    /// Exponentiation: `^`.
-    Exp,
-    /// Modulus: `%`.
-    Mod,
-    /// Multiplication: `*`.
-    Mul,
-    /// Bitwise OR: `|||`.
-    OrB,
-    /// Logical OR: `or`.
-    OrL,
-    /// Shift left: `<<<`.
-    Shl,
-    /// Shift right: `>>>`.
-    Shr,
-    /// Subtraction: `-`.
-    Sub,
-    /// Bitwise XOR: `^^^`.
-    XorB,
+    /// `&&&`
+    AmpAmpAmp,
+    /// `and`
+    And,
+    /// `|||`
+    BarBarBar,
+    /// `^`
+    Caret,
+    /// `^`
+    CaretCaretCaret,
+    /// `>>>`
+    GtGtGt,
+    /// `<<<`
+    LtLtLt,
+    /// `-`
+    Minus,
+    /// `or`
+    Or,
+    /// `%`
+    Percent,
+    /// `+`
+    Plus,
+    /// `/`
+    Slash,
+    /// `*`
+    Star,
 }
 
 pub(crate) enum Delim {
+    /// `{` `}`
     Brace,
+    /// `[` `]`
     Bracket,
+    /// `(` `)`
     Paren,
 }
