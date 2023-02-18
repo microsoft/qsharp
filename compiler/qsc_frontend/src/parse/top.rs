5d18e434a5ad4fd173479a058f215642229aec8c
Sarah Marshall <sarah.marshall@microsoft.com>
2023-02-17 17:22:31 -0800

    Parse internal visibility

--- compiler/qsc_frontend/src/parse/top.rs
+++ compiler/qsc_frontend/src/parse/top.rs
@@ -12,7 +12,7 @@ use super::{
 use crate::lex::{Delim, TokenKind};
 use qsc_ast::ast::{
     Block, CallableBody, CallableDecl, CallableKind, DeclMeta, Item, ItemKind, Namespace, NodeId,
-    Package, Spec, SpecBody, SpecDecl, SpecGen,
+    Package, Spec, SpecBody, SpecDecl, SpecGen, Visibility, VisibilityKind,
 };
 
 pub(super) fn package(s: &mut Scanner) -> Result<Package> {
@@ -41,7 +41,9 @@ fn namespace(s: &mut Scanner) -> Result<Namespace> {
 
 fn item(s: &mut Scanner) -> Result<Item> {
     let lo = s.peek().span.lo;
-    let kind = if keyword(s, Keyword::Open).is_ok() {
+    let kind = if let Some(meta) = opt(s, decl_meta)? {
+        Ok(ItemKind::Callable(meta, callable_decl(s)?))
+    } else if keyword(s, Keyword::Open).is_ok() {
         let name = dot_ident(s)?;
         let alias = if keyword(s, Keyword::As).is_ok() {
             Some(dot_ident(s)?)
@@ -64,6 +66,20 @@ fn item(s: &mut Scanner) -> Result<Item> {
         id: NodeId::PLACEHOLDER,
         span: s.span(lo),
         kind,
+    })
+}
+
+fn decl_meta(s: &mut Scanner) -> Result<DeclMeta> {
+    let lo = s.peek().span.lo;
+    keyword(s, Keyword::Internal)?;
+    let visibility = Visibility {
+        id: NodeId::PLACEHOLDER,
+        span: s.span(lo),
+        kind: VisibilityKind::Internal,
+    };
+    Ok(DeclMeta {
+        attrs: Vec::new(),
+        visibility: Some(visibility),
     })
 }
 
@@ -2225,6 +2241,198 @@ mod tests {
                             lo: 15,
                             hi: 16,
                         },
+                    },
+                )
+            "#]],
+        );
+    }
+
+    #[test]
+    fn internal_function() {
+        check(
+            item,
+            "internal function Foo() : Unit {}",
+            &expect![[r#"
+                Ok(
+                    Item {
+                        id: NodeId(
+                            4294967295,
+                        ),
+                        span: Span {
+                            lo: 0,
+                            hi: 33,
+                        },
+                        kind: Callable(
+                            DeclMeta {
+                                attrs: [],
+                                visibility: Some(
+                                    Visibility {
+                                        id: NodeId(
+                                            4294967295,
+                                        ),
+                                        span: Span {
+                                            lo: 0,
+                                            hi: 8,
+                                        },
+                                        kind: Internal,
+                                    },
+                                ),
+                            },
+                            CallableDecl {
+                                id: NodeId(
+                                    4294967295,
+                                ),
+                                span: Span {
+                                    lo: 9,
+                                    hi: 33,
+                                },
+                                kind: Function,
+                                name: Ident {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 18,
+                                        hi: 21,
+                                    },
+                                    name: "Foo",
+                                },
+                                ty_params: [],
+                                input: Pat {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 21,
+                                        hi: 23,
+                                    },
+                                    kind: Tuple(
+                                        [],
+                                    ),
+                                },
+                                output: Ty {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 26,
+                                        hi: 30,
+                                    },
+                                    kind: Tuple(
+                                        [],
+                                    ),
+                                },
+                                functors: None,
+                                body: Block(
+                                    Block {
+                                        id: NodeId(
+                                            4294967295,
+                                        ),
+                                        span: Span {
+                                            lo: 31,
+                                            hi: 33,
+                                        },
+                                        stmts: [],
+                                    },
+                                ),
+                            },
+                        ),
+                    },
+                )
+            "#]],
+        );
+    }
+
+    #[test]
+    fn internal_operation() {
+        check(
+            item,
+            "internal operation Foo() : Unit {}",
+            &expect![[r#"
+                Ok(
+                    Item {
+                        id: NodeId(
+                            4294967295,
+                        ),
+                        span: Span {
+                            lo: 0,
+                            hi: 34,
+                        },
+                        kind: Callable(
+                            DeclMeta {
+                                attrs: [],
+                                visibility: Some(
+                                    Visibility {
+                                        id: NodeId(
+                                            4294967295,
+                                        ),
+                                        span: Span {
+                                            lo: 0,
+                                            hi: 8,
+                                        },
+                                        kind: Internal,
+                                    },
+                                ),
+                            },
+                            CallableDecl {
+                                id: NodeId(
+                                    4294967295,
+                                ),
+                                span: Span {
+                                    lo: 9,
+                                    hi: 34,
+                                },
+                                kind: Operation,
+                                name: Ident {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 19,
+                                        hi: 22,
+                                    },
+                                    name: "Foo",
+                                },
+                                ty_params: [],
+                                input: Pat {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 22,
+                                        hi: 24,
+                                    },
+                                    kind: Tuple(
+                                        [],
+                                    ),
+                                },
+                                output: Ty {
+                                    id: NodeId(
+                                        4294967295,
+                                    ),
+                                    span: Span {
+                                        lo: 27,
+                                        hi: 31,
+                                    },
+                                    kind: Tuple(
+                                        [],
+                                    ),
+                                },
+                                functors: None,
+                                body: Block(
+                                    Block {
+                                        id: NodeId(
+                                            4294967295,
+                                        ),
+                                        span: Span {
+                                            lo: 32,
+                                            hi: 34,
+                                        },
+                                        stmts: [],
+                                    },
+                                ),
+                            },
+                        ),
                     },
                 )
             "#]],
