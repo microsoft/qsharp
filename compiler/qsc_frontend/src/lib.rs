356e81099007ec7255f769f87a5a0a04164a421a
Sarah Marshall <sarah.marshall@microsoft.com>
2023-02-27 13:12:44 -0800

    Offset spans for each file

--- compiler/qsc_frontend/src/lib.rs
+++ compiler/qsc_frontend/src/lib.rs
@@ -16,11 +16,55 @@ pub mod symbol;
 
 #[derive(Debug)]
 pub struct Context {
-    pub package: Package,
-    pub entry: Option<Expr>,
-    pub symbols: symbol::Table,
-    pub errors: Vec<Error>,
+    package: Package,
+    entry: Option<Expr>,
+    symbols: symbol::Table,
+    errors: Vec<Error>,
+    offsets: Vec<usize>,
 }
+
+impl Context {
+    #[must_use]
+    pub fn package(&self) -> &Package {
+        &self.package
+    }
+
+    #[must_use]
+    pub fn entry(&self) -> Option<&Expr> {
+        self.entry.as_ref()
+    }
+
+    #[must_use]
+    pub fn symbols(&self) -> &symbol::Table {
+        &self.symbols
+    }
+
+    #[must_use]
+    pub fn errors(&self) -> &[Error] {
+        &self.errors
+    }
+
+    #[must_use]
+    pub fn file_span(&self, span: Span) -> (FileId, Span) {
+        let (index, &offset) = self
+            .offsets
+            .iter()
+            .enumerate()
+            .rev()
+            .find(|(_, &offset)| span.lo >= offset)
+            .expect("Span should match at least one offset.");
+
+        (
+            FileId(index),
+            Span {
+                lo: span.lo - offset,
+                hi: span.hi - offset,
+            },
+        )
+    }
+}
+
+pub struct FileId(pub usize);
 
 #[allow(dead_code)] // TODO: Format errors for display.
 #[derive(Debug)]
@@ -53,17 +97,30 @@ enum ErrorKind {
     Symbol(symbol::ErrorKind),
 }
 
+struct Offsetter(usize);
+
+impl MutVisitor for Offsetter {
+    fn visit_span(&mut self, span: &mut Span) {
+        span.lo += self.0;
+        span.hi += self.0;
+    }
+}
+
 pub fn compile(files: &[&str], entry_expr: &str) -> Context {
     let (mut package, mut parse_errors) = (Package::default(), vec![]);
+    let mut offset = 0;
+    let mut offsets = Vec::new();
     for file in files {
         let (mut file_package, mut file_errors) = parse::package(file);
+        Offsetter(offset).visit_package(&mut file_package);
         package.namespaces.append(&mut file_package.namespaces);
         parse_errors.append(&mut file_errors);
+        offsets.push(offset);
+        offset += file.len();
     }
 
     let mut assigner = id::Assigner::new();
     assigner.visit_package(&mut package);
-
     let mut globals = symbol::GlobalTable::new();
     globals.visit_package(&package);
     let mut resolver = globals.into_resolver();
@@ -82,7 +139,6 @@ pub fn compile(files: &[&str], entry_expr: &str) -> Context {
     }
 
     let (symbols, symbol_errors) = resolver.into_table();
-
     let mut errors = Vec::new();
     errors.extend(parse_errors.into_iter().map(Into::into));
     errors.extend(entry_parse_errors.into_iter().map(Into::into));
@@ -93,5 +149,6 @@ pub fn compile(files: &[&str], entry_expr: &str) -> Context {
         entry,
         symbols,
         errors,
+        offsets,
     }
 }
