75da4166d2c250203dd29d578bf27d4f39ca5c31
Ian Davis <ian.f.davis@microsoft.com>
2023-02-28 16:05:40 -0800

    Add comment on why from_str is written this way

--- compiler/qsc_frontend/src/parse/keyword.rs
+++ compiler/qsc_frontend/src/parse/keyword.rs
@@ -138,6 +138,9 @@ impl Keyword {
 impl FromStr for Keyword {
     type Err = ();
 
+    // This is a hot function. Use a match expression so that the Rust compiler
+    // can optimize the string comparisons better, and order the cases by
+    // frequency in Q# so that fewer comparisons are needed on average.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "let" => Ok(Self::Let),
