64ada523e18ec0e3dc5016167389dc01e5783e64
Sarah Marshall <sarah.marshall@microsoft.com>
2023-03-15 12:12:58 -0700

    Run evaluator from main, fix error handling

--- compiler/qsc/src/main.rs
+++ compiler/qsc/src/main.rs
@@ -5,37 +5,48 @@
 
 use clap::Parser;
 use miette::{Diagnostic, NamedSource, Report};
+use qsc_eval::Evaluator;
 use qsc_frontend::{
-    compile::{self, compile, Context, PackageStore},
+    compile::{self, compile, Context, PackageStore, SourceIndex},
     diagnostic::OffsetError,
 };
 use std::{
     fs, io,
     path::{Path, PathBuf},
+    process::ExitCode,
     result::Result,
     string::String,
     sync::Arc,
 };
 
 #[derive(Parser)]
+#[command(arg_required_else_help(true))]
 struct Cli {
-    #[arg(required(true), num_args(1..))]
     sources: Vec<PathBuf>,
     #[arg(short, long, default_value = "")]
     entry: String,
 }
 
-fn main() {
+fn main() -> Result<ExitCode, qsc_eval::Error> {
     let cli = Cli::parse();
     let sources: Vec<_> = cli.sources.iter().map(read_source).collect();
     let mut store = PackageStore::new();
     let std = store.insert(compile::std());
     let unit = compile(&store, [std], &sources, &cli.entry);
 
-    let sources: Vec<_> = sources.into_iter().map(Arc::new).collect();
-    for error in unit.context.errors() {
-        let report = error_report(&cli.sources, &sources, &unit.context, error);
-        eprintln!("{report:?}");
+    if unit.context.errors().is_empty() {
+        let value = Evaluator::new(&unit).run()?;
+        println!("{value}");
+        Ok(ExitCode::SUCCESS)
+    } else {
+        let sources: Vec<_> = sources.into_iter().map(Arc::new).collect();
+        let entry = Arc::new(cli.entry);
+        for error in unit.context.errors() {
+            let report = error_report(&cli.sources, &sources, &entry, &unit.context, error.clone());
+            eprintln!("{report:?}");
+        }
+
+        Ok(ExitCode::FAILURE)
     }
 }
 
@@ -50,19 +61,35 @@ fn read_source(path: impl AsRef<Path>) -> String {
 fn error_report(
     paths: &[PathBuf],
     sources: &[Arc<String>],
+    entry: &Arc<String>,
     context: &Context,
-    error: &compile::Error,
+    error: compile::Error,
 ) -> Report {
     let Some(first_label) = error.labels().and_then(|mut ls| ls.next()) else {
-        return Report::new(error.clone());
+        return Report::new(error);
     };
 
     // Use the offset of the first labeled span to find which source code to include in the report.
     let (index, offset) = context.source(first_label.offset());
-    let name = paths[index.0].to_str().unwrap();
-    let source = NamedSource::new(name, sources[index.0].clone());
+    let name = source_name(paths, index);
+    let source = sources.get(index.0).unwrap_or(entry).clone();
+    let source = NamedSource::new(name, source);
 
     // Adjust all spans in the error to be relative to the start of this source.
     let offset = -isize::try_from(offset).unwrap();
-    Report::new(OffsetError::new(error.clone(), offset)).with_source_code(source)
+    Report::new(OffsetError::new(error, offset)).with_source_code(source)
+}
+
+fn source_name(paths: &[PathBuf], index: SourceIndex) -> &str {
+    if index.0 == paths.len() {
+        "<entry>"
+    } else {
+        paths
+            .get(index.0)
+            .map_or("<unknown>", |p| match p.to_str() {
+                Some("-") => "<stdin>",
+                Some(name) => name,
+                None => "<unknown>",
+            })
+    }
 }
