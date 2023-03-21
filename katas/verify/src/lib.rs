5aa95861a8060fc84e7102248ffcb636e10238ef
César Zaragoza Cortés <cesarzc@microsoft.com>
2023-03-21 16:06:29 -0700

    Use stdlib.

--- katas/verify/src/lib.rs
+++ katas/verify/src/lib.rs
@@ -4,15 +4,16 @@
 #[cfg(test)]
 mod tests;
 
-use qsc_frontend::compile::{compile, PackageStore};
+use qsc_frontend::compile::{self, compile, PackageStore};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 pub fn verify_reference(reference: &str) {
-    let store = PackageStore::new();
-    let unit = compile(&store, [], [reference], "");
+    let mut store = PackageStore::new();
+    let stdlib = store.insert(compile::std());
+    let unit = compile(&store, [stdlib], [reference], "");
     assert!(
         unit.context.errors().is_empty(),
         "Compilation errors: {:?}",
