ba24c7baf6ac5dbc41026ce0a43336e15d339210
Cesar Zaragoza Cortes <cesarzc@ntdev.microsoft.com>
2023-03-22 22:12:34 -0700

    Move logic around to their corresponding place.

--- katas/verify/src/lib.rs
+++ katas/verify/src/lib.rs
@@ -6,15 +6,29 @@ mod tests;
 
 use qsc_frontend::compile::{self, compile, PackageStore};
 
-pub fn verify_kata(verification_source: &str, implementation: &str) {
+pub fn verify_kata(verification_source: &str, kata_implementation: &str) -> bool {
     let mut store = PackageStore::new();
     let stdlib = store.insert(compile::std());
-    // TODO: The expression should probably be "Kata.Verify()"
-    let unit = compile(&store, [stdlib], [verification_source, implementation], "");
-    // TODO (cesarzc): this function should probably return something rather than assert.
-    assert!(
-        unit.context.errors().is_empty(),
-        "Compilation errors: {:?}",
-        unit.context.errors()
-    );
+
+    // Wrap both the verification source and the kata implementation into the same namespace.
+    let namespace_begin = "namespace Kata {\n";
+    let namespace_end = "\n}";
+    let mut wrapped_verification_source = String::from(verification_source);
+    wrapped_verification_source.insert_str(0, namespace_begin);
+    wrapped_verification_source.push_str(namespace_end);
+    let mut wrapped_kata_implementation = String::from(kata_implementation);
+    wrapped_kata_implementation.insert_str(0, namespace_begin);
+    wrapped_kata_implementation.push_str(namespace_end);
+
+    // Validate that the code successfully compiles.
+    // N.B. Once evaluation works for katas, the expression to compile should be "Kata.Verify()".
+    let unit = compile(&store, [stdlib], [wrapped_verification_source, wrapped_kata_implementation], "");
+    if !unit.context.errors().is_empty() {
+        println!("Compilation errors: {:?}", unit.context.errors());
+        return false;
+    }
+
+    // N.B. Once evaluation works for katas, run the Verify operation.
+
+    return true;
 }
--- katas/verify/src/tests.rs
+++ katas/verify/src/tests.rs
@@ -17,27 +17,16 @@ fn katas_qsharp_source_dir() -> PathBuf {
     katas_qsharp_source_dir.to_path_buf()
 }
 
-fn validate_exercise() {
-    let mut exercise_dir = katas_qsharp_source_dir();
-    exercise_dir.push("single_qubit_gates");
-    exercise_dir.push("task_01");
-
+fn validate_exercise(exercise_dir: PathBuf) {
     let mut reference_file = exercise_dir.clone();
     reference_file.push("reference.qs");
-    let mut reference = read_to_string(reference_file).expect("Unable to read file");
-    // TODO (cesarzc): this should probably be done in the verify_kata function.
-    reference.insert_str(0, "namespace Kata {\n");
-    reference.push_str("\n}");
-    println!("{}", reference);
+    let reference = read_to_string(reference_file).expect("Unable to read reference file.");
 
     let mut verification_source_file = exercise_dir.clone();
     verification_source_file.push("verify.qs");
-    let mut verification_source = read_to_string(verification_source_file).expect("Unable to read file");
-    // TODO (cesarzc): this should probably be done in the verify_kata function.
-    verification_source.insert_str(0, "namespace Kata {\n");
-    verification_source.push_str("\n}");
-    println!("{}", verification_source);
-    verify_kata(verification_source.as_str(), reference.as_str());
+    let verification_source = read_to_string(verification_source_file).expect("Unable to read verification file.");
+    let is_exercise_valid = verify_kata(verification_source.as_str(), reference.as_str());
+    assert!(is_exercise_valid, "Exercise is invalid.");
 }
 
 // TODO (cesarzc): implement.
@@ -46,5 +35,8 @@ fn validate_exercise() {
 
 #[test]
 fn verify_single_qubit_gates_kata() {
-    validate_exercise();
+    let mut exercise_dir = katas_qsharp_source_dir();
+    exercise_dir.push("single_qubit_gates");
+    exercise_dir.push("task_01");
+    validate_exercise(exercise_dir);
 }
