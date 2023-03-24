7fa1e4d3d2844fbb1912df481be6f16311fd622f
Stefan J. Wernli <swernli@microsoft.com>
2023-03-24 16:06:18 -0700

    Switch from `c_double` to equivalent `f64`

--- compiler/qsc_eval/src/val.rs
+++ compiler/qsc_eval/src/val.rs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 use std::{
-    ffi::{c_double, c_void},
+    ffi::c_void,
     fmt::{self, Display, Formatter},
     iter,
 };
@@ -187,12 +187,12 @@ impl TryFrom<Value> for *mut c_void {
     }
 }
 
-impl TryFrom<Value> for c_double {
+impl TryFrom<Value> for f64 {
     type Error = ConversionError;
 
     fn try_from(value: Value) -> Result<Self, Self::Error> {
         if let Value::Double(v) = value {
-            Ok(v as c_double)
+            Ok(v)
         } else {
             Err(ConversionError {
                 expected: "Double",
