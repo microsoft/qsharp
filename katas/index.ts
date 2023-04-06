4526f486616eae74a98636127d728d5e33c5f1a9
Cesar Zaragoza Cortes <cesarzc@ntdev.microsoft.com>
2023-04-06 12:48:22 -0700

    Add katas index file.

---
+++ katas/index.ts
@@ -0,0 +1,37 @@
+export type KatasMetadata = {
+    modules: Array<{
+        id: string;
+        title: string;
+        description: string;
+        tasks: Array<{
+            id: string;
+            title: string;
+            description: string;
+            sourcePath : string;
+        }>;
+    }>;
+};
+
+export const katasMetadata: KatasMetadata = {
+    modules: [
+        {
+            id: "single-qubit-gates",
+            title: "Single-Qubit Gates",
+            description: "",
+            tasks: [
+                {
+                    id: "single-qubit-gates_y-gate",
+                    title: "The Y Gate",
+                    description: "",
+                    sourcePath: "./qs/single_qubit_gates/y_gate"
+                },
+                {
+                    id: "single-qubit-gates_global-phase-i",
+                    title: "Global Phase i",
+                    description: "",
+                    sourcePath: "./qs/single_qubit_gates/global_phase_i"
+                }
+            ]
+        }
+    ]
+}
