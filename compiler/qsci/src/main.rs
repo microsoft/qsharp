82df4659f3b6358e5c50892abaff688e533c70ff
Ian Davis <Ian.F.Davis@microsoft.com>
2023-04-11 12:50:45 -0700

    Create reusable stateful and stateless interpreters (#140)
    Fixes #138
    
    ---------
    
    Co-authored-by: Stefan J. Wernli <swernli@microsoft.com>

--- compiler/qsci/src/main.rs
+++ compiler/qsci/src/main.rs
@@ -1,28 +1,24 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#![warn(clippy::mod_module_files, clippy::pedantic)]
+#![warn(clippy::mod_module_files, clippy::pedantic, clippy::unwrap_used)]
 
-use std::collections::HashMap;
+use std::sync::Arc;
 use std::{path::PathBuf, process::ExitCode};
 
 use clap::Parser;
-use qsc_ast::ast::CallableDecl;
-use qsc_ast::ast::Stmt;
+use qsc_eval::output::CursorReceiver;
+use qsc_eval::stateful::{Error, Interpreter};
+use qsc_eval::val::Value;
+use qsc_eval::AggregateError;
+use qsc_frontend::compile::{Context, SourceIndex};
+use qsc_frontend::diagnostic::OffsetError;
 
 use std::string::String;
 
-use miette::{IntoDiagnostic, Result};
-use qsc_eval::eval_stmt;
-use qsc_eval::Env;
-use qsc_passes::globals::GlobalId;
-
-use qsc_eval::output::GenericReceiver;
-use qsc_frontend::compile::{self, compile, PackageId, PackageStore};
-use qsc_frontend::incremental::{Compiler, Fragment};
-use qsc_passes::globals::extract_callables;
+use miette::{Diagnostic, IntoDiagnostic, NamedSource, Report, Result};
 use std::io::prelude::BufRead;
-use std::io::Write;
+use std::io::{Cursor, Write};
 use std::{fs, io};
 
 #[derive(Debug, Parser)]
@@ -50,44 +46,22 @@ fn main() -> Result<ExitCode> {
 
 fn repl(cli: Cli) -> Result<ExitCode> {
     let sources: Vec<_> = read_source(cli.sources.as_slice()).into_diagnostic()?;
-    let mut store = PackageStore::new();
 
-    let deps = if cli.nostdlib {
-        vec![]
-    } else {
-        vec![store.insert(compile::std())]
-    };
-
-    let user = store.insert(compile(&store, [], sources, ""));
-    let mut compiler = Compiler::new(&store, deps);
-    let mut globals = extract_callables(&store);
-    let mut env = Env::empty();
-
-    let mut stdout = io::stdout();
-    let mut out = GenericReceiver::new(&mut stdout);
-
-    let mut execute_line = |line: String, env: &mut Env| match compiler.compile_fragment(&line) {
-        Ok(fragment) => match fragment {
-            Fragment::Stmt(stmt) => eval(stmt, &store, &globals, &compiler, user, env, &mut out),
-            Fragment::Callable(decl) => {
-                globals.insert(
-                    GlobalId {
-                        package: user,
-                        node: decl.name.id,
-                    },
-                    decl,
-                );
-            }
-        },
-        Err(errors) => {
-            for error in errors {
-                eprintln!("{error}");
-            }
+    let interpreter = Interpreter::new(!cli.nostdlib, sources.clone());
+    if let Err((_, unit)) = interpreter {
+        let reporter = ErrorReporter::new(cli, sources, &unit.context);
+        for error in unit.context.errors() {
+            eprintln!("{:?}", reporter.report(error.clone()));
         }
-    };
-    if cli.entry.is_some() {
-        let line = cli.entry.unwrap_or_default();
-        execute_line(line, &mut env);
+        return Ok(ExitCode::FAILURE);
+    }
+    let mut interpreter = interpreter.expect("interpreter creation failed");
+    let mut cursor = Cursor::new(Vec::<u8>::new());
+    let mut receiver = CursorReceiver::new(&mut cursor);
+
+    if let Some(line) = cli.entry {
+        let results = interpreter.line(&mut receiver, line.clone());
+        print_results(results, &receiver.dump(), &line);
     }
 
     if cli.exec {
@@ -103,46 +77,37 @@ fn repl(cli: Cli) -> Result<ExitCode> {
             while !line.is_empty() && &line[line.len() - 1..] == "\\" {
                 print_prompt(true);
                 line.pop(); // remove '\' from line
-                let next = iter.next().unwrap();
+                let next = iter.next().expect("Could nod read next line");
                 line.push_str(&next);
             }
 
-            // TODO: if multiple statements are entered, we currently only
-            // evaluate the first one. We need to evaluate all of them. This
-            // will require updates to parsing to read multiple statements
-            // followed by the EOF token.
-            execute_line(line, &mut env);
+            if !line.trim().is_empty() {
+                let results = interpreter.line(&mut receiver, line.clone());
+                print_results(results, &receiver.dump(), &line);
+            }
 
             print_prompt(false);
         }
     }
 }
 
-fn eval(
-    stmt: &Stmt,
-    store: &PackageStore,
-    globals: &HashMap<GlobalId, &CallableDecl>,
-    compiler: &Compiler,
-    package: PackageId,
-    env: &mut Env,
-    out: &mut GenericReceiver,
+fn print_results(
+    results: impl Iterator<Item = Result<Value, AggregateError<Error>>>,
+    output: &str,
+    line: &str,
 ) {
-    let result = eval_stmt(
-        stmt,
-        store,
-        globals,
-        compiler.resolutions(),
-        package,
-        env,
-        out,
-    );
-
-    match result {
-        Ok(value) => {
-            println!("{value}");
+    for result in results {
+        if !output.is_empty() {
+            println!("{output}");
         }
-        Err(errors) => {
-            eprintln!("{errors}");
+        match result {
+            Ok(value) => println!("{value:?}"),
+            Err(errors) => {
+                let reporter = InteractiveErrorReporter::new(line);
+                for error in errors.0 {
+                    eprintln!("{:?}", reporter.report(error.clone()));
+                }
+            }
         }
     }
 }
@@ -153,9 +118,69 @@ fn print_prompt(is_multiline: bool) {
     } else {
         print!("qsci$ ");
     }
-    io::stdout().flush().unwrap();
+    io::stdout().flush().expect("Could not flush stdout");
 }
 
 fn read_source(paths: &[PathBuf]) -> io::Result<Vec<String>> {
     paths.iter().map(fs::read_to_string).collect()
+}
+
+struct InteractiveErrorReporter {
+    line: String,
+}
+
+impl InteractiveErrorReporter {
+    fn new(line: impl AsRef<str>) -> Self {
+        Self {
+            line: line.as_ref().to_owned(),
+        }
+    }
+
+    fn report(&self, error: impl Diagnostic + Send + Sync + 'static) -> Report {
+        Report::new(error).with_source_code(self.line.clone())
+    }
+}
+
+struct ErrorReporter<'a> {
+    context: &'a Context,
+    paths: Vec<PathBuf>,
+    sources: Vec<Arc<String>>,
+    entry: Arc<String>,
+}
+
+impl<'a> ErrorReporter<'a> {
+    fn new(cli: Cli, sources: Vec<String>, context: &'a Context) -> Self {
+        Self {
+            context,
+            paths: cli.sources,
+            sources: sources.into_iter().map(Arc::new).collect(),
+            entry: Arc::new(cli.entry.unwrap_or_default()),
+        }
+    }
+
+    fn report(&self, error: impl Diagnostic + Send + Sync + 'static) -> Report {
+        let Some(first_label) = error.labels().and_then(|mut ls| ls.next()) else {
+            return Report::new(error);
+        };
+
+        // Use the offset of the first labeled span to find which source code to include in the report.
+        let (index, offset) = self.context.source(first_label.offset());
+        let name = source_name(&self.paths, index);
+        let source = self.sources.get(index.0).unwrap_or(&self.entry).clone();
+        let source = NamedSource::new(name, source);
+
+        // Adjust all spans in the error to be relative to the start of this source.
+        let offset = -isize::try_from(offset).expect("Could not convert offset to isize");
+        Report::new(OffsetError::new(error, offset)).with_source_code(source)
+    }
+}
+
+fn source_name(paths: &[PathBuf], index: SourceIndex) -> &str {
+    paths
+        .get(index.0)
+        .map_or("<unknown>", |p| match p.to_str() {
+            Some("-") => "<stdin>",
+            Some(name) => name,
+            None => "<unknown>",
+        })
 }
