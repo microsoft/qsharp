7160a51be608e250e66b379f2e3be58dd4fcb798
Ian Davis <ian.f.davis@microsoft.com>
2023-04-11 13:13:55 -0700

    Updating to new eval infrastructure

--- pip/qsharp/src/python.rs
+++ pip/qsharp/src/python.rs
@@ -1,22 +1,26 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use pyo3::prelude::*;
+use std::io::Cursor;
 
-use crate::eval::ExecutionContext;
+use pyo3::{exceptions::PyException, prelude::*};
+use qsc_eval::{output::CursorReceiver, stateful::Interpreter};
 
 #[pyclass(unsendable)]
 pub(crate) struct Evaluator {
-    pub(crate) context: ExecutionContext,
+    pub(crate) interpreter: Interpreter,
 }
 
 #[pymethods]
 impl Evaluator {
     #[new]
     /// Initializes a new Q# evaluator.
-    pub(crate) fn new(_py: Python) -> Self {
-        Self {
-            context: crate::eval::create_context(),
+    pub(crate) fn new(_py: Python) -> PyResult<Self> {
+        const SOURCES: [&str; 0] = [];
+        let result = Interpreter::new(true, SOURCES);
+        match result {
+            Ok(interpreter) => Ok(Self { interpreter }),
+            Err((err, _)) => Err(PyException::new_err(format!("{:?}", err))),
         }
     }
 
@@ -27,9 +31,23 @@ impl Evaluator {
     /// .1 is the output from the simulation.
     /// .2 is the error output.
     #[pyo3(text_signature = "(expr)")]
-    fn eval(&mut self, py: Python, expr: String) -> PyResult<(PyObject, PyObject, PyObject)> {
-        let (value, out, err) = self.context.eval(expr);
-        Ok((value.to_object(py), out.to_object(py), err.to_object(py)))
+    fn eval(&mut self, py: Python, expr: String) -> PyResult<(PyObject, PyObject)> {
+        let mut cursor = Cursor::new(Vec::<u8>::new());
+        let mut receiver = CursorReceiver::new(&mut cursor);
+        let results = self
+            .interpreter
+            .line(&mut receiver, expr)
+            .collect::<Vec<_>>();
+
+        // TODO: Handle multiple results.
+        let result = &results[0];
+        match result {
+            Ok(value) => Ok((
+                value.to_string().to_object(py),
+                receiver.dump().to_object(py),
+            )),
+            Err(err) => Err(PyException::new_err(format!("{:?}", err))),
+        }
     }
 }
 
