36f1ac8d0183262b0b8b81683d7449c01a9b88f5
Cesar Zaragoza Cortes <cesarzc@ntdev.microsoft.com>
2023-04-17 19:39:25 -0700

    Added JSDoc annotations.

--- katas/content/katas.js
+++ katas/content/katas.js
@@ -1,3 +1,28 @@
+//@ts-check
+
+/**
+ * Katas Organization
+ * 
+ * A Kata is a top-level container of educational items (exercises and examples) which are used to explain a particular
+ * topic.
+ * 
+ * This file contains the information needed to build the content for all the Katas, and its main purpose is to convey
+ * ordering. Each Kata and each item within a Kata  is listed in the order it is meant to be presented to students.
+ * 
+ * Each Kata is organized in a directory where a content.md file is present, and multiple sub-directories. Each
+ * sub-directory represents an item within the Kata and its specific content depends on the type of item it represents.
+ */
+
+/**
+ * @type {Array<
+ * {
+ *      directory: string,
+ *      items: Array<{
+ *          type: string,
+ *          directory: string
+ *      }>
+ * }>}
+ */
 exports.katas = [
     {
         directory: "single_qubit_gates",
