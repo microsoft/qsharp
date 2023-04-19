6e73b24eb7652398a781ebe745dcdeb689b1e771
Mine Starks <mineyalc@microsoft.com>
2023-04-19 13:07:11 -0700

    Update state visualization for web

--- npm/src/state-table.ts
+++ npm/src/state-table.ts
@@ -28,25 +28,49 @@ export const exampleDump: Dump = {
 //     dumpTable.innerHTML = renderDump(theDumpObject);
 //
 export function renderDump(dump: Dump): string {
-    let table = `
-  <thead>
-    <tr>
-      <th>Basis state</th>
-      <th>Amplitude</th>
-      <th>Probability</th>
-    </tr>
-  </thead>
-  <tbody>`;
-    function probability(real: number, imag: number) {
-        return (real * real + imag * imag);
-    }
-    Object.keys(dump).forEach(basis => {
-        let [real, imag] = dump[basis];
-        let prob = probability(real, imag);
-        table += `<tr>
-            <td>${basis}</td><td>${real.toFixed(4)} + ${imag.toFixed(4)}i</td><td>${prob.toFixed(4)}</td>
-          </tr>`;
-    });
-    table += `</tbody>`;
-    return table;
+  let table = `
+    <thead>
+      <tr>
+        <th>Basis State<br/>(|&#x1D713;&#x2099;...&#x1D713;&#x2080;⟩)</th>
+        <th>Amplitude</th>
+        <th>Measurement Probability</th>
+        <th colspan="2">Phase</th>
+      </tr>
+    </thead>
+    <tbody>`;
+    
+  function probability(real: number, imag: number) {
+    return (real * real + imag * imag);
+  }
+
+  function formatComplex(real: number, imag: number) {
+    // toLocaleString() correctly identifies -0 in JavaScript
+    // String interpolation drops minus sign from -0
+    // &#x2212; is the unicode minus sign, &#x1D456; is the mathematical i
+    const realPart = `${real.toLocaleString()[0] === "-" ? "&#x2212;" : ""}${Math.abs(real).toFixed(4)}`;
+    const imagPart = `${imag.toLocaleString()[0] === "-" ? "&#x2212;" : "+"}${Math.abs(imag).toFixed(4)}&#x1D456;`;
+    return `${realPart}${imagPart}`;
+  }
+
+  Object.keys(dump).forEach(basis => {
+    const [real, imag] = dump[basis];
+    const complex = formatComplex(real, imag)
+    const probabilityPercent = probability(real, imag) * 100;
+    const phase = Math.atan2(imag, real);
+    table +=
+      `<tr>
+          <td><span>${basis}</span></td>
+          <td><span">${complex}</span></td>
+          <td>
+              <progress max="100" value="${probabilityPercent}"></progress>
+              <span>${probabilityPercent.toFixed(4)}%</span>
+          </td>
+          <td style="transform: rotate(${phase.toFixed(4)}rad)">&uarr;</td>
+          <td>
+              <span">${phase.toFixed(4)}</span>
+          </td>
+    </tr>`;
+  });
+  table += `</tbody>`;
+  return table;
 }
--- pip/qsharp/samples/sample.py
+++ pip/qsharp/samples/sample.py
@@ -4,76 +4,76 @@ from qsharp import (CompilationException, RuntimeException)
 
 sample_qs = pathlib.Path(__file__).parent.resolve().joinpath("./sample.qs")
 
-# Interpret some code.
-# Just defining an operation.
-# TODO: Q: namespace suport?
-qsharp.interpret("""
-operation Main() : Unit {
-    use q = Qubit();
-    let r = M(q);
-    Message("Result: " + AsString(r));
-}
-""")
+# # Interpret some code.
+# # Just defining an operation.
+# # TODO: Q: namespace suport?
+# qsharp.interpret("""
+# operation Main() : Unit {
+#     use q = Qubit();
+#     let r = M(q);
+#     Message("Result: " + AsString(r));
+# }
+# """)
 
-# Interpret some more code.
-# Should output Message output to stdout.
-# TODO: Do something fancy for notebooks
-qsharp.interpret("Main()")
+# # Interpret some more code.
+# # Should output Message output to stdout.
+# # TODO: Do something fancy for notebooks
+# qsharp.interpret("Main()")
 
-# Add some source from a file
-qsharp.interpret_file(sample_qs)
+# # Add some source from a file
+# qsharp.interpret_file(sample_qs)
 
-# Call an operation from the file
-qsharp.interpret("AllBasisVectorsWithPhases_TwoQubits()")
+# # Call an operation from the file
+# qsharp.interpret("AllBasisVectorsWithPhases_TwoQubits()")
 
-# Get result
-result = qsharp.interpret("1 + 2")
+# # Get result
+# result = qsharp.interpret("1 + 2")
 
-print(f"Result was: {result} (type: {type(result).__name__})")
+# print(f"Result was: {result} (type: {type(result).__name__})")
 
-# Return a Result type
-print(qsharp.interpret("One"))
+# # Return a Result type
+# print(qsharp.interpret("One"))
 
-# Return a bool type
-print(qsharp.interpret("true"))
+# # Return a bool type
+# print(qsharp.interpret("true"))
 
-# Return a Pauli type
-print(qsharp.interpret("PauliX"))
+# # Return a Pauli type
+# print(qsharp.interpret("PauliX"))
 
-# Compile time error - parse error
-try:
-    qsharp.interpret("operation Foo() {}")
-except CompilationException as ex:
-    for diagnostic in ex.diagnostics:
-        print("\x1b[33m" + diagnostic.message + "\x1b[0m")
+# # Compile time error - parse error
+# try:
+#     qsharp.interpret("operation Foo() {}")
+# except CompilationException as ex:
+#     for diagnostic in ex.diagnostics:
+#         print("\x1b[33m" + diagnostic.message + "\x1b[0m")
 
-# Compile time errors - multiple errors
-try:
-    qsharp.interpret("operation Foo() : Unit { Bar(); Baz(); }")
-except CompilationException as ex:
-    for diagnostic in ex.diagnostics:
-        print("\x1b[33m" + diagnostic.message + "\x1b[0m")
+# # Compile time errors - multiple errors
+# try:
+#     qsharp.interpret("operation Foo() : Unit { Bar(); Baz(); }")
+# except CompilationException as ex:
+#     for diagnostic in ex.diagnostics:
+#         print("\x1b[33m" + diagnostic.message + "\x1b[0m")
 
-# Runtime error
-try:
-    qsharp.interpret("let (x, y, z) = (0, 1);")
-except RuntimeException as ex:
-    for diagnostic in ex.diagnostics:
-        print("\x1b[31m" + diagnostic.message + "\x1b[0m")
+# # Runtime error
+# try:
+#     qsharp.interpret("let (x, y, z) = (0, 1);")
+# except RuntimeException as ex:
+#     for diagnostic in ex.diagnostics:
+#         print("\x1b[31m" + diagnostic.message + "\x1b[0m")
 
-# State visualization
-(value, outputs) = qsharp.interpret_with_dumps(
-    "AllBasisVectorsWithPhases_TwoQubits()")
-print(f"States: {outputs}")
+# # State visualization
+# (value, outputs) = qsharp.interpret_with_dumps(
+#     "AllBasisVectorsWithPhases_TwoQubits()")
+# print(f"States: {outputs}")
 
-# Multiple statements
-print(qsharp.interpret("3; 4; 5"))
+# # Multiple statements
+# print(qsharp.interpret("3; 4; 5"))
 
-# No statements
-print(qsharp.interpret(""))
+# # No statements
+# print(qsharp.interpret(""))
 
-# Return a tuple
-print(qsharp.interpret("(1,2,\"hi\")"))
+# # Return a tuple
+# print(qsharp.interpret("(1,2,\"hi\")"))
 
 # Check bit order in state output
 qsharp.interpret("""
