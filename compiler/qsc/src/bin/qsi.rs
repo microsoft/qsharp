07e14b4f3ad1978c34aebfe1a0c9c7a53729b254
Sarah Marshall <sarah.marshall@microsoft.com>
2023-04-28 14:20:57 -0700

    Clean up qsi

--- compiler/qsc/src/bin/qsi.rs
+++ compiler/qsc/src/bin/qsi.rs
@@ -4,16 +4,15 @@
 #![warn(clippy::mod_module_files, clippy::pedantic, clippy::unwrap_used)]
 
 use clap::Parser;
-use miette::{Diagnostic, IntoDiagnostic, Report, Result};
+use miette::{Context, IntoDiagnostic, Report, Result};
 use num_bigint::BigUint;
 use num_complex::Complex64;
 use qsc::stateful::{Interpreter, LineError};
 use qsc_eval::{
-    output::{format_state_id, Receiver},
+    output::{self, Receiver},
     val::Value,
 };
-use qsc_frontend::compile::SourceMap;
-use std::sync::Arc;
+use qsc_frontend::compile::{SourceContents, SourceMap, SourceName};
 use std::{
     fs,
     io::{self, prelude::BufRead, Write},
@@ -21,117 +20,26 @@ use std::{
     process::ExitCode,
     string::String,
 };
+use std::{path::Path, sync::Arc};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, next_line_help = true)]
 struct Cli {
-    /// Use the given file on startup as initial session input
+    /// Use the given file on startup as initial session input.
     #[arg(long = "use")]
     sources: Vec<PathBuf>,
-    /// Execute the given Q# expression on startup
+
+    /// Execute the given Q# expression on startup.
     #[arg(long)]
     entry: Option<String>,
-    /// Disable automatic inclusion of the standard library
+
+    /// Disable automatic inclusion of the standard library.
     #[arg(long)]
     nostdlib: bool,
-    /// Exit after loading the files or running the given file(s)/entry on the command line
+
+    /// Exit after loading the files or running the given file(s)/entry on the command line.
     #[arg(long)]
     exec: bool,
-}
-
-fn main() -> Result<ExitCode> {
-    let cli = Cli::parse();
-
-    repl(cli)
-}
-
-fn repl(cli: Cli) -> Result<ExitCode> {
-    let sources: Vec<_> = read_source(cli.sources.as_slice()).into_diagnostic()?;
-
-    let interpreter = Interpreter::new(!cli.nostdlib, SourceMap::new(sources, None));
-    if let Err(errors) = interpreter {
-        for error in errors {
-            eprintln!("{:?}", Report::new(error));
-        }
-        return Ok(ExitCode::FAILURE);
-    }
-    let mut interpreter = interpreter.expect("interpreter creation failed");
-    let mut receiver = TerminalReceiver {};
-
-    if let Some(line) = cli.entry {
-        let results = interpreter.interpret_line(&mut receiver, &line);
-        print_results(results, &line);
-    }
-
-    if cli.exec {
-        return Ok(ExitCode::SUCCESS);
-    }
-
-    loop {
-        print_prompt(false);
-
-        let stdin = io::BufReader::new(io::stdin());
-        let mut iter = stdin.lines().map(Result::unwrap);
-        while let Some(mut line) = iter.next() {
-            while !line.is_empty() && &line[line.len() - 1..] == "\\" {
-                print_prompt(true);
-                line.pop(); // remove '\' from line
-                let next = iter.next().expect("Could nod read next line");
-                line.push_str(&next);
-            }
-
-            if !line.trim().is_empty() {
-                let results = interpreter.interpret_line(&mut receiver, &line);
-                print_results(results, &line);
-            }
-
-            print_prompt(false);
-        }
-    }
-}
-
-fn print_results(result: Result<Value, Vec<LineError>>, line: &str) {
-    match result {
-        Ok(value) => println!("{value}"),
-        Err(errors) => {
-            let reporter = InteractiveErrorReporter::new(line);
-            for error in errors {
-                eprintln!("{:?}", reporter.report(error.clone()));
-            }
-        }
-    }
-}
-
-fn print_prompt(is_multiline: bool) {
-    if is_multiline {
-        print!("    > ");
-    } else {
-        print!("qsi$ ");
-    }
-    io::stdout().flush().expect("Could not flush stdout");
-}
-
-fn read_source(paths: &[PathBuf]) -> io::Result<Vec<(Arc<str>, Arc<str>)>> {
-    paths
-        .iter()
-        .map(|p| Ok((p.to_string_lossy().into(), fs::read_to_string(p)?.into())))
-        .collect()
-}
-
-struct InteractiveErrorReporter {
-    line: String,
-}
-
-impl InteractiveErrorReporter {
-    fn new(line: impl AsRef<str>) -> Self {
-        Self {
-            line: line.as_ref().to_owned(),
-        }
-    }
-
-    fn report(&self, error: impl Diagnostic + Send + Sync + 'static) -> Report {
-        Report::new(error).with_source_code(self.line.clone())
-    }
 }
 
 struct TerminalReceiver;
@@ -141,22 +49,108 @@ impl Receiver for TerminalReceiver {
         &mut self,
         states: Vec<(BigUint, Complex64)>,
         qubit_count: usize,
-    ) -> Result<(), qsc_eval::output::Error> {
+    ) -> Result<(), output::Error> {
         println!("DumpMachine:");
-        for state in states {
-            println!(
-                "{}: [{}, {}]",
-                format_state_id(&state.0, qubit_count),
-                state.1.re,
-                state.1.im
-            );
+        for (qubit, amplitude) in states {
+            let id = output::format_state_id(&qubit, qubit_count);
+            println!("{id}: [{}, {}]", amplitude.re, amplitude.im);
         }
 
         Ok(())
     }
 
-    fn message(&mut self, msg: &str) -> Result<(), qsc_eval::output::Error> {
+    fn message(&mut self, msg: &str) -> Result<(), output::Error> {
         println!("{msg}");
         Ok(())
+    }
+}
+
+fn main() -> miette::Result<ExitCode> {
+    let cli = Cli::parse();
+    let sources = cli
+        .sources
+        .iter()
+        .map(read_source)
+        .collect::<miette::Result<Vec<_>>>()?;
+
+    let mut interpreter = match Interpreter::new(!cli.nostdlib, SourceMap::new(sources, None)) {
+        Ok(interpreter) => interpreter,
+        Err(errors) => {
+            for error in errors {
+                eprintln!("{:?}", Report::new(error));
+            }
+            return Ok(ExitCode::FAILURE);
+        }
+    };
+
+    if let Some(entry) = cli.entry {
+        print_result(
+            &entry,
+            interpreter.interpret_line(&mut TerminalReceiver, &entry),
+        );
+    }
+
+    if cli.exec {
+        Ok(ExitCode::SUCCESS)
+    } else {
+        loop {
+            repl(&mut interpreter, &mut TerminalReceiver).into_diagnostic()?;
+        }
+    }
+}
+
+fn repl(interpreter: &mut Interpreter, receiver: &mut dyn Receiver) -> io::Result<()> {
+    print_prompt(false);
+    let stdin = io::BufReader::new(io::stdin());
+    let mut lines = stdin.lines();
+
+    while let Some(line) = lines.next() {
+        let mut line = line?;
+        while line.ends_with('\\') {
+            print_prompt(true);
+            let continuation = lines.next().expect("should have continuation line")?;
+            line.pop(); // Remove backslash.
+            line.push_str(&continuation);
+        }
+
+        if !line.trim().is_empty() {
+            print_result(&line, interpreter.interpret_line(receiver, &line));
+        }
+
+        print_prompt(false);
+    }
+
+    Ok(())
+}
+
+fn read_source(path: impl AsRef<Path>) -> miette::Result<(SourceName, SourceContents)> {
+    let path = path.as_ref();
+    let contents = fs::read_to_string(path)
+        .into_diagnostic()
+        .with_context(|| format!("could not read source file `{}`", path.display()))?;
+
+    Ok((path.to_string_lossy().into(), contents.into()))
+}
+
+fn print_prompt(continuation: bool) {
+    if continuation {
+        print!("    > ");
+    } else {
+        print!("qsi$ ");
+    }
+
+    io::stdout().flush().expect("standard out should flush");
+}
+
+fn print_result(line: &str, result: Result<Value, Vec<LineError>>) {
+    match result {
+        Ok(value) => println!("{value}"),
+        Err(errors) => {
+            let source: Arc<str> = line.into();
+            for error in errors {
+                let report = Report::new(error).with_source_code(Arc::clone(&source));
+                eprintln!("{report:?}");
+            }
+        }
     }
 }
