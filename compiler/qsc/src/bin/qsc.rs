dac4a64f6c32bf3dfa3894bef721cc9039216e95
Sarah Marshall <sarah.marshall@microsoft.com>
2023-04-28 13:37:16 -0700

    Clean up qsc

--- compiler/qsc/src/bin/qsc.rs
+++ compiler/qsc/src/bin/qsc.rs
@@ -3,8 +3,8 @@
 
 #![warn(clippy::mod_module_files, clippy::pedantic, clippy::unwrap_used)]
 
-use clap::{error::ErrorKind, Parser, ValueEnum};
-use miette::{Context, IntoDiagnostic, Result};
+use clap::{ArgGroup, Parser, ValueEnum};
+use miette::{Context, IntoDiagnostic, Report};
 use qsc::compile::compile;
 use qsc_frontend::compile::{PackageStore, SourceContents, SourceMap, SourceName};
 use qsc_hir::hir::Package;
@@ -16,103 +16,82 @@ use std::{
     string::String,
 };
 
+#[derive(Debug, Parser)]
+#[command(version, arg_required_else_help(true))]
+#[clap(group(ArgGroup::new("input").args(["entry", "sources"]).required(true).multiple(true)))]
+struct Cli {
+    /// Disable automatic inclusion of the standard library.
+    #[arg(long)]
+    nostdlib: bool,
+
+    /// Emit the compilation unit in the specified format.
+    #[arg(long, value_enum)]
+    emit: Vec<Emit>,
+
+    /// Write output to compiler-chosen filename in <dir>.
+    #[arg(long = "outdir", value_name = "DIR")]
+    out_dir: Option<PathBuf>,
+
+    /// Enable verbose output.
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Entry expression to execute as the main operation.
+    #[arg(short, long)]
+    entry: Option<String>,
+
+    /// Q# source files to compile, or `-` to read from stdin.
+    #[arg()]
+    sources: Vec<PathBuf>,
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Emit {
     Hir,
 }
 
-#[derive(Debug, Parser)]
-#[command(version, arg_required_else_help(true))]
-struct Cli {
-    /// Disable automatic inclusion of the standard library
-    #[arg(long)]
-    nostdlib: bool,
-    /// Emit the compilation unit in the specified format
-    #[arg(long, value_enum)]
-    emit: Vec<Emit>,
-    /// Write output to compiler-chosen filename in <dir>
-    #[arg(long = "outdir", value_name = "DIR")]
-    out_dir: Option<PathBuf>,
-    /// Enable verbose output
-    #[arg(short, long)]
-    verbose: bool,
-    /// Entry expression to execute as the main operation.
-    #[arg(short, long)]
-    entry: Option<String>,
-    /// Q# source files to compile, or `-` to read from stdin
-    #[arg()]
-    input: Vec<PathBuf>,
-}
-
-fn main() -> Result<ExitCode> {
+fn main() -> miette::Result<ExitCode> {
     let cli = Cli::parse();
-    validate_input(&cli.input, &cli.entry.clone().unwrap_or_default()).into_diagnostic()?;
+    // validate_input(&cli.input, &cli.entry.clone().unwrap_or_default()).into_diagnostic()?;
+
+    let mut store = PackageStore::new();
+    let mut dependencies = Vec::new();
+    if !cli.nostdlib {
+        dependencies.push(store.insert(qsc::compile::std()));
+    }
+
     let sources = cli
-        .input
+        .sources
         .iter()
         .map(read_source)
         .collect::<Result<Vec<_>, _>>()
         .into_diagnostic()?;
+
     let entry = cli.entry.unwrap_or_default();
+    let sources = SourceMap::new(sources, Some(entry.into()));
+    let (unit, errors) = compile(&store, dependencies, sources);
 
-    let mut store = PackageStore::new();
-    let dependencies = if cli.nostdlib {
-        vec![]
-    } else {
-        vec![store.insert(qsc::compile::std())]
-    };
-
-    let (unit, reports) = compile(
-        &store,
-        dependencies,
-        SourceMap::new(sources, Some(entry.into())),
-    );
-
-    for (_, emit) in cli.emit.iter().enumerate() {
+    let out_dir = cli.out_dir.as_ref().map_or(".".as_ref(), PathBuf::as_path);
+    for emit in &cli.emit {
         match emit {
-            Emit::Hir => {
-                let out_dir = match &cli.out_dir {
-                    Some(value) => value.clone(),
-                    None => PathBuf::from("."),
-                };
-
-                emit_hir(&unit.package, &out_dir)
-                    .into_diagnostic()
-                    .context("could not emit HIR")?;
-            }
+            Emit::Hir => emit_hir(&unit.package, out_dir)
+                .into_diagnostic()
+                .context("could not emit HIR")?,
         }
     }
 
-    if reports.is_empty() {
+    if errors.is_empty() {
         Ok(ExitCode::SUCCESS)
     } else {
-        for report in reports {
-            eprintln!("{report:?}");
+        for error in errors {
+            if let Some(source) = unit.sources.find_diagnostic(&error) {
+                eprintln!("{:?}", Report::new(error).with_source_code(source.clone()));
+            } else {
+                eprintln!("{:?}", Report::new(error));
+            }
         }
-        Ok(ExitCode::FAILURE)
-    }
-}
 
-fn validate_input(sources: &[PathBuf], entry: &str) -> Result<(), clap::Error> {
-    if sources.is_empty() {
-        if entry.is_empty() {
-            let msg = "No input specified";
-            let err = clap::Error::raw(ErrorKind::ValueValidation, msg);
-            Err(err)
-        } else {
-            Ok(())
-        }
-    } else if sources.len() == 1 {
-        Ok(())
-    } else if sources
-        .iter()
-        .any(|path| path.display().to_string() == *"-")
-    {
-        let msg = "Specifying stdin `-` is not allowed with file inputs";
-        let err = clap::Error::raw(ErrorKind::ValueValidation, msg);
-        Err(err)
-    } else {
-        Ok(())
+        Ok(ExitCode::FAILURE)
     }
 }
 
