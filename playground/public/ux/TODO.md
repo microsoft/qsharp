becaee29155ebbf8c3bd87d87c2de13e4eb3e4a0
Bill Ticehurst <billti@microsoft.com>
2023-04-28 23:56:59 -0700

    Dynamic error list

--- playground/public/ux/TODO.md
+++ playground/public/ux/TODO.md
@@ -5,10 +5,13 @@
 - Draw histogram
 - Editor expand when results overflow the page.
   - Should be able to set output to 100% and use absolute with scrolling.
+- Honor Start and Shots, and disable Run while busy
 - Enable loading of samples
 - Enable navigation through shot results
+- Fix up squiggles to be proper error markers
 - Enable filtering shot results via histogram
 - Enable katas
+- Show a progress bar as shots run
 
 
 Use <section> <header> <nav> <main> <menu> elements <https://developer.mozilla.org/en-US/docs/Web/HTML/Element>
--- playground/src/editor.tsx
+++ playground/src/editor.tsx
@@ -2,11 +2,12 @@
 // Licensed under the MIT License.
 
 import { useEffect, useRef, useState } from "preact/hooks";
-import { ICompilerWorker, QscEventTarget } from "qsharp";
+import { ICompilerWorker, QscEventTarget, VSDiagnostic } from "qsharp";
 
 export function Editor(props: {code: string, compiler: ICompilerWorker, evtTarget: QscEventTarget}) {
     const editorRef = useRef(null);
     const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+    const [errors, setErrors] = useState<{location: string, msg: string}[]>([]);
 
     useEffect(() => {
         // Create the monaco editor
@@ -18,6 +19,54 @@ export function Editor(props: {code: string, compiler: ICompilerWorker, evtTarge
     
         // If the browser window resizes, tell the editor to update it's layout
         window.addEventListener('resize', _ => editor.layout());
+
+        // As code is edited check it for errors and update the error list
+        async function check() {
+            // TODO: As this is async, code may be being edited while earlier check calls are still running.
+            // Need to ensure that if this occurs, wait and try again on the next animation frame.
+            // i.e. Don't queue a bunch of checks if some are still outstanding
+            diagnosticsFrame = 0;
+            let code = srcModel.getValue();
+            let errs = await props.compiler.checkCode(code);
+
+            // Note that as this is async, the code may have changed since checkCode was called.
+            // TODO: Account for this scenario (e.g. delta positions with old source version)
+            squiggleDiagnostics(errs);
+            // TODO: Disable run button on errors: errs.length ?
+            //    runButton.setAttribute("disabled", "true") : runButton.removeAttribute("disabled");
+
+        }
+
+        // Helpers to turn errors into editor squiggles
+        let currentsquiggles: string[] = [];
+        function squiggleDiagnostics(errors: VSDiagnostic[]) {
+            let errList: {location: string, msg: string}[] = [];
+            let newDecorations = errors?.map(err => {
+                let startPos = srcModel.getPositionAt(err.start_pos);
+                let endPos = srcModel.getPositionAt(err.end_pos);
+                let range = monaco.Range.fromPositions(startPos, endPos);
+                let decoration: monaco.editor.IModelDeltaDecoration = {
+                    range,
+                    options: { className: 'err-span', hoverMessage: { value: err.message } }
+                }
+                errList.push({
+                    location: `main.qs@(${startPos.lineNumber},${startPos.column})`,
+                    msg: err.message // TODO: Handle line breaks and 'help' notes
+                });
+                return decoration;
+            });
+            currentsquiggles = srcModel.deltaDecorations(currentsquiggles, newDecorations || []);
+            setErrors(errList);
+        }
+
+        // While the code is changing, update the diagnostics as fast as the browser will render frames
+        let diagnosticsFrame = requestAnimationFrame(check);
+
+        srcModel.onDidChangeContent(ev => {
+            if (!diagnosticsFrame) {
+                diagnosticsFrame = requestAnimationFrame(check);
+            }
+        });
 
         return () => editor.dispose();
     }, []);
@@ -43,9 +92,12 @@ export function Editor(props: {code: string, compiler: ICompilerWorker, evtTarge
     <input id="shot" type="number" value="100" max="1000" min="1" />
     <button id="run" class='main-button' onClick={onRun}>Run</button>
   </div>
-  <div class="error-list">
-    <div class="error-row"><span>main.qs@(10,12)</span>: Syntax error. Expected identifier.</div>
-    <div class="error-row"><span>main.qs@(15,14)</span>: Identifier 'foo' is unknown</div>
-  </div>
+{ errors.length ? 
+  (<div class="error-list">
+    {errors.map(err => (
+        <div class="error-row"><span>{err.location}</span>: {err.msg}</div>
+    ))}
+  </div>) : null
+}
 </div>);
 }
