87bc23e795a41f7a49dc4d43c35ce2d59b7841ae
Mine Starks <mineyalc@microsoft.com>
2023-05-11 11:31:07 -0700

    remove more unneeded stuff from jupyterlab

--- jupyterlab/src/__tests__/qsharp_jupyterlab.spec.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
-
-describe('qsharp_jupyterlab', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
-  });
-});
