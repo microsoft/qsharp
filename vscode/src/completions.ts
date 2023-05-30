022bade3bfb9eee81255d3ec83060e4f7792900f
Mine Starks <mineyalc@microsoft.com>
2023-05-30 16:22:59 -0700

    Completions in VS Code

---
+++ vscode/src/completions.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { type getCompiler } from "qsharp";
+import * as vscode from "vscode";
+import { CompletionItem } from "vscode";
+
+type ICompiler = Awaited<ReturnType<typeof getCompiler>>;
+
+export function createCompletionItemProvider(
+  compiler: ICompiler,
+  output: vscode.OutputChannel
+) {
+  return new QSharpCompletionItemProvider(compiler, output);
+}
+
+class QSharpCompletionItemProvider implements vscode.CompletionItemProvider {
+  constructor(
+    public compiler: ICompiler,
+    public output: vscode.OutputChannel
+  ) {}
+
+  async provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken,
+    context: vscode.CompletionContext
+  ) {
+    this.output.appendLine("getting completions");
+    let compiler;
+    try {
+      compiler = await this.compiler;
+    } catch (e: unknown) {
+      this.output.appendLine("failed to get compiler");
+      this.output.appendLine(JSON.stringify(e));
+    }
+    if (compiler) {
+      try {
+        const completions = await compiler.getCompletions();
+        this.output.appendLine(`got ${completions.items.length} completions`);
+        return completions.items.map((c) => new CompletionItem(c.label));
+      } catch (e: unknown) {
+        this.output.appendLine("failed to get completions");
+        this.output.appendLine(JSON.stringify(e));
+      }
+    }
+    return [new CompletionItem("DUMMY")];
+  }
+}
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,55 +1,28 @@
 import * as vscode from "vscode";
+import { getCompiler, loadWasmModule } from "qsharp";
+import { createCompletionItemProvider } from "./completions.js";
+import { registerQSharpNotebookHandlers } from "./notebooks.js";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  const output = vscode.window.createOutputChannel("Q#");
+  output.appendLine("Q# extension activated.");
+
+  // load the compiler
+  const wasmUri = vscode.Uri.joinPath(
+    context.extensionUri,
+    "./wasm/qsc_wasm_bg.wasm"
+  );
+  const wasmBytes = await vscode.workspace.fs.readFile(wasmUri);
+  await loadWasmModule(wasmBytes);
+  const compiler = await getCompiler();
+
+  // completions
+  vscode.languages.registerCompletionItemProvider(
+    "qsharp",
+    createCompletionItemProvider(compiler, output),
+    "."
+  );
+
+  // notebooks
   registerQSharpNotebookHandlers(context);
-}
-
-/**
- * Sets up handlers to detect Q# code cells in Jupyter notebooks and set the language to Q#.
- */
-function registerQSharpNotebookHandlers(context: vscode.ExtensionContext) {
-  const qsharpLanguageId = "qsharp";
-  const qsharpCellMagic = "%%qsharp";
-  const jupyterNotebookType = "jupyter-notebook";
-
-  context.subscriptions.push(
-    vscode.workspace.onDidOpenNotebookDocument((notebookDocument) => {
-      if (notebookDocument.notebookType === jupyterNotebookType) {
-        updateQSharpCellLanguages(notebookDocument.getCells());
-      }
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.workspace.onDidChangeNotebookDocument((event) => {
-      if (event.notebook.notebookType === jupyterNotebookType) {
-        // change.document will be undefined if the cell contents did not change -- filter those out.
-        const changedCells = event.cellChanges
-          .filter((change) => change.document)
-          .map((change) => change.cell);
-        const addedCells = event.contentChanges
-          .map((change) => change.addedCells)
-          .flat();
-        updateQSharpCellLanguages(changedCells.concat(addedCells));
-      }
-    })
-  );
-
-  function updateQSharpCellLanguages(cells: vscode.NotebookCell[]) {
-    for (const cell of cells) {
-      // If this is a code cell that starts with %%qsharp, and language isn't already set to Q#, set it.
-      if (cell.kind === vscode.NotebookCellKind.Code) {
-        const document = cell.document;
-        if (
-          document.languageId !== qsharpLanguageId &&
-          document.lineAt(0).text.startsWith(qsharpCellMagic)
-        ) {
-          vscode.languages.setTextDocumentLanguage(
-            cell.document,
-            qsharpLanguageId
-          );
-        }
-      }
-    }
-  }
 }
