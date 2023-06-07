9929b733e6f07e5e78f17f75cbaa9fa68fdda0e4
Mine Starks <mineyalc@microsoft.com>
2023-06-07 07:10:55 -0700

    Language service crate

---
+++ language_service/src/completion.rs
@@ -0,0 +1,174 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#[cfg(test)]
+mod tests;
+
+use crate::qsc_utils::{span_contains, Compilation};
+use qsc::hir::ItemKind;
+use qsc_hir::hir::Package;
+use qsc_hir::{
+    hir::{Block, Item},
+    visit::Visitor,
+};
+use std::collections::HashSet;
+
+// It would have been nice to match these enum values to the ones used by
+// VS Code and Monaco, but unfortunately those two disagree on the values.
+// So we define our own unique enum here to reduce confusion.
+#[derive(Clone, Debug, PartialEq)]
+#[allow(clippy::module_name_repetitions)]
+pub enum CompletionItemKind {
+    Function,
+    Module,
+    Keyword,
+    Issue,
+}
+
+#[derive(Debug)]
+#[allow(clippy::module_name_repetitions)]
+pub struct CompletionList {
+    pub items: Vec<CompletionItem>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+#[allow(clippy::module_name_repetitions)]
+pub struct CompletionItem {
+    pub label: String,
+    pub kind: CompletionItemKind,
+}
+
+pub(crate) fn get_completions(
+    compilation: &Compilation,
+    source_name: &str,
+    offset: u32,
+) -> CompletionList {
+    let compile_unit = &compilation.compile_unit;
+    // Map the file offset into a SourceMap offset
+    let offset = compile_unit.sources.map_offset(source_name, offset);
+    let package = &compile_unit.package;
+    let std_package = &compilation
+        .package_store
+        .get(compilation.std_package_id)
+        .expect("expected to find std package")
+        .package;
+
+    // Collect namespaces
+    let mut namespace_collector = NamespaceCollector {
+        namespaces: HashSet::new(),
+    };
+    namespace_collector.visit_package(package);
+    namespace_collector.visit_package(std_package);
+
+    let mut res = CompletionList { items: Vec::new() };
+
+    // Callables from the current document
+    let mut current_callables = callable_names_from_package(package);
+
+    // All callables from std package
+    let mut std_callables = callable_names_from_package(std_package);
+
+    // All namespaces
+    let mut namespaces = namespace_collector
+        .namespaces
+        .drain()
+        .map(|ns| CompletionItem {
+            label: ns,
+            kind: CompletionItemKind::Module,
+        })
+        .collect::<Vec<_>>();
+
+    // Determine context for the offset
+    let mut context_builder = ContextFinder {
+        offset,
+        context: if compile_unit.package.items.values().next().is_none() {
+            Context::NoCompilation
+        } else {
+            Context::TopLevel
+        },
+    };
+    context_builder.visit_package(package);
+    let context = context_builder.context;
+
+    match context {
+        Context::Namespace => {
+            res.items.push(CompletionItem {
+                label: "open".to_string(),
+                kind: CompletionItemKind::Keyword,
+            });
+            res.items.append(&mut namespaces);
+        }
+        Context::Block | Context::NoCompilation => {
+            // Add everything we know of.
+            res.items.append(&mut std_callables);
+            res.items.append(&mut current_callables);
+            res.items.append(&mut namespaces);
+        }
+        Context::TopLevel | Context::NotSignificant => res.items.push(CompletionItem {
+            label: "namespace".to_string(),
+            kind: CompletionItemKind::Keyword,
+        }),
+    }
+    res
+}
+
+struct NamespaceCollector {
+    namespaces: HashSet<String>,
+}
+
+impl Visitor<'_> for NamespaceCollector {
+    fn visit_item(&mut self, item: &Item) {
+        if let ItemKind::Namespace(ident, _) = &item.kind {
+            // Collect namespaces
+            self.namespaces.insert(ident.name.to_string());
+        }
+        qsc_hir::visit::walk_item(self, item);
+    }
+}
+
+struct ContextFinder {
+    offset: u32,
+    context: Context,
+}
+
+#[derive(Debug, PartialEq)]
+enum Context {
+    NoCompilation,
+    TopLevel,
+    Namespace,
+    Block,
+    NotSignificant,
+}
+
+impl Visitor<'_> for ContextFinder {
+    fn visit_item(&mut self, item: &Item) {
+        if span_contains(item.span, self.offset) {
+            self.context = match &item.kind {
+                ItemKind::Namespace(..) => Context::Namespace,
+                _ => Context::NotSignificant,
+            }
+        }
+
+        qsc_hir::visit::walk_item(self, item);
+    }
+
+    fn visit_block(&mut self, block: &Block) {
+        if span_contains(block.span, self.offset) {
+            self.context = Context::Block;
+        }
+    }
+}
+
+fn callable_names_from_package(package: &Package) -> Vec<CompletionItem> {
+    package
+        .items
+        .values()
+        .filter_map(|i| match &i.kind {
+            ItemKind::Callable(callable_decl) => Some(CompletionItem {
+                label: callable_decl.name.name.to_string(),
+                kind: CompletionItemKind::Function,
+            }),
+            _ => None,
+        })
+        .collect::<Vec<_>>()
+}
---
+++ language_service/src/definition.rs
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#[cfg(test)]
+mod tests;
+
+use crate::qsc_utils::{span_contains, Compilation};
+use qsc::SourceMap;
+use qsc_hir::hir::{ExprKind, ItemKind, Package, Res};
+use qsc_hir::visit::Visitor;
+
+#[derive(Debug, PartialEq)]
+pub struct Definition {
+    pub source: String,
+    pub offset: u32,
+}
+
+pub(crate) fn get_definition(
+    compilation: &Compilation,
+    source_name: &str,
+    offset: u32,
+) -> Option<Definition> {
+    let compile_unit = &compilation.compile_unit;
+    // Map the file offset into a SourceMap offset
+    let offset = compile_unit.sources.map_offset(source_name, offset);
+    let package = &compile_unit.package;
+
+    let mut definition_finder = DefinitionFinder {
+        package,
+        source_map: &compile_unit.sources,
+        offset,
+        definition: None,
+    };
+    definition_finder.visit_package(package);
+
+    definition_finder
+        .definition
+        .map(|(name, offset)| Definition {
+            source: name,
+            offset,
+        })
+}
+
+struct DefinitionFinder<'a> {
+    package: &'a Package,
+    source_map: &'a SourceMap,
+    offset: u32,
+    definition: Option<(String, u32)>,
+}
+
+impl<'a> Visitor<'_> for DefinitionFinder<'a> {
+    fn visit_expr(&mut self, expr: &qsc_hir::hir::Expr) {
+        if span_contains(expr.span, self.offset) {
+            if let ExprKind::Var(res) = expr.kind {
+                let item = match res {
+                    Res::Err => None,
+                    // Just one package plus std for now, so let's live with this hack
+                    Res::Item(item) => {
+                        if item.package.is_none() {
+                            self.package.items.get(item.item)
+                        } else {
+                            // Handling std library is tricky for now
+                            None
+                        }
+                    }
+                    Res::Local(_node) => None,
+                };
+                if let Some(def) = item {
+                    if let ItemKind::Callable(decl) = &def.kind {
+                        self.definition = Some((
+                            self.source_map
+                                .find_offset(decl.name.span.lo)
+                                .name
+                                .to_string(),
+                            decl.name.span.lo,
+                        ));
+                    }
+                }
+            }
+        }
+        qsc_hir::visit::walk_expr(self, expr);
+    }
+}
