82f84c8a851266247c14286057689de0a962843a
Stefan J. Wernli <swernli@microsoft.com>
2023-06-12 15:19:26 -0700

    Use generic large input benchmark (#385)

--- compiler/qsc/benches/large.rs
+++ compiler/qsc/benches/large.rs
@@ -5,19 +5,19 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use qsc::compile::{self, compile};
 use qsc_frontend::compile::{PackageStore, SourceMap};
 
-const INPUT: &str = include_str!("./nqueens.qs");
+const INPUT: &str = include_str!("./large.qs");
 
-pub fn nqueens(c: &mut Criterion) {
-    c.bench_function("NQueens large input file", |b| {
+pub fn large_file(c: &mut Criterion) {
+    c.bench_function("Large input file", |b| {
         b.iter(|| {
             let mut store = PackageStore::new(compile::core());
             let std = store.insert(compile::std(&store));
-            let sources = SourceMap::new([("nqueens.qs".into(), INPUT.into())], None);
+            let sources = SourceMap::new([("large.qs".into(), INPUT.into())], None);
             let (_, reports) = compile(&store, &[std], sources);
             assert!(reports.is_empty());
         })
     });
 }
 
-criterion_group!(benches, nqueens);
+criterion_group!(benches, large_file);
 criterion_main!(benches);
