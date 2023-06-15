2580c3bbc4b997194d006977202ea8e04fa5cee8
César Zaragoza Cortés <cesarzc@microsoft.com>
2023-06-15 11:57:21 -0700

    Initial setup.

--- npm/generate_katas_content.js
+++ npm/generate_katas_content.js
@@ -11,6 +11,8 @@ import { inspect } from "node:util";
 import { marked } from "marked";
 
 import { katas } from "../katas/content/katas.js";
+import { it } from "node:test";
+import { Console } from "node:console";
 
 const thisDir = dirname(fileURLToPath(import.meta.url));
 const katasContentDir = join(thisDir, "..", "katas", "content");
@@ -100,6 +102,12 @@ function buildItemContent(item, kataDir) {
 
 function buildKataContent(kata, katasDir) {
   const kataDir = join(katasDir, kata.directory);
+  const itemsJson = readFileSync(join(kataDir, "items.json"), "utf8");
+  const items = JSON.parse(itemsJson);
+  for (const item of items) {
+    console.log(item);
+  }
+
   let itemsContent = [];
   for (const item of kata.items) {
     const moduleContent = buildItemContent(item, kataDir);
