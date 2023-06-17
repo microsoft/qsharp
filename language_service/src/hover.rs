844df69c6e7fc8457ea89849c880cf7572fe4a86
Scott Carda <Scott.Carda@microsoft.com>
2023-06-16 17:36:20 -0700

    Added some logic to Hover

--- language_service/src/hover.rs
+++ language_service/src/hover.rs
@@ -5,7 +5,12 @@
 mod tests;
 
 use crate::qsc_utils::{map_offset, span_contains, Compilation};
-use qsc::hir::{ty::Ty, visit::Visitor, CallableDecl, CallableKind};
+use qsc::hir::{
+    ty::{FunctorSetValue, Ty},
+    visit::{walk_callable_decl, walk_expr, walk_item, walk_pat, Visitor},
+    CallableDecl, CallableKind, Expr, ExprKind, Ident, Item, LocalItemId, NodeId, Pat, PatKind,
+    Res,
+};
 
 #[derive(Debug, PartialEq)]
 pub struct Hover {
@@ -28,64 +33,164 @@ pub(crate) fn get_hover(
     let offset = map_offset(&compilation.source_map, source_name, offset);
     let package = &compilation.package;
 
-    let mut callable_finder = CallableFinder {
+    let mut hover_visitor = HoverVisitor {
+        compilation,
         offset,
         header: None,
         start: 0,
         end: 0,
     };
-    callable_finder.visit_package(package);
+    hover_visitor.visit_package(package);
 
-    callable_finder.header.map(|header| Hover {
+    hover_visitor.header.map(|header| Hover {
         contents: header,
         span: Span {
-            start: callable_finder.start,
-            end: callable_finder.end,
+            start: hover_visitor.start,
+            end: hover_visitor.end,
         },
     })
 }
 
-struct CallableFinder {
+struct HoverFromItemId {
+    item_id: LocalItemId,
+    header: Option<String>,
+}
+
+impl Visitor<'_> for HoverFromItemId {
+    fn visit_item(&mut self, item: &'_ Item) {
+        if item.id == self.item_id {
+            walk_item(self, item);
+        }
+    }
+
+    fn visit_callable_decl(&mut self, decl: &'_ CallableDecl) {
+        self.header = Some(header_from_call_decl(decl));
+    }
+}
+
+struct HoverFromNodeId {
+    node_id: NodeId,
+    header: Option<String>,
+}
+
+impl Visitor<'_> for HoverFromNodeId {
+    fn visit_pat(&mut self, pat: &'_ Pat) {
+        match &pat.kind {
+            PatKind::Bind(ident) => {
+                if ident.id == self.node_id {
+                    self.header = Some(header_from_ident(ident, &pat.ty));
+                }
+            }
+            _ => walk_pat(self, pat),
+        }
+    }
+}
+
+struct HoverVisitor<'a> {
+    compilation: &'a Compilation,
     offset: u32,
     header: Option<String>,
     start: u32,
     end: u32,
 }
 
-impl Visitor<'_> for CallableFinder {
-    fn visit_callable_decl(&mut self, decl: &CallableDecl) {
+impl Visitor<'_> for HoverVisitor<'_> {
+    fn visit_callable_decl(&mut self, decl: &'_ CallableDecl) {
         if span_contains(decl.name.span, self.offset) {
-            let kind = match decl.kind {
-                CallableKind::Function => "function",
-                CallableKind::Operation => "operation",
-            };
-
-            // Doc comments would be formatted as markdown into this
-            // string once we're able to parse them out.
-            let header = format!(
-                "```qsharp
-{} {}{} : {}
-```
-",
-                kind,
-                decl.name.name,
-                match &decl.input.ty {
-                    Ty::Tuple(items) => {
-                        // I'm just doing this so I can format Unit as ()
-                        if items.is_empty() {
-                            "()".to_string()
-                        } else {
-                            format!("{}", &decl.input.ty)
-                        }
-                    }
-                    x => x.to_string(),
-                },
-                decl.output
-            );
-
-            self.header = Some(header);
+            self.header = Some(header_from_call_decl(decl));
             self.start = decl.name.span.lo;
             self.end = decl.name.span.hi;
+        } else if span_contains(decl.span, self.offset) {
+            walk_callable_decl(self, decl);
         }
     }
+
+    fn visit_pat(&mut self, pat: &'_ Pat) {
+        if span_contains(pat.span, self.offset) {
+            match &pat.kind {
+                PatKind::Bind(ident) => {
+                    self.header = Some(header_from_ident(ident, &pat.ty));
+                    self.start = pat.span.lo;
+                    self.end = pat.span.hi;
+                }
+                _ => walk_pat(self, pat),
+            }
+        }
+    }
+
+    fn visit_expr(&mut self, expr: &'_ Expr) {
+        if span_contains(expr.span, self.offset) {
+            if let ExprKind::Var(r, _) = &expr.kind {
+                self.header = match r {
+                    Res::Err => None,
+                    Res::Item(item_id) => {
+                        let mut finder_pass = HoverFromItemId {
+                            item_id: item_id.item,
+                            header: None,
+                        };
+                        if let Some(package_id) = item_id.package {
+                            let foreign_package = &self
+                                .compilation
+                                .package_store
+                                .get(package_id)
+                                .unwrap_or_else(|| panic!("bad package id: {package_id}"))
+                                .package;
+                            finder_pass.visit_package(foreign_package);
+                            finder_pass.header
+                        } else {
+                            finder_pass.visit_package(&self.compilation.package);
+                            finder_pass.header
+                        }
+                    }
+                    Res::Local(node_id) => {
+                        let mut finder_pass = HoverFromNodeId {
+                            node_id: *node_id,
+                            header: None,
+                        };
+                        finder_pass.visit_package(&self.compilation.package);
+                        finder_pass.header
+                    }
+                };
+                self.start = expr.span.lo;
+                self.end = expr.span.hi;
+            } else {
+                walk_expr(self, expr);
+            }
+        }
+    }
+}
+
+fn header_from_call_decl(decl: &CallableDecl) -> String {
+    let (kind, arrow) = match decl.kind {
+        CallableKind::Function => ("function", "->"),
+        CallableKind::Operation => ("operation", "=>"),
+    };
+
+    let functors = if let FunctorSetValue::Empty = decl.functors {
+        String::new()
+    } else {
+        format!(" is {}", decl.functors)
+    };
+
+    let temp = decl.input.ty.to_string();
+
+    // Doc comments would be formatted as markdown into this
+    // string once we're able to parse them out.
+    format!(
+        "```qsharp
+{} {} {} {} {}{}
+```
+",
+        kind, decl.name.name, temp, arrow, decl.output, functors
+    )
+}
+
+fn header_from_ident(ident: &Ident, ty: &Ty) -> String {
+    format!(
+        "```qsharp
+{} {}
+```
+",
+        ident.name, ty,
+    )
 }
--- language_service/src/hover/tests.rs
+++ language_service/src/hover/tests.rs
@@ -1,43 +1,62 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use super::{get_hover, Hover, Span};
+use super::get_hover;
 use crate::test_utils::{compile_with_fake_stdlib, get_source_and_marker_offsets};
-use indoc::indoc;
-
-#[test]
-fn hover_callable() {
-    assert_hover(
-        r#"
-        namespace Test {
-            /// Doc comment!
-            operation ◉F↘oo◉() : Unit {
-            }
-        }
-    "#,
-        Some(indoc!(
-            r#"```qsharp
-            operation Foo() : Unit
-            ```
-        "#
-        )),
-    );
-}
+use expect_test::{expect, Expect};
 
 /// Asserts that the hover text at the given cursor position matches the expected hover text.
 /// The cursor position is indicated by a `↘` marker in the source text.
 /// The expected hover span is indicated by two `◉` markers in the source text.
-fn assert_hover(source_with_markers: &str, expected_contents: Option<&str>) {
-    let (source, cursor_offsets, target_offsets) =
-        get_source_and_marker_offsets(source_with_markers);
+fn check(source_with_markers: &str, expect: &Expect) {
+    let (source, cursor_offsets, _) = get_source_and_marker_offsets(source_with_markers);
     let compilation = compile_with_fake_stdlib("<source>", &source);
-    let actual_hover = get_hover(&compilation, "<source>", cursor_offsets[0]);
-    let expected_hover: Option<Hover> = expected_contents.map(|expected_contents| Hover {
-        contents: expected_contents.to_string(),
-        span: Span {
-            start: target_offsets[0],
-            end: target_offsets[1],
-        },
-    });
-    assert_eq!(&expected_hover, &actual_hover);
+    let actual = get_hover(&compilation, "<source>", cursor_offsets[0]);
+    expect.assert_debug_eq(&actual);
+}
+
+#[test]
+fn hover_callable_unit_types() {
+    check(
+        r#"
+        namespace Test {
+            /// Doc comment!
+            operation ◉F↘oo◉() : Unit {}
+        }
+    "#,
+        &expect![[r#"
+            Some(
+                Hover {
+                    contents: "```qsharp\noperation Foo Unit => Unit\n```\n",
+                    span: Span {
+                        start: 77,
+                        end: 80,
+                    },
+                },
+            )
+        "#]],
+    );
+}
+
+#[test]
+fn hover_callable_with_callable_types() {
+    check(
+        r#"
+        namespace Test {
+            /// Doc comment!
+            operation ◉F↘oo◉(x : (Int => Int)) : (Int => Int) {x}
+        }
+    "#,
+        &expect![[r#"
+            Some(
+                Hover {
+                    contents: "```qsharp\noperation Foo (Int => Int is 0) => (Int => Int)\n```\n",
+                    span: Span {
+                        start: 77,
+                        end: 80,
+                    },
+                },
+            )
+        "#]],
+    );
 }
