ac47fa32b8471f998fde8733be995af3b4bd1ceb
Sarah Marshall <sarah.marshall@microsoft.com>
2023-06-21 16:13:42 -0700

    Display node in panic messages

--- compiler/qsc_ast/src/validate.rs
+++ compiler/qsc_ast/src/validate.rs
@@ -9,6 +9,7 @@ use crate::{
     visit::{self, Visitor},
 };
 use qsc_data_structures::index_map::IndexMap;
+use std::fmt::Display;
 
 #[derive(Default)]
 pub struct Validator {
@@ -16,11 +17,11 @@ pub struct Validator {
 }
 
 impl Validator {
-    fn check(&mut self, id: NodeId) {
+    fn check(&mut self, id: NodeId, node: impl Display) {
         if id.is_default() {
-            panic!("default node ID should be replaced")
+            panic!("default node ID should be replaced: {node}")
         } else if self.ids.contains_key(id) {
-            panic!("duplicate node ID");
+            panic!("duplicate node ID: {node}");
         } else {
             self.ids.insert(id, ());
         }
@@ -29,85 +30,85 @@ impl Validator {
 
 impl Visitor<'_> for Validator {
     fn visit_package(&mut self, package: &Package) {
-        self.check(package.id);
+        self.check(package.id, package);
         visit::walk_package(self, package);
     }
 
     fn visit_namespace(&mut self, namespace: &Namespace) {
-        self.check(namespace.id);
+        self.check(namespace.id, namespace);
         visit::walk_namespace(self, namespace);
     }
 
     fn visit_item(&mut self, item: &Item) {
-        self.check(item.id);
+        self.check(item.id, item);
         visit::walk_item(self, item);
     }
 
     fn visit_attr(&mut self, attr: &Attr) {
-        self.check(attr.id);
+        self.check(attr.id, attr);
         visit::walk_attr(self, attr);
     }
 
     fn visit_visibility(&mut self, visibility: &Visibility) {
-        self.check(visibility.id);
+        self.check(visibility.id, visibility);
     }
 
     fn visit_ty_def(&mut self, def: &TyDef) {
-        self.check(def.id);
+        self.check(def.id, def);
         visit::walk_ty_def(self, def);
     }
 
     fn visit_callable_decl(&mut self, decl: &CallableDecl) {
-        self.check(decl.id);
+        self.check(decl.id, decl);
         visit::walk_callable_decl(self, decl);
     }
 
     fn visit_spec_decl(&mut self, decl: &SpecDecl) {
-        self.check(decl.id);
+        self.check(decl.id, decl);
         visit::walk_spec_decl(self, decl);
     }
 
     fn visit_functor_expr(&mut self, expr: &FunctorExpr) {
-        self.check(expr.id);
+        self.check(expr.id, expr);
         visit::walk_functor_expr(self, expr);
     }
 
     fn visit_ty(&mut self, ty: &Ty) {
-        self.check(ty.id);
+        self.check(ty.id, ty);
         visit::walk_ty(self, ty);
     }
 
     fn visit_block(&mut self, block: &Block) {
-        self.check(block.id);
+        self.check(block.id, block);
         visit::walk_block(self, block);
     }
 
     fn visit_stmt(&mut self, stmt: &Stmt) {
-        self.check(stmt.id);
+        self.check(stmt.id, stmt);
         visit::walk_stmt(self, stmt);
     }
 
     fn visit_expr(&mut self, expr: &Expr) {
-        self.check(expr.id);
+        self.check(expr.id, expr);
         visit::walk_expr(self, expr);
     }
 
     fn visit_pat(&mut self, pat: &Pat) {
-        self.check(pat.id);
+        self.check(pat.id, pat);
         visit::walk_pat(self, pat);
     }
 
     fn visit_qubit_init(&mut self, init: &QubitInit) {
-        self.check(init.id);
+        self.check(init.id, init);
         visit::walk_qubit_init(self, init);
     }
 
     fn visit_path(&mut self, path: &Path) {
-        self.check(path.id);
+        self.check(path.id, path);
         visit::walk_path(self, path);
     }
 
     fn visit_ident(&mut self, ident: &Ident) {
-        self.check(ident.id);
+        self.check(ident.id, ident);
     }
 }
--- compiler/qsc_hir/src/validate.rs
+++ compiler/qsc_hir/src/validate.rs
@@ -6,6 +6,7 @@ use crate::{
     visit::{self, Visitor},
 };
 use qsc_data_structures::index_map::IndexMap;
+use std::fmt::Display;
 
 #[derive(Default)]
 pub struct Validator {
@@ -13,11 +14,11 @@ pub struct Validator {
 }
 
 impl Validator {
-    fn check(&mut self, id: NodeId) {
+    fn check(&mut self, id: NodeId, node: impl Display) {
         if id.is_default() {
-            panic!("default node ID should be replaced")
+            panic!("default node ID should be replaced: {node}")
         } else if self.ids.contains_key(id) {
-            panic!("duplicate node ID");
+            panic!("duplicate node ID: {node}");
         } else {
             self.ids.insert(id, ());
         }
@@ -26,41 +27,41 @@ impl Validator {
 
 impl Visitor<'_> for Validator {
     fn visit_callable_decl(&mut self, decl: &CallableDecl) {
-        self.check(decl.id);
+        self.check(decl.id, decl);
         visit::walk_callable_decl(self, decl);
     }
 
     fn visit_spec_decl(&mut self, decl: &SpecDecl) {
-        self.check(decl.id);
+        self.check(decl.id, decl);
         visit::walk_spec_decl(self, decl);
     }
 
     fn visit_block(&mut self, block: &Block) {
-        self.check(block.id);
+        self.check(block.id, block);
         visit::walk_block(self, block);
     }
 
     fn visit_stmt(&mut self, stmt: &Stmt) {
-        self.check(stmt.id);
+        self.check(stmt.id, stmt);
         visit::walk_stmt(self, stmt);
     }
 
     fn visit_expr(&mut self, expr: &Expr) {
-        self.check(expr.id);
+        self.check(expr.id, expr);
         visit::walk_expr(self, expr);
     }
 
     fn visit_pat(&mut self, pat: &Pat) {
-        self.check(pat.id);
+        self.check(pat.id, pat);
         visit::walk_pat(self, pat);
     }
 
     fn visit_qubit_init(&mut self, init: &QubitInit) {
-        self.check(init.id);
+        self.check(init.id, init);
         visit::walk_qubit_init(self, init);
     }
 
     fn visit_ident(&mut self, ident: &Ident) {
-        self.check(ident.id);
+        self.check(ident.id, ident);
     }
 }
