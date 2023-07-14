11ecee930547154bf155b755575029eb8a18e2cf
Scott Carda <Scott.Carda@microsoft.com>
2023-07-14 10:53:58 -0700

    go to def for local items

--- language_service/src/definition.rs
+++ language_service/src/definition.rs
@@ -4,9 +4,10 @@
 #[cfg(test)]
 mod tests;
 
-use crate::qsc_utils::{map_offset, span_contains, Compilation};
-use qsc::hir::{visit::walk_expr, visit::Visitor, Expr, ExprKind, ItemKind, Package, Res};
+use crate::qsc_utils::{find_item, map_offset, span_contains, Compilation};
+use qsc::ast::visit::{walk_callable_decl, walk_expr, walk_pat, walk_ty_def, Visitor};
 use qsc::SourceMap;
+use qsc::{ast, hir, resolve};
 
 #[derive(Debug, PartialEq)]
 pub struct Definition {
@@ -21,15 +22,16 @@ pub(crate) fn get_definition(
 ) -> Option<Definition> {
     // Map the file offset into a SourceMap offset
     let offset = map_offset(&compilation.unit.sources, source_name, offset);
-    let package = &compilation.unit.package;
+    let ast_package = &compilation.unit.ast;
 
     let mut definition_finder = DefinitionFinder {
-        package,
+        compilation,
         source_map: &compilation.unit.sources,
         offset,
         definition: None,
+        curr_callable: None,
     };
-    definition_finder.visit_package(package);
+    definition_finder.visit_package(&ast_package.package);
 
     definition_finder
         .definition
@@ -40,43 +42,162 @@ pub(crate) fn get_definition(
 }
 
 struct DefinitionFinder<'a> {
-    package: &'a Package,
+    compilation: &'a Compilation,
     source_map: &'a SourceMap,
     offset: u32,
     definition: Option<(String, u32)>,
+    curr_callable: Option<&'a ast::CallableDecl>,
 }
 
-impl<'a> Visitor<'_> for DefinitionFinder<'a> {
-    fn visit_expr(&mut self, expr: &Expr) {
+impl DefinitionFinder<'_> {
+    fn set_definition_from_position(&mut self, lo: u32) {
+        self.definition = Some((
+            self.source_map
+                .find_by_offset(lo)
+                .expect("source should exist for offset")
+                .name
+                .to_string(),
+            lo,
+        ));
+    }
+}
+
+impl<'a> Visitor<'a> for DefinitionFinder<'a> {
+
+    // Handles callable and UDT definitions
+    fn visit_item(&mut self, item: &'a ast::Item) {
+        if span_contains(item.span, self.offset) {
+            match &*item.kind {
+                ast::ItemKind::Callable(decl) => {
+                    if span_contains(decl.name.span, self.offset) {
+                        self.set_definition_from_position(decl.name.span.lo);
+                    } else if span_contains(decl.span, self.offset) {
+                        self.curr_callable = Some(decl);
+                        walk_callable_decl(self, decl);
+                        self.curr_callable = None;
+                    }
+                }
+                ast::ItemKind::Ty(ident, def) => {
+                    if span_contains(ident.span, self.offset) {
+                        self.set_definition_from_position(ident.span.lo);
+                    } else {
+                        self.visit_ty_def(def);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    // Handles UDT field definitions
+    fn visit_ty_def(&mut self, def: &'a ast::TyDef) {
+        if span_contains(def.span, self.offset) {
+            if let ast::TyDefKind::Field(ident, ty) = &*def.kind {
+                if let Some(ident) = ident {
+                    if span_contains(ident.span, self.offset) {
+                        self.set_definition_from_position(ident.span.lo);
+                    } else {
+                        self.visit_ty(ty);
+                    }
+                } else {
+                    self.visit_ty(ty);
+                }
+            } else {
+                walk_ty_def(self, def);
+            }
+        }
+    }
+
+    // Handles local variable definitions
+    fn visit_pat(&mut self, pat: &'a ast::Pat) {
+        if span_contains(pat.span, self.offset) {
+            match &*pat.kind {
+                ast::PatKind::Bind(ident, anno) => {
+                    if span_contains(ident.span, self.offset) {
+                        self.set_definition_from_position(ident.span.lo);
+                    } else if let Some(ty) = anno {
+                        self.visit_ty(ty);
+                    }
+                }
+                _ => walk_pat(self, pat),
+            }
+        }
+    }
+
+    // Handles UDT field references
+    fn visit_expr(&mut self, expr: &'a ast::Expr) {
         if span_contains(expr.span, self.offset) {
-            if let ExprKind::Var(res, _) = expr.kind {
-                let item = match res {
-                    Res::Err => None,
-                    // Just one package plus std for now, so let's live with this hack
-                    Res::Item(item) => {
-                        if item.package.is_none() {
-                            self.package.items.get(item.item)
-                        } else {
-                            // Handling std library is tricky for now
-                            None
+            match &*expr.kind {
+                ast::ExprKind::Field(_, field) if span_contains(field.span, self.offset) => {
+                    self.set_definition_from_position(field.span.lo);
+                }
+                _ => walk_expr(self, expr),
+            }
+        }
+    }
+
+    // Handles local variable, UDT, and callable references
+    fn visit_path(&mut self, path: &'_ ast::Path) {
+        if span_contains(path.span, self.offset) {
+            let res = self.compilation.unit.ast.names.get(path.id);
+            if let Some(res) = res {
+                match &res {
+                    resolve::Res::Item(item_id) => {
+                        if let Some(item) = find_item(self.compilation, item_id) {
+                            let lo = match &item.kind {
+                                hir::ItemKind::Callable(decl) => decl.name.span.lo,
+                                hir::ItemKind::Namespace(_, _) => {
+                                    panic!(
+                                        "Reference node should not refer to a namespace: {}",
+                                        path.id
+                                    )
+                                }
+                                hir::ItemKind::Ty(ident, _) => ident.span.lo,
+                            };
+                            self.set_definition_from_position(lo);
+                        };
+                    }
+                    resolve::Res::Local(node_id) => {
+                        if let Some(curr) = self.curr_callable {
+                            {
+                                let mut finder = AstFindNode {
+                                    node_id,
+                                    result: None,
+                                };
+                                finder.visit_callable_decl(curr);
+                                if let Some(lo) = finder.result {
+                                    self.set_definition_from_position(lo);
+                                }
+                            }
                         }
                     }
-                    Res::Local(_node) => None,
-                };
-                if let Some(def) = item {
-                    if let ItemKind::Callable(decl) = &def.kind {
-                        self.definition = Some((
-                            self.source_map
-                                .find_by_offset(decl.name.span.lo)
-                                .expect("source should exist for offset")
-                                .name
-                                .to_string(),
-                            decl.name.span.lo,
-                        ));
-                    }
+                    _ => {}
                 }
             }
         }
-        walk_expr(self, expr);
+    }
+}
+
+struct AstFindNode<'a> {
+    node_id: &'a ast::NodeId,
+    result: Option<u32>,
+}
+
+impl<'a> Visitor<'a> for AstFindNode<'_> {
+    fn visit_pat(&mut self, pat: &'a ast::Pat) {
+        match &*pat.kind {
+            ast::PatKind::Bind(ident, _) => {
+                if ident.id == *self.node_id {
+                    self.result = Some(ident.span.lo);
+                }
+            }
+            _ => walk_pat(self, pat),
+        }
+    }
+
+    fn visit_expr(&mut self, expr: &'a ast::Expr) {
+        if self.result.is_none() {
+            walk_expr(self, expr);
+        }
     }
 }
--- language_service/src/definition/tests.rs
+++ language_service/src/definition/tests.rs
@@ -4,22 +4,6 @@
 use super::{get_definition, Definition};
 use crate::test_utils::{compile_with_fake_stdlib, get_source_and_marker_offsets};
 
-#[test]
-fn definition_callable() {
-    assert_definition(
-        r#"
-    namespace Test {
-        operation ◉Callee() : Unit {
-        }
-
-        operation Caller() : Unit {
-            C↘allee();
-        }
-    }
-    "#,
-    );
-}
-
 /// Asserts that the definition found at the given cursor position matches the expected position.
 /// The cursor position is indicated by a `↘` marker in the source text.
 /// The expected definition position is indicated by a `◉` marker in the source text.
@@ -37,4 +21,209 @@ fn assert_definition(source_with_markers: &str) {
         })
     };
     assert_eq!(&expected_definition, &actual_definition);
+}
+
+#[test]
+fn definition_callable() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation ◉F↘oo() : Unit {
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_callable_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation ◉Callee() : Unit {
+        }
+
+        operation Caller() : Unit {
+            C↘allee();
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_variable() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo() : Unit {
+            let ◉↘x = 3;
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_variable_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo() : Unit {
+            let ◉x = 3;
+            let y = ↘x;
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_parameter() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(◉↘x: Int) : Unit {
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_parameter_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(◉x: Int) : Unit {
+            let y = ↘x;
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉B↘ar = (a: Int, b: Double);
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉Bar = (a: Int, b: Double);
+
+        operation Foo() : Unit {
+            let x = B↘ar(1, 2.3);
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt_ref_sig() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉Bar = (a: Int, b: Double);
+
+        operation Foo() : B↘ar {
+            Bar(1, 2.3)
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt_ref_param() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉Bar = (a: Int, b: Double);
+
+        operation Foo(x: B↘ar) : Unit {
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt_ref_anno() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉Bar = (a: Int, b: Double);
+
+        operation Foo() : Unit {
+            let x: B↘ar = Bar(1, 2.3);
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_udt_ref_ty_def() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype ◉Bar = (a: Int, b: Double);
+        newtype Foo = (a: B↘ar, b: Double);
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_lambda_param() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo() : Unit {
+            let local = (◉↘x, y) => x;
+            let z = local(1, 2.3);
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_lambda_param_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo() : Unit {
+            let local = (◉x, y) => ↘x;
+            let z = local(1, 2.3);
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn definition_lambda_closure_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo() : Unit {
+            let ◉a = "Hello";
+            let local = (x, y) => ↘a;
+            let z = local(1, 2.3);
+        }
+    }
+    "#,
+    );
 }
