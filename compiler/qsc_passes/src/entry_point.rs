792fc03c2df9a4134e6167ec71200277103da51e
Ian Davis <Ian.F.Davis@microsoft.com>
2023-07-14 12:40:35 -0700

    Update compiler/qsc_passes/src/entry_point.rs
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>

--- compiler/qsc_passes/src/entry_point.rs
+++ compiler/qsc_passes/src/entry_point.rs
@@ -38,7 +38,7 @@ pub enum Error {
 // If no entry expression is provided, generate one from the entry point callable.
 // Only one callable should be annotated with the entry point attribute.
 // If more than one callable is annotated, or none are annotated, we skip this pass.
-pub fn generate_entry_expr_from_entrypoint(unit: &mut CompileUnit) -> Vec<super::Error> {
+pub fn generate_entry_expr_from_entry_point(unit: &mut CompileUnit) -> Vec<super::Error> {
     if unit.package.entry.is_some() {
         return vec![];
     }
