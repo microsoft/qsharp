259503dc09434886a814b93ab815a88b98c5266e
Scott Carda <Scott.Carda@microsoft.com>
2023-07-19 11:57:05 -0700

    test changes/additions

--- language_service/src/definition/tests.rs
+++ language_service/src/definition/tests.rs
@@ -24,7 +24,7 @@ fn assert_definition(source_with_markers: &str) {
 }
 
 #[test]
-fn definition_callable() {
+fn callable() {
     assert_definition(
         r#"
     namespace Test {
@@ -36,7 +36,7 @@ fn definition_callable() {
 }
 
 #[test]
-fn definition_callable_ref() {
+fn callable_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -52,7 +52,7 @@ fn definition_callable_ref() {
 }
 
 #[test]
-fn definition_variable() {
+fn variable() {
     assert_definition(
         r#"
     namespace Test {
@@ -65,7 +65,7 @@ fn definition_variable() {
 }
 
 #[test]
-fn definition_variable_ref() {
+fn variable_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -79,7 +79,7 @@ fn definition_variable_ref() {
 }
 
 #[test]
-fn definition_parameter() {
+fn parameter() {
     assert_definition(
         r#"
     namespace Test {
@@ -91,7 +91,7 @@ fn definition_parameter() {
 }
 
 #[test]
-fn definition_parameter_ref() {
+fn parameter_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -104,7 +104,7 @@ fn definition_parameter_ref() {
 }
 
 #[test]
-fn definition_udt() {
+fn udt() {
     assert_definition(
         r#"
     namespace Test {
@@ -115,7 +115,7 @@ fn definition_udt() {
 }
 
 #[test]
-fn definition_udt_ref() {
+fn udt_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -130,7 +130,7 @@ fn definition_udt_ref() {
 }
 
 #[test]
-fn definition_udt_ref_sig() {
+fn udt_ref_sig() {
     assert_definition(
         r#"
     namespace Test {
@@ -145,7 +145,7 @@ fn definition_udt_ref_sig() {
 }
 
 #[test]
-fn definition_udt_ref_param() {
+fn udt_ref_param() {
     assert_definition(
         r#"
     namespace Test {
@@ -159,7 +159,7 @@ fn definition_udt_ref_param() {
 }
 
 #[test]
-fn definition_udt_ref_anno() {
+fn udt_ref_anno() {
     assert_definition(
         r#"
     namespace Test {
@@ -174,7 +174,7 @@ fn definition_udt_ref_anno() {
 }
 
 #[test]
-fn definition_udt_ref_ty_def() {
+fn udt_ref_ty_def() {
     assert_definition(
         r#"
     namespace Test {
@@ -186,7 +186,33 @@ fn definition_udt_ref_ty_def() {
 }
 
 #[test]
-fn definition_lambda_param() {
+fn udt_field() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype Pair = (◉f↘st: Int, snd: Double);
+    }
+    "#,
+    );
+}
+
+#[test]
+fn udt_field_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        newtype Pair = (fst: Int, ◉snd: Double);
+        operation Foo() : Unit {
+            let a = Pair(1, 2.3);
+            let b = a::s↘nd;
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn lambda_param() {
     assert_definition(
         r#"
     namespace Test {
@@ -200,7 +226,7 @@ fn definition_lambda_param() {
 }
 
 #[test]
-fn definition_lambda_param_ref() {
+fn lambda_param_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -214,7 +240,7 @@ fn definition_lambda_param_ref() {
 }
 
 #[test]
-fn definition_lambda_closure_ref() {
+fn lambda_closure_ref() {
     assert_definition(
         r#"
     namespace Test {
@@ -229,13 +255,92 @@ fn definition_lambda_closure_ref() {
 }
 
 #[test]
-fn definition_std_call() {
+fn std_call_no_goto() {
     assert_definition(
         r#"
     namespace Test {
         open FakeStdLib;
         operation Foo() : Unit {
             F↘ake();
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn other_namespace_call_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        open Other;
+        operation Foo() : Unit {
+            B↘ar();
+        }
+    }
+
+    namespace Other {
+        operation ◉Bar() : Unit {}
+    }
+    "#,
+    );
+}
+
+#[test]
+fn parameter_ref_with_body_specialization() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(◉x: Int) : Unit is Adj {
+            body ... {
+                let y = ↘x;
+            }
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn parameter_ref_with_adj_specialization() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(◉x: Int) : Unit is Adj {
+            body ... {}
+            adjoint ... {
+                let y = ↘x;
+            }
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn ctl_specialization_parameter() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(x: Int) : Unit is Ctl {
+            body ... {}
+            controlled (◉c↘s, ...) {}
+        }
+    }
+    "#,
+    );
+}
+
+#[test]
+fn ctl_specialization_parameter_ref() {
+    assert_definition(
+        r#"
+    namespace Test {
+        operation Foo(x: Int) : Unit is Ctl {
+            body ... {}
+            controlled (◉cs, ...) {
+                let y = c↘s;
+            }
         }
     }
     "#,
