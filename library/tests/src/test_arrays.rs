6aa483e453bd41ea614ae6eec5ede1954e9bc242
Dmitry Vasilevsky <dmitryv@microsoft.com>
2023-07-25 12:18:57 -0700

    cargo fmt

--- library/tests/src/test_arrays.rs
+++ library/tests/src/test_arrays.rs
@@ -585,25 +585,11 @@ fn check_padded() {
     );
     test_expression(
         "Microsoft.Quantum.Arrays.Padded(3, 2, [10, 11, 12])",
-        &Value::Array(
-            vec![
-                Value::Int(10),
-                Value::Int(11),
-                Value::Int(12),
-            ]
-            .into(),
-        ),
+        &Value::Array(vec![Value::Int(10), Value::Int(11), Value::Int(12)].into()),
     );
     test_expression(
         "Microsoft.Quantum.Arrays.Padded(-3, 2, [10, 11, 12])",
-        &Value::Array(
-            vec![
-                Value::Int(10),
-                Value::Int(11),
-                Value::Int(12),
-            ]
-            .into(),
-        ),
+        &Value::Array(vec![Value::Int(10), Value::Int(11), Value::Int(12)].into()),
     );
 }
 
