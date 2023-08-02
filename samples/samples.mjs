fe1e5f7af85a42d5b64677bf9d94f6644a4b36cf
Cesar Zaragoza Cortes <cesarzc@ntdev.microsoft.com>
2023-08-01 21:55:19 -0700

    Fix path.

--- samples/samples.mjs
+++ samples/samples.mjs
@@ -9,10 +9,10 @@
 export default [
     {title: "Getting Started", file: "./language/GettingStarted.qs", shots: 100},
     {title: "Bell State", file: "./algorithms/BellState.qs", shots: 100},
-    {title: "Teleportation", file: "./algorithms/Teleportation.qs", shots: 10},
+    {title: "Teleportation", file: "./algorithms/Teleportation.qs", shots: 1},
     {title: "Random Number Generator", file: "./algorithms/QRNG.qs", shots: 1000},
     {title: "Deutsch-Jozsa", file: "./algorithms/DeutschJozsa.qs", shots: 1},
-    {title: "Bernstein–Vazirani", file: "BernsteinVazirani.qs", shots: 1},
+    {title: "Bernstein–Vazirani", file: "./algorithms/BernsteinVazirani.qs", shots: 1},
     {title: "Grover's search", file: "Grover.qs", shots: 100},
     {title: "Hidden shift", file: "HiddenShift.qs", shots: 1},
     {title: "Shor", file: "Shor.qs", shots: 1},
