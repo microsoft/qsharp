25c6b1576d015160af5f4fcdd4cb938e9463aa87
Ian Davis <Ian.F.Davis@microsoft.com>
2023-08-11 14:06:40 -0700

    Update vscode/src/common.ts
    Co-authored-by: Mine Starks <16928427+minestarks@users.noreply.github.com>

--- vscode/src/common.ts
+++ vscode/src/common.ts
@@ -3,7 +3,7 @@
 
 export const qsharpLanguageId = "qsharp";
 export const qsharpExtensionId = "qsharp-vscode";
-export const QsLibraryUriScheme = "qsharp-source-request";
+export const qsharpLibraryUriScheme = "qsharp-library-source";
 
 export interface FileAccessor {
   readFile(uri: string): Promise<Uint8Array>;
