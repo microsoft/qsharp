ccd312880dfc067a7d8a54288791cc86934e79c4
Bill Ticehurst <billti@microsoft.com>
2023-08-22 12:02:53 -0700

    Prettier fixes

--- vscode/src/qirGeneration.ts
+++ vscode/src/qirGeneration.ts
@@ -4,24 +4,33 @@
 import * as vscode from "vscode";
 
 export function initCodegen(context: vscode.ExtensionContext) {
-    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 200);
-    statusBarItem.command = "quantum-set-target";
-    statusBarItem.text = "QIR:Full";
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    200
+  );
+  statusBarItem.command = "quantum-set-target";
+  statusBarItem.text = "QIR:Full";
 
-    context.subscriptions.push(vscode.commands.registerCommand("quantum-set-target", async () => {
-        const target = await vscode.window.showQuickPick(["QIR:Base", "QIR:Adaptive", "QIR:Full"], 
-        {placeHolder: "Select the QIR target profile"});
-        if (target) statusBarItem.text = target;
-    }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("quantum-set-target", async () => {
+      const target = await vscode.window.showQuickPick(
+        ["QIR:Base", "QIR:Adaptive", "QIR:Full"],
+        { placeHolder: "Select the QIR target profile" }
+      );
+      if (target) statusBarItem.text = target;
+    })
+  );
 
-    if (vscode.window.activeTextEditor?.document.languageId === "qsharp") {
+  if (vscode.window.activeTextEditor?.document.languageId === "qsharp") {
+    statusBarItem.show();
+  }
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor?.document.languageId === "qsharp") {
         statusBarItem.show();
-    }
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((editor) => {
-        if (editor?.document.languageId === "qsharp") {
-            statusBarItem.show();
-        } else {
-            statusBarItem.hide();
-        }
-    }));
+      } else {
+        statusBarItem.hide();
+      }
+    })
+  );
 }
