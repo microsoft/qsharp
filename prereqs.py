1181f082e1e0e6c768efe47be9f51e40de714e37
filipw <wojcieszyn@gmail.com>
2023-08-30 19:50:15 +0200

    fixed wasm-pack version detection

--- prereqs.py
+++ prereqs.py
@@ -134,10 +134,8 @@ def check_prereqs(install=False):
 
     ### Check the wasm_pack version ###
     try:
-        # This is a workaround since the --version option was removed in the latest release.
-        wasm_pack_help = subprocess.check_output(["wasm-pack", "--help"])
-        wasm_pack_version = wasm_pack_help.decode().splitlines()[0]
-        print(f"Detected wasm-pack version {wasm_pack_version}")
+        wasm_pack_version = subprocess.check_output(["wasm-pack", "--version"])
+        print(f"Detected wasm-pack version {wasm_pack_version.decode()}")
     except FileNotFoundError:
         if install == True:
             if platform.system() == "Windows":
@@ -164,15 +162,14 @@ def check_prereqs(install=False):
                     print("Attempting to install wasm-pack")
                     subprocess.run(["sh", file_name], check=True)
 
-            wasm_pack_help = subprocess.check_output(["wasm-pack", "--help"])
-            wasm_pack_version = wasm_pack_help.decode().splitlines()[0]
+            wasm_pack_version = subprocess.check_output(["wasm-pack", "--version"])
         else:
             print(
                 "wasm-pack not found. Please install from https://rustwasm.github.io/wasm-pack/installer/"
             )
             exit(1)
 
-    version_match = re.search(r"wasm-pack (\d+)\.(\d+).\d+", wasm_pack_version)
+    version_match = re.search(r"wasm-pack (\d+)\.(\d+).\d+", wasm_pack_version.decode())
     if version_match:
         wasm_major = int(version_match.group(1))
         wasm_minor = int(version_match.group(2))
