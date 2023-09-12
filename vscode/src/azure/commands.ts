73c365fe4bcf2454398c8ae5c72a55a9c0d98c34
Bill Ticehurst <billti@microsoft.com>
2023-09-11 20:37:34 -0700

    VS Code editor codegen and Azure submission (#621)
    A work in progress, but with this you can:
    
    - Run the "Get QIR for current Q# program" to see the LL codegen for the
    currently active editor - if base profile compliant. (Note: May not
    leave this in, but it's handy for testing and those who are
    QIR-curious).
    - Go into settings and enable the Azure Integration (need to opt in
    currently due to experimental nature and use of temp service for
    processing)
    - Sign-in to Azure via you AAD or MSA account and see your workspaces
    (ignore the 'Connection String' option for now which is mock data for
    quick validation)
    - Click on the submit icon (looks like a Play button) to the right of a
    hardware target to submit the currently active editor code.
     - Refresh the workspace view to see latest job status.
    - Click on the download icon to see the raw histogram data for completed
    jobs.
    
    It's spaghetti code currently from much experimentation, but sharing for
    early feedback on design and for a peek into service integration.
    
    Still TODO for this PR:
    
    - [x] Wire up the QIR Profile selector to switch the language service
    between base and full profiles and show corresponding errors.
    - [x] Disable job submission if active program has errors
    - [x] Improve display/filtering of providers and only enable submission
    on QIR capable ones.
    - [x] Better code structure, error handling, and diagnostics logging.
    
    TODO in a follow-up PR:
    
    - Auto-refresh the workspace tree periodically if any jobs are in the
    waiting or executing state, or newly submitted.
    - Remember and automatically reload workspaces in the tree view across
    editor sessions. (And ability to remove)
    - Ability to filter, sort, and manage (cancel, delete, etc.) jobs.
    - Ability to fetch the raw result data, display as histogram, etc.
    
    ---------
    
    Co-authored-by: Mine Starks <mineyalc@microsoft.com>
    Co-authored-by: Ian Davis <ian.f.davis@microsoft.com>
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>
    Co-authored-by: Stefan J. Wernli <swernli@microsoft.com>

---
+++ vscode/src/azure/commands.ts
@@ -0,0 +1,138 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as vscode from "vscode";
+import { log } from "qsharp-lang";
+
+import {
+  Job,
+  Target,
+  WorkspaceTreeItem,
+  WorkspaceTreeProvider,
+} from "./treeView";
+import {
+  getJobFiles,
+  getTokenForWorkspace,
+  queryWorkspace,
+  queryWorkspaces,
+  submitJob,
+} from "./workspaceActions";
+import { QuantumUris, compileToBitcode } from "./networkRequests";
+import { getQirForActiveWindow } from "../qirGeneration";
+
+const corsDocsUri = "https://github.com/microsoft/qsharp/wiki/Enabling-CORS";
+
+export function initAzureWorkspaces() {
+  const workspaceTreeProvider = new WorkspaceTreeProvider();
+  vscode.window.createTreeView("quantum-workspaces", {
+    treeDataProvider: workspaceTreeProvider,
+  });
+
+  vscode.commands.registerCommand(
+    "qsharp-vscode.targetSubmit",
+    async (arg: WorkspaceTreeItem) => {
+      const target = arg.itemData as Target;
+      const providerId = target.id.split(".")?.[0];
+
+      let qir = "";
+      try {
+        qir = await getQirForActiveWindow();
+      } catch (e: any) {
+        if (e?.name === "QirGenerationError") {
+          vscode.window.showErrorMessage(e.message);
+          return;
+        }
+      }
+      if (!qir) return;
+
+      // Note: Below compilation to be removed when all regions support .ll text directly
+      const compilerService: string | undefined = vscode.workspace
+        .getConfiguration("Q#")
+        .get("compilerService"); // e.g. in settings.json: "Q#.compilerService": "https://qsx-proxy.azurewebsites.net/api/compile"
+
+      const payload = !compilerService
+        ? qir
+        : await compileToBitcode(compilerService, qir, providerId);
+      // End of compilation to be removed
+
+      const token = await getTokenForWorkspace(arg.workspace);
+      if (!token) return;
+
+      const quantumUris = new QuantumUris(
+        arg.workspace.endpointUri,
+        arg.workspace.id
+      );
+
+      try {
+        await submitJob(token, quantumUris, payload, providerId, target.id);
+      } catch (e: any) {
+        log.error("Failed to submit job. ", e);
+        vscode.window.showErrorMessage(
+          "Failed to submit the job to Azure. " +
+            "Ensure CORS is configured correctly on the workspace storage account. " +
+            `See ${corsDocsUri} for more information.`
+        );
+        return;
+      }
+
+      setTimeout(async () => {
+        await queryWorkspace(arg.workspace);
+        workspaceTreeProvider.updateWorkspace(arg.workspace);
+      }, 1000);
+    }
+  );
+
+  vscode.commands.registerCommand("qsharp-vscode.workspacesRefresh", () => {
+    workspaceTreeProvider.refresh();
+  });
+
+  vscode.commands.registerCommand("qsharp-vscode.workspacesAdd", async () => {
+    const workspace = await queryWorkspaces();
+    if (workspace) {
+      workspaceTreeProvider.updateWorkspace(workspace);
+      workspaceTreeProvider.refresh();
+    }
+  });
+
+  vscode.commands.registerCommand(
+    "qsharp-vscode.downloadResults",
+    async (arg: WorkspaceTreeItem) => {
+      const job = arg.itemData as Job;
+
+      if (!job.outputDataUri) {
+        log.error("Download called for job with null outputDataUri", job);
+        return;
+      }
+
+      const fileUri = vscode.Uri.parse(job.outputDataUri);
+      const [, container, blob] = fileUri.path.split("/");
+
+      const token = await getTokenForWorkspace(arg.workspace);
+      if (!token) return;
+
+      const quantumUris = new QuantumUris(
+        arg.workspace.endpointUri,
+        arg.workspace.id
+      );
+
+      try {
+        const file = await getJobFiles(container, blob, token, quantumUris);
+        if (file) {
+          const doc = await vscode.workspace.openTextDocument({
+            content: file,
+            language: "plaintext",
+          });
+          vscode.window.showTextDocument(doc);
+        }
+      } catch (e: any) {
+        log.error("Failed to download result file. ", e);
+        vscode.window.showErrorMessage(
+          "Failed to download the results file. " +
+            "Ensure CORS is configured correctly on the workspace storage account. " +
+            `See ${corsDocsUri} for more information.`
+        );
+        return;
+      }
+    }
+  );
+}
---
+++ vscode/src/statusbar.ts
@@ -0,0 +1,119 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { log } from "qsharp-lang";
+import * as vscode from "vscode";
+import { isQsharpDocument } from "./common";
+
+export function activateTargetProfileStatusBarItem(): vscode.Disposable[] {
+  const disposables = [];
+
+  disposables.push(registerTargetProfileCommand());
+
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    200
+  );
+  disposables.push(statusBarItem);
+
+  statusBarItem.command = "qsharp-vscode.setTargetProfile";
+
+  disposables.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor && isQsharpDocument(editor.document)) {
+        refreshStatusBarItemValue();
+      } else {
+        // Could still be showing if the document language was changed away from Q#
+        statusBarItem.hide();
+      }
+    })
+  );
+
+  disposables.push(
+    vscode.workspace.onDidChangeConfiguration((event) => {
+      if (
+        vscode.window.activeTextEditor &&
+        isQsharpDocument(vscode.window.activeTextEditor.document) &&
+        event.affectsConfiguration("Q#.targetProfile")
+      ) {
+        refreshStatusBarItemValue();
+      }
+    })
+  );
+
+  if (
+    vscode.window.activeTextEditor &&
+    isQsharpDocument(vscode.window.activeTextEditor.document)
+  ) {
+    refreshStatusBarItemValue();
+  }
+
+  function refreshStatusBarItemValue() {
+    // The target profile setting is a "window" scoped setting,
+    // meaning it can't be set on a per-folder basis. So we don't
+    // need to pass a specific scope here to retrieve the document
+    // value - we just use the workspace setting.
+    // VS Code will return the default value defined by the extension
+    // if none was set by the user, so targetProfile should always
+    // be a valid string.
+    const targetProfile = vscode.workspace
+      .getConfiguration("Q#")
+      .get<string>("targetProfile", "full");
+
+    statusBarItem.text = getTargetProfileUiText(targetProfile);
+    statusBarItem.show();
+  }
+
+  return disposables;
+}
+
+function registerTargetProfileCommand() {
+  return vscode.commands.registerCommand(
+    "qsharp-vscode.setTargetProfile",
+    async () => {
+      const target = await vscode.window.showQuickPick(
+        targetProfiles.map((profile) => profile.uiText),
+        { placeHolder: "Select the QIR target profile" }
+      );
+
+      if (target) {
+        vscode.workspace
+          .getConfiguration("Q#")
+          .update(
+            "targetProfile",
+            getTargetProfileSetting(target),
+            vscode.ConfigurationTarget.Global
+          );
+      }
+    }
+  );
+}
+
+const targetProfiles = [
+  { configName: "base", uiText: "QIR:Base" },
+  { configName: "full", uiText: "QIR:Full" },
+];
+
+function getTargetProfileUiText(targetProfile?: string) {
+  switch (targetProfile) {
+    case "base":
+      return "QIR:Base";
+    case "full":
+      return "QIR:Full";
+    default:
+      log.error("invalid target profile found");
+      return "QIR:Invalid";
+  }
+}
+
+function getTargetProfileSetting(uiText: string) {
+  switch (uiText) {
+    case "QIR:Base":
+      return "base";
+    case "QIR:Full":
+      return "full";
+    default:
+      log.error("invalid target profile found");
+      return "full";
+  }
+}
