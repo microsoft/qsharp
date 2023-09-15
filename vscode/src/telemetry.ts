982721b18ef477ab11206b81d51554ea7dda976c
Alex Hansen <alex@alex-hansen.com>
2023-09-15 12:50:54 -0700

    switch aikey to devdiv ingestion key (#717)
    The service is responding 200s and successfully ingesting! 
    - switch to new key for telemetry ingestion
    - upgrade `extension-telemetry` package to latest version
    
    ---------
    
    Co-authored-by: Bill Ticehurst <billti@microsoft.com>

--- vscode/src/telemetry.ts
+++ vscode/src/telemetry.ts
@@ -3,10 +3,10 @@ import TelemetryReporter from "@vscode/extension-telemetry";
 import { log } from "qsharp-lang";
 
 export enum EventType {
-  DebugSessionStart = "DebugSessionStart",
-  InitializePlugin = "InitializePlugin",
-  LoadLanguageService = "LoadLanguageService",
-  QSharpJupyterCellInitialized = "QSharpJupyterCellInitialized",
+  DebugSessionStart = "Qsharp.DebugSessionStart",
+  InitializePlugin = "Qsharp.InitializePlugin",
+  LoadLanguageService = "Qsharp.LoadLanguageService",
+  QSharpJupyterCellInitialized = "Qsharp.JupyterCellInitialized",
 }
 
 type Empty = { [K in any]: never };
@@ -43,11 +43,7 @@ export function initTelemetry(context: vscode.ExtensionContext) {
   }
   // see issue here: https://github.com/microsoft/vscode-extension-telemetry/issues/185
   // we cannot use the latest version of extension-telemetry until this is fixed
-  reporter = new TelemetryReporter(
-    "qsharp-vscode",
-    packageJson.version,
-    packageJson.aiKey
-  );
+  reporter = new TelemetryReporter(packageJson.aiKey);
 
   sendTelemetryEvent(EventType.InitializePlugin, {}, {});
 }
