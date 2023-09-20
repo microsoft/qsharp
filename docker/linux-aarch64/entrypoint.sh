0c2074ad26bf643c0b1cf2dc7ee136a25d827836
Ian Davis <Ian.F.Davis@microsoft.com>
2023-09-20 18:35:44 +0000

    Setting up initial scaffolding.

---
+++ docker/linux-aarch64/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+
+WHEEL_ARCH="${WHEEL_ARCH:-aarch64}"
+echo "WHEEL_ARCH: ${WHEEL_ARCH}"
+
+WHEEL_PLATFORM="${WHEEL_PLATFORM:-manylinux_2_31_${WHEEL_ARCH}}"
+echo "WHEEL_PLATFORM: ${WHEEL_PLATFORM}"
+
+PIP_DIR="${PIP_DIR:-${SCRIPT_DIR}/../../pip}"
+echo "PIP_DIR: ${PIP_DIR}"
+
+WHEEL_DIR="${WHEEL_DIR:-${SCRIPT_DIR}/../../target/wheels}"
+echo "WHEEL_DIR: ${WHEEL_DIR}"
+
+echo "Repairing the wheels"
+ls ${WHEEL_DIR}
+ls ${WHEEL_DIR}/*.whl | xargs auditwheel show
+ls ${WHEEL_DIR}/*.whl | xargs auditwheel repair --wheel-dir ${WHEEL_DIR}/ --plat ${WHEEL_PLATFORM}
+rm ${WHEEL_DIR}/*-linux_${WHEEL_ARCH}.whl
+ls ${WHEEL_DIR}
+
+echo "Installing the wheels"
+ls ${WHEEL_DIR}/*.whl | xargs pip install
+
+pushd ${PIP_DIR}
+
+pip install -r test_requirements.txt
+
+python -m pytest
+
+popd
+
+
+
+
