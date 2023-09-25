09c0a2ec31fd24b052f56de3f12ffd7706c0cfbf
Stefan J. Wernli <swernli@microsoft.com>
2023-09-25 16:50:05 -0700

    Relax clippy docs requirements (#754)

--- katas/src/lib.rs
+++ katas/src/lib.rs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #![warn(clippy::mod_module_files, clippy::pedantic, clippy::unwrap_used)]
+#![allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 
 #[cfg(test)]
 mod tests;
