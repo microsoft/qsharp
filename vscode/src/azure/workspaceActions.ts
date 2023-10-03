d94aa986597f14aa45479c1d487cf8cad7d79e25
Bill Ticehurst <billti@microsoft.com>
2023-10-03 15:46:37 -0700

    Conform to RFC 4122 for UUIDs

--- vscode/src/azure/workspaceActions.ts
+++ vscode/src/azure/workspaceActions.ts
@@ -47,6 +47,12 @@ export async function getAuthSession(
 export function getRandomGuid(): string {
   const bytes = crypto.getRandomValues(new Uint8Array(16));
 
+  // Per https://www.ietf.org/rfc/rfc4122.txt, for UUID v4 (random GUIDs):
+  // - Octet 6 contains the version in top 4 bits (0b0100)
+  // - Octet 8 contains the variant in the top 2 bits (0b10)
+  bytes[6] = (bytes[6] & 0x0f) | 0x40;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+
   // Convert the 16 bytes into 32 hex digits
   const hex = bytes.reduce(
     (acc, byte) => acc + byte.toString(16).padStart(2, "0"),
