e714ac614c79669847fa6251405c7ca33a4a4c70
Alex Hansen <alehansen@microsoft.com>
2023-10-19 09:34:10 -0700

    remove unused glob walk dependency

--- compiler/qsc_project/src/error.rs
+++ compiler/qsc_project/src/error.rs
@@ -7,8 +7,6 @@ pub enum Error {
     SerdeJson(#[from] serde_json::Error),
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("failed to walk the project directory to discover Q# source files: {0}")]
-    FsWalk(#[from] globwalk::GlobError),
     #[error("failed to construct regular expression from excluded file item: {0}")]
     RegexError(#[from] regex_lite::Error),
 }
