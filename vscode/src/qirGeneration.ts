03a88cb3c9d6b18e22611a418046b56b53955e7f
Alex Hansen <alehansen@microsoft.com>
2023-10-20 13:01:33 -0700

    wip

--- vscode/src/qirGeneration.ts
+++ vscode/src/qirGeneration.ts
@@ -39,7 +39,7 @@ export async function getQirForActiveWindow(): Promise<string> {
     if (result?.action !== "set") {
       throw new QirGenerationError(
         "Submitting to Azure is only supported when targeting the QIR base profile. " +
-          "Please update the QIR target via the status bar selector or extension settings."
+        "Please update the QIR target via the status bar selector or extension settings."
       );
     } else {
       await configuration.update(
@@ -72,17 +72,23 @@ export async function getQirForActiveWindow(): Promise<string> {
     const associationId = generateAssociationId();
     sendTelemetryEvent(
       EventType.GenerateQirStart,
-      {associationId},
+      { associationId },
 
-      { }
+      {}
     );
     result = await worker.getQir(code);
+    sendTelemetryEvent(
+      EventType.GenerateQirEnd,
+      { associationId },
+
+      {}
+    );
     clearTimeout(compilerTimeout);
   } catch (e: any) {
     log.error("Codegen error. ", e.toString());
     throw new QirGenerationError(
       "Code generation failed. Please ensure the code is compatible with the QIR base profile " +
-        "by setting the target QIR profile to 'base' and fixing any errors."
+      "by setting the target QIR profile to 'base' and fixing any errors."
     );
   } finally {
     worker.terminate();
