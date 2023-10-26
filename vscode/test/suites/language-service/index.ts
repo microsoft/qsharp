e4acbe06b570fa3a83489ca9cc0b625b4c68a0f2
Mine Starks <mineyalc@microsoft.com>
2023-10-25 22:33:40 -0700

    vscode tests

--- vscode/test/suites/language-service/index.ts
+++ vscode/test/suites/language-service/index.ts
@@ -8,6 +8,7 @@ export function run(): Promise<void> {
     // We can't use any wildcards or dynamically discovered
     // paths here since ESBuild needs these modules to be
     // real paths on disk at bundling time.
-    require("./extension.test");
+    require("./language-service.test");
+    require("./notebook.test");
   });
 }
