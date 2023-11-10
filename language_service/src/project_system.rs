af517a6ea286d8f2e91c9b3e49038bfedd5ac652
Alex <alex@alex-hansen.com>
2023-11-10 06:58:40 -0800

    add async trait

--- language_service/src/project_system.rs
+++ language_service/src/project_system.rs
@@ -1,25 +1,33 @@
+use crate::LanguageService;
+use async_trait::async_trait;
 use std::{
     future::{self, Future},
-    path::PathBuf,
+    path::{Path, PathBuf},
     pin::Pin,
+    sync::Arc,
 };
 
-use crate::LanguageService;
-
+#[async_trait]
 impl qsc_project::FileSystem for LanguageService<'_> {
     type Entry = PathBuf;
-
-    fn read_file(
-        &self,
-        path: &std::path::Path,
-    ) -> miette::Result<(std::sync::Arc<str>, std::sync::Arc<str>)> {
-        Ok((self.read_file)(path.into()))
+    async fn read_file(&self, path: &Path) -> miette::Result<(Arc<str>, Arc<str>)> {
+        todo!()
     }
 
-    fn list_directory(
-        &self,
-        path: &std::path::Path,
-    ) -> Pin<Box<dyn Future<Output = miette::Result<Vec<Self::Entry>>>>> {
-        Box::pin(future::ready(Ok((self.list_directory)(path.into()))))
+    async fn list_directory(&self, path: &Path) -> miette::Result<Vec<Self::Entry>> {
+        todo!()
     }
+    // async fn read_file(
+    //     &self,
+    //     path: &std::path::Path,
+    // ) -> miette::Result<(std::sync::Arc<str>, std::sync::Arc<str>)> {
+    //     Ok((self.read_file)(path.into()))
+    // }
+
+    // async fn list_directory(
+    //     &self,
+    //     path: &std::path::Path,
+    // ) -> Pin<Box<dyn Future<Output = miette::Result<Vec<Self::Entry>>>>> {
+    //     Box::pin(future::ready(Ok((self.list_directory)(path.into()))))
+    // }
 }
