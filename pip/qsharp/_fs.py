f021e07a032fbbaa67c230728525eb284bb20da0
Mine Starks <16928427+minestarks@users.noreply.github.com>
2024-01-04 13:40:23 -0800

    qsharp.json support in Python (#861)
    Allow `qsharp.init` to take an optional project root. We'll look for a
    qsharp.json at the project root and compile the project sources at
    initialization.
    
    This PR originally removed `qsharp.eval_file` but I decided to relegate
    that to a separate PR to keep it manageable
    
    ----- 
    
    
    ![image](https://github.com/microsoft/qsharp/assets/16928427/f5fad3ce-ebea-48e7-914c-ff355c7293dd)
    
    -----
    
    ---------
    
    Co-authored-by: Ian Davis <ian.f.davis@microsoft.com>
    Co-authored-by: Stefan J. Wernli <swernli@microsoft.com>

---
+++ pip/qsharp/_fs.py
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import os
+from typing import Dict, List, Tuple
+
+
+def read_file(path: str) -> Tuple[str, str]:
+    with open(path, mode="r", encoding="utf-8") as f:
+        return (path, f.read())
+
+
+def list_directory(dir_path: str) -> List[Dict[str, str]]:
+    def map_dir(e: str) -> Dict[str, str]:
+        return {
+            "path": os.path.join(dir_path, e),
+            "entry_name": e,
+            "type": "file"
+            if os.path.isfile(os.path.join(dir_path, e))
+            else "folder"
+            if os.path.isdir(os.path.join(dir_path, e))
+            else "unknown",
+        }
+
+    return list(map(map_dir, os.listdir(dir_path)))
+
+
+def exists(path) -> bool:
+    return os.path.exists(path)
+
+
+def join(path: str, *paths) -> str:
+    return os.path.join(path, *paths)
