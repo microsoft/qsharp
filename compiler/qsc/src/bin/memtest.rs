970f920fff5bb043fa98e05fca43275678a8a9d8
Alex Hansen <alehansen@microsoft.com>
2024-01-05 13:38:54 -0800

    test comment

--- compiler/qsc/src/bin/memtest.rs
+++ compiler/qsc/src/bin/memtest.rs
@@ -9,37 +9,22 @@
 // - compare against base branch
 
 use jemalloc_ctl::{epoch, stats};
-use libc::{c_char, c_void};
-use qsc::compile;
+use qsc::{compile, CompileUnit};
 use qsc_frontend::compile::{PackageStore, RuntimeCapabilityFlags};
-use std::ptr::{null, null_mut};
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-pub fn library() {
+pub fn library() -> CompileUnit {
     let store = PackageStore::new(compile::core());
-    compile::std(&store, RuntimeCapabilityFlags::all());
-}
-
-extern "C" fn write_cb(_: *mut c_void, message: *const c_char) {
-    print!(
-        "{}",
-        String::from_utf8_lossy(unsafe {
-            std::ffi::CStr::from_ptr(message as *const i8).to_bytes()
-        })
-    );
-}
-
-fn mem_print() {
-    unsafe { jemalloc_sys::malloc_stats_print(Some(write_cb), null_mut(), null()) }
+    compile::std(&store, RuntimeCapabilityFlags::all())
 }
 
 fn main() {
     epoch::advance().unwrap();
 
     let before_allocated = stats::allocated::read().unwrap();
-    library();
+    let _ = library();
     epoch::advance().unwrap();
 
     let after_allocated = stats::allocated::read().unwrap();
