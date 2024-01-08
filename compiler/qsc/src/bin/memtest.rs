f692689405f0943c9d4e2e718288a2d5799b761d
Alex Hansen <alehansen@microsoft.com>
2024-01-07 20:26:54 -0800

    switch to non-jemalloc based profile

--- compiler/qsc/src/bin/memtest.rs
+++ compiler/qsc/src/bin/memtest.rs
@@ -8,32 +8,62 @@
 // - generate report for PR
 // - compare against base branch
 
-use jemalloc_ctl::{epoch, stats};
 use qsc::{compile, CompileUnit};
 use qsc_frontend::compile::{PackageStore, RuntimeCapabilityFlags};
 
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::sync::atomic::{AtomicU64, Ordering};
 
-pub fn library() -> CompileUnit {
+pub struct Trallocator<A: GlobalAlloc>(pub A, AtomicU64);
+
+unsafe impl<A: GlobalAlloc> GlobalAlloc for Trallocator<A> {
+    unsafe fn alloc(&self, l: Layout) -> *mut u8 {
+        self.1.fetch_add(l.size() as u64, Ordering::SeqCst);
+        self.0.alloc(l)
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, l: Layout) {
+        self.0.dealloc(ptr, l);
+        self.1.fetch_sub(l.size() as u64, Ordering::SeqCst);
+    }
+}
+
+impl<A: GlobalAlloc> Trallocator<A> {
+    pub const fn new(a: A) -> Self {
+        Trallocator(a, AtomicU64::new(0))
+    }
+
+    pub fn reset(&self) {
+        self.1.store(0, Ordering::SeqCst);
+    }
+    pub fn get(&self) -> u64 {
+        self.1.load(Ordering::SeqCst)
+    }
+}
+
+#[global_allocator]
+static GLOBAL: Trallocator<System> = Trallocator::new(System);
+
+pub fn compile_stdlib() -> CompileUnit {
     let store = PackageStore::new(compile::core());
     compile::std(&store, RuntimeCapabilityFlags::all())
 }
 
 fn main() {
-    epoch::advance().unwrap();
+    let _stdlib = compile_stdlib();
+    let std = GLOBAL.get();
 
-    let before_allocated = stats::allocated::read().unwrap();
-    let _ = library();
-    epoch::advance().unwrap();
+    GLOBAL.reset();
 
-    let after_allocated = stats::allocated::read().unwrap();
-    let std = after_allocated - before_allocated;
+    let _core = PackageStore::new(compile::core());
+
+    let core = GLOBAL.get();
+
     println!(
         r#"# Memory Report
-| Test         | This Branch | On Main |
-|--------------|-------------|---------|
-| standard lib | {std}       | 376080  |
+| Test                        | This Branch | On Main |
+|-----------------------------|-------------|---------|
+| compile core                | {core}      | 376080  |
+| compile core + standard lib | {std}       | 376080  |
 "#
     );
 }
