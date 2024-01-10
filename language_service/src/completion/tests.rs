6b01df561a3b1c344bfc96a52d93f0a1972b98a0
Mine Starks <mineyalc@microsoft.com>
2024-01-09 16:25:39 -0800

    add another test case

--- language_service/src/completion/tests.rs
+++ language_service/src/completion/tests.rs
@@ -822,7 +822,7 @@ fn notebook_block() {
 }
 
 #[test]
-fn notebook_auto_open_start_of_cell() {
+fn notebook_auto_open_start_of_cell_empty() {
     check_notebook(
         &[
             ("cell1", "namespace Foo { operation Bar() : Unit {} }"),
@@ -849,6 +849,44 @@ fn notebook_auto_open_start_of_cell() {
                                         end: 0,
                                     },
                                     "open FakeStdLib;\n",
+                                ),
+                            ],
+                        ),
+                    },
+                ),
+            ]
+        "#]],
+    );
+}
+
+#[test]
+fn notebook_auto_open_start_of_cell() {
+    check_notebook(
+        &[
+            ("cell1", "namespace Foo { operation Bar() : Unit {} }"),
+            ("cell2", r#"   Message("hi") ↘"#),
+        ],
+        &["Fake"],
+        &expect![[r#"
+            [
+                Some(
+                    CompletionItem {
+                        label: "Fake",
+                        kind: Function,
+                        sort_text: Some(
+                            "0800Fake",
+                        ),
+                        detail: Some(
+                            "operation Fake() : Unit",
+                        ),
+                        additional_text_edits: Some(
+                            [
+                                (
+                                    Span {
+                                        start: 3,
+                                        end: 3,
+                                    },
+                                    "open FakeStdLib;\n   ",
                                 ),
                             ],
                         ),
