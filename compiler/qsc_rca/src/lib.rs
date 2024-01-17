5d8dbce212c662602a86e20ffe91d512501d648c
cesarzc <cesarzc@microsoft.com>
2024-01-16 17:22:15 -0800

    Setup for callables analysis.

--- compiler/qsc_rca/src/lib.rs
+++ compiler/qsc_rca/src/lib.rs
@@ -15,28 +15,28 @@ use qsc_fir::fir::{
 use qsc_frontend::compile::RuntimeCapabilityFlags;
 use rca::analyze_package_and_update_compute_props;
 
-/// A trait to find the compute properties of elements in a package store.
+/// A trait to look for the compute properties of elements in a package store.
 pub trait ComputePropsLookup {
-    /// Gets the compute properties of an item.
-    fn get_item(&self, id: StoreItemId) -> &ItemComputeProps;
-    /// Gets the compute properties of a block.
-    fn get_block(&self, id: StoreBlockId) -> &ElmntComputeProps;
-    /// Gets the compute properties of a statement.
-    fn get_stmt(&self, id: StoreStmtId) -> &ElmntComputeProps;
-    /// Gets the compute properties of an expression.
-    fn get_expr(&self, id: StoreExprId) -> &ElmntComputeProps;
-    /// Gets the compute properties of a pattern.
-    fn get_pats(&self, id: StorePatId) -> &PatComputeProps;
-    /// Searches for the compute properties of an item with the specified ID.
-    fn find_item(&self, id: StoreItemId) -> Option<&ItemComputeProps>;
     /// Searches for the compute properties of a block with the specified ID.
     fn find_block(&self, id: StoreBlockId) -> Option<&ElmntComputeProps>;
-    /// Searches for the compute properties of a statement with the specified ID.
-    fn find_stmt(&self, id: StoreStmtId) -> Option<&ElmntComputeProps>;
     /// Searches for the compute properties of an expression with the specified ID.
     fn find_expr(&self, id: StoreExprId) -> Option<&ElmntComputeProps>;
+    /// Searches for the compute properties of an item with the specified ID.
+    fn find_item(&self, id: StoreItemId) -> Option<&ItemComputeProps>;
     /// Searches for the compute properties of a pattern with the specified ID.
     fn find_pats(&self, id: StorePatId) -> Option<&PatComputeProps>;
+    /// Searches for the compute properties of a statement with the specified ID.
+    fn find_stmt(&self, id: StoreStmtId) -> Option<&ElmntComputeProps>;
+    /// Gets the compute properties of a block.
+    fn get_block(&self, id: StoreBlockId) -> &ElmntComputeProps;
+    /// Gets the compute properties of an expression.
+    fn get_expr(&self, id: StoreExprId) -> &ElmntComputeProps;
+    /// Gets the compute properties of an item.
+    fn get_item(&self, id: StoreItemId) -> &ItemComputeProps;
+    /// Gets the compute properties of a pattern.
+    fn get_pats(&self, id: StorePatId) -> &PatComputeProps;
+    /// Gets the compute properties of a statement.
+    fn get_stmt(&self, id: StoreStmtId) -> &ElmntComputeProps;
 }
 
 /// The compute properties of a package store.
@@ -44,44 +44,9 @@ pub trait ComputePropsLookup {
 pub struct PackageStoreComputeProps(IndexMap<PackageId, PackageComputeProps>);
 
 impl ComputePropsLookup for PackageStoreComputeProps {
-    fn get_item(&self, id: StoreItemId) -> &ItemComputeProps {
-        self.find_item(id)
-            .expect("item compute properties should exist")
-    }
-
-    fn get_block(&self, id: StoreBlockId) -> &ElmntComputeProps {
-        self.find_block(id)
-            .expect("block compute properties should exist")
-    }
-
-    fn get_stmt(&self, id: StoreStmtId) -> &ElmntComputeProps {
-        self.find_stmt(id)
-            .expect("statement compute properties should exist")
-    }
-
-    fn get_expr(&self, id: StoreExprId) -> &ElmntComputeProps {
-        self.find_expr(id)
-            .expect("expression compute properties should exist")
-    }
-
-    fn get_pats(&self, id: StorePatId) -> &PatComputeProps {
-        self.find_pats(id)
-            .expect("pattern compute properties should exist")
-    }
-
-    fn find_item(&self, id: StoreItemId) -> Option<&ItemComputeProps> {
-        self.get(id.package)
-            .and_then(|package| package.items.get(id.item))
-    }
-
     fn find_block(&self, id: StoreBlockId) -> Option<&ElmntComputeProps> {
         self.get(id.package)
             .and_then(|package| package.blocks.get(id.block))
-    }
-
-    fn find_stmt(&self, id: StoreStmtId) -> Option<&ElmntComputeProps> {
-        self.get(id.package)
-            .and_then(|package| package.stmts.get(id.stmt))
     }
 
     fn find_expr(&self, id: StoreExprId) -> Option<&ElmntComputeProps> {
@@ -89,13 +54,83 @@ impl ComputePropsLookup for PackageStoreComputeProps {
             .and_then(|package| package.exprs.get(id.expr))
     }
 
+    fn find_item(&self, id: StoreItemId) -> Option<&ItemComputeProps> {
+        self.get(id.package)
+            .and_then(|package| package.items.get(id.item))
+    }
+
     fn find_pats(&self, id: StorePatId) -> Option<&PatComputeProps> {
         self.get(id.package)
             .and_then(|package| package.pats.get(id.pat))
     }
+
+    fn find_stmt(&self, id: StoreStmtId) -> Option<&ElmntComputeProps> {
+        self.get(id.package)
+            .and_then(|package| package.stmts.get(id.stmt))
+    }
+
+    fn get_block(&self, id: StoreBlockId) -> &ElmntComputeProps {
+        self.find_block(id)
+            .expect("block compute properties should exist")
+    }
+
+    fn get_expr(&self, id: StoreExprId) -> &ElmntComputeProps {
+        self.find_expr(id)
+            .expect("expression compute properties should exist")
+    }
+
+    fn get_item(&self, id: StoreItemId) -> &ItemComputeProps {
+        self.find_item(id)
+            .expect("item compute properties should exist")
+    }
+
+    fn get_pats(&self, id: StorePatId) -> &PatComputeProps {
+        self.find_pats(id)
+            .expect("pattern compute properties should exist")
+    }
+
+    fn get_stmt(&self, id: StoreStmtId) -> &ElmntComputeProps {
+        self.find_stmt(id)
+            .expect("statement compute properties should exist")
+    }
 }
 
 impl PackageStoreComputeProps {
+    pub fn insert_block(&mut self, id: StoreBlockId, value: ElmntComputeProps) {
+        self.get_mut(id.package)
+            .expect("package should exist")
+            .blocks
+            .insert(id.block, value);
+    }
+
+    pub fn insert_expr(&mut self, id: StoreExprId, value: ElmntComputeProps) {
+        self.get_mut(id.package)
+            .expect("package should exist")
+            .exprs
+            .insert(id.expr, value);
+    }
+
+    pub fn insert_item(&mut self, id: StoreItemId, value: ItemComputeProps) {
+        self.get_mut(id.package)
+            .expect("package should exist")
+            .items
+            .insert(id.item, value);
+    }
+
+    pub fn insert_pat(&mut self, id: StorePatId, value: PatComputeProps) {
+        self.get_mut(id.package)
+            .expect("package should exist")
+            .pats
+            .insert(id.pat, value);
+    }
+
+    pub fn insert_stmt(&mut self, id: StoreExprId, value: ElmntComputeProps) {
+        self.get_mut(id.package)
+            .expect("package should exist")
+            .exprs
+            .insert(id.expr, value);
+    }
+
     pub fn get(&self, id: PackageId) -> Option<&PackageComputeProps> {
         self.0.get(id)
     }
--- compiler/qsc_rca/src/rca.rs
+++ compiler/qsc_rca/src/rca.rs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use crate::PackageStoreComputeProps;
-use qsc_fir::fir::{PackageId, PackageStore, PackageStoreLookup, StoreItemId, StoreStmtId};
+use crate::{ComputePropsLookup, ItemComputeProps, PackageStoreComputeProps};
+use qsc_fir::fir::{
+    CallableDecl, Global, PackageId, PackageStore, PackageStoreLookup, StoreItemId, StoreStmtId,
+};
 
 pub fn analyze_package_and_update_compute_props(
     id: PackageId,
@@ -18,23 +20,41 @@ pub fn analyze_package_and_update_compute_props(
 
     // Analyze all items in the package.
     for (item_id, _) in &package.items {
-        analyze_item_and_update_compute_props((id, item_id).into(), package_store, compute_props);
+        analyze_and_update_item_compute_props((id, item_id).into(), package_store, compute_props);
     }
 
     // Analyze all statements in the package.
     for (stmt_id, _) in &package.stmts {
-        analyze_stmt_and_update_compute_props((id, stmt_id).into(), package_store, compute_props);
+        analyze_and_update_stmt_compute_props((id, stmt_id).into(), package_store, compute_props);
     }
 }
 
-fn analyze_item_and_update_compute_props(
+fn analyze_and_update_callable_compute_props(
     _id: StoreItemId,
+    _callable: &CallableDecl,
     _package_store: &impl PackageStoreLookup,
     _compute_props: &mut PackageStoreComputeProps,
 ) {
 }
 
-fn analyze_stmt_and_update_compute_props(
+fn analyze_and_update_item_compute_props(
+    id: StoreItemId,
+    package_store: &impl PackageStoreLookup,
+    compute_props: &mut PackageStoreComputeProps,
+) {
+    // If the item has already been analyzed, there's nothing else left to do.
+    if compute_props.find_item(id).is_some() {
+        return;
+    }
+
+    if let Some(Global::Callable(callable)) = package_store.get_global(id) {
+        analyze_and_update_callable_compute_props(id, callable, package_store, compute_props);
+    } else {
+        compute_props.insert_item(id, ItemComputeProps::NonCallable);
+    }
+}
+
+fn analyze_and_update_stmt_compute_props(
     _id: StoreStmtId,
     _package_store: &impl PackageStoreLookup,
     _compute_props: &mut PackageStoreComputeProps,
