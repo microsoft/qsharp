54fdb7d1a0626451c1e579ca5fc2eaef0914ede8
Alex Hansen <alex@alex-hansen.com>
2024-01-18 19:01:46 -0800

    Update samples/samples.mjs
    Co-authored-by: Stefan J. Wernli <swernli@microsoft.com>

--- samples/samples.mjs
+++ samples/samples.mjs
@@ -23,7 +23,7 @@ export default [
     { title: "Hidden Shift", file: "./algorithms/HiddenShiftNISQ.qs", shots: 1 },
     { title: "Hidden Shift (Advanced)", file: "./algorithms/HiddenShift.qs", shots: 1 },
     { title: "Shor", file: "./algorithms/Shor.qs", shots: 1 },
-    { title: "Dynamics", file: "./estimation/Dynamics.qs", shots: 1 },
-    { title: "Precalculated", file: "./estimation/Precalculated.qs", shots: 1 },
-    { title: "Shor's Resource Estimation", file: "./estimation/ShorRE.qs", shots: 1 },
+    { title: "Dynamics (Resource Estimation)", file: "./estimation/Dynamics.qs", shots: 1 },
+    { title: "Precalculated (Resource Estimation)", file: "./estimation/Precalculated.qs", shots: 1 },
+    { title: "Shor (Resource Estimation)", file: "./estimation/ShorRE.qs", shots: 1 },
 ];
