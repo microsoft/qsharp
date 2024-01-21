ee5fe65517a077e78d5f2b1d504f0d497bae58ea
cesarzc <cesarzc@microsoft.com>
2024-01-20 16:15:58 -0800

    Add some tests for intrinsics.

--- compiler/qsc_rca/src/tests.rs
+++ compiler/qsc_rca/src/tests.rs
@@ -22,9 +22,7 @@ fn qubit_allocation_intrinsics_analysis_is_correct() {
     )
     .expect("should be able to create a new compiler");
     let fir_store = lower_hir_package_store(compiler.package_store());
-    write_fir_store_to_files(&fir_store); // TODO (cesarzc): for debugging purposes only.
     let analyzer = Analyzer::new(&fir_store, map_hir_package_to_fir(compiler.package_id()));
-    write_compute_properties_to_files(analyzer.get_package_store_compute_properties()); // TODO (cesarzc): for debugging purposes only.
     check_callable_compute_properties(
         &fir_store,
         &analyzer.compute_properties,
@@ -124,5 +122,194 @@ fn core_lib_functions_analysis_is_correct() {
         &analyzer.compute_properties,
         "Repeated",
         &expect![r#""#],
+    );
+}
+
+#[test]
+fn std_lib_measurement_intrisics_analysis_is_correct() {
+    let compiler = Compiler::new(
+        true,
+        SourceMap::default(),
+        PackageType::Lib,
+        RuntimeCapabilityFlags::all(),
+    )
+    .expect("should be able to create a new compiler");
+    let fir_store = lower_hir_package_store(compiler.package_store());
+    let analyzer = Analyzer::new(&fir_store, map_hir_package_to_fir(compiler.package_id()));
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "__quantum__qis__m__body",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                        quantum_source: Intrinsic
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(HigherLevelConstructs)
+                            quantum_source: Intrinsic
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "__quantum__qis__mresetz__body",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                        quantum_source: Intrinsic
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(HigherLevelConstructs)
+                            quantum_source: Intrinsic
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+}
+
+#[test]
+fn std_lib_convert_intrisics_analysis_is_correct() {
+    let compiler = Compiler::new(
+        true,
+        SourceMap::default(),
+        PackageType::Lib,
+        RuntimeCapabilityFlags::all(),
+    )
+    .expect("should be able to create a new compiler");
+    let fir_store = lower_hir_package_store(compiler.package_store());
+    let analyzer = Analyzer::new(&fir_store, map_hir_package_to_fir(compiler.package_id()));
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "IntAsDouble",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(IntegerComputations)
+                            quantum_source: Intrinsic
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "IntAsBigInt",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(IntegerComputations)
+                            quantum_source: Intrinsic
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+}
+
+#[test]
+fn std_lib_diagnostics_intrisics_analysis_is_correct() {
+    let compiler = Compiler::new(
+        true,
+        SourceMap::default(),
+        PackageType::Lib,
+        RuntimeCapabilityFlags::all(),
+    )
+    .expect("should be able to create a new compiler");
+    let fir_store = lower_hir_package_store(compiler.package_store());
+    let analyzer = Analyzer::new(&fir_store, map_hir_package_to_fir(compiler.package_id()));
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "DumpMachine",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                    dynamic_params_properties:
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "CheckZero",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                        quantum_source: Intrinsic
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(HigherLevelConstructs)
+                            quantum_source: Intrinsic
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
+    );
+}
+
+#[test]
+fn std_lib_available_by_default_intrisics_analysis_is_correct() {
+    let compiler = Compiler::new(
+        true,
+        SourceMap::default(),
+        PackageType::Lib,
+        RuntimeCapabilityFlags::all(),
+    )
+    .expect("should be able to create a new compiler");
+    let fir_store = lower_hir_package_store(compiler.package_store());
+    write_fir_store_to_files(&fir_store); // TODO (cesarzc): for debugging purposes only.
+    let analyzer = Analyzer::new(&fir_store, map_hir_package_to_fir(compiler.package_id()));
+    write_compute_properties_to_files(analyzer.get_package_store_compute_properties()); // TODO (cesarzc): for debugging purposes only.
+    check_callable_compute_properties(
+        &fir_store,
+        &analyzer.compute_properties,
+        "Message",
+        &expect![
+            r#"
+            Callable: CallableComputeProperties:
+                body: ApplicationsTable:
+                    inherent: ComputeProperties:
+                        runtime_capabilities: RuntimeCapabilityFlags(0x0)
+                    dynamic_params_properties:
+                        [0]: ComputeProperties:
+                            runtime_capabilities: RuntimeCapabilityFlags(HigherLevelConstructs)
+                adj: <none>
+                ctl: <none>
+                ctl-adj: <none>"#
+        ],
     );
 }
