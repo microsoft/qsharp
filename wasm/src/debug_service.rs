df14c57dd5dd7a7938893077889db466db7ab542
Mine Starks <mineyalc@microsoft.com>
2024-01-31 16:05:40 -0800

    Eliminate FileAccessor in the debugger

--- wasm/src/debug_service.rs
+++ wasm/src/debug_service.rs
@@ -8,7 +8,7 @@ use qsc::interpret::{Debugger, Error, StepAction, StepResult};
 use qsc::line_column::Encoding;
 use qsc::{fmt_complex, target::Profile};
 
-use crate::line_column::Range;
+use crate::line_column::{Location, Range};
 use crate::{get_source_map, serializable_type, CallbackReceiver};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -67,11 +67,10 @@ impl DebugService {
 
         StackFrameList {
             frames: frames
-                .iter()
+                .into_iter()
                 .map(|s| StackFrame {
                     name: format!("{} {}", s.name, s.functor),
-                    path: s.path.clone(),
-                    range: s.range.into(),
+                    location: s.location.into(),
                 })
                 .collect(),
         }
@@ -326,13 +325,11 @@ serializable_type! {
     StackFrame,
     {
         pub name: String,
-        pub path: String,
-        pub range: Range
+        pub location: Location
     },
     r#"export interface IStackFrame {
         name: string;
-        path: string;
-        range: IRange;
+        location: ILocation
     }"#
 }
 
