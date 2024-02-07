27c6e032b932967946fa15addc95821847421cee
Mine Starks <16928427+minestarks@users.noreply.github.com>
2024-02-07 10:48:27 -0800

    Set notebook cell language back to Python if `%%qsharp` magic isn't there (#1118)
    Fixes #1117

--- vscode/test/suites/language-service/notebook.test.ts
+++ vscode/test/suites/language-service/notebook.test.ts
@@ -3,7 +3,7 @@
 
 import * as vscode from "vscode";
 import { assert } from "chai";
-import { activateExtension } from "../extensionUtils";
+import { activateExtension, waitForCondition } from "../extensionUtils";
 
 suite("Q# Notebook Tests", function suite() {
   const workspaceFolder =
@@ -23,40 +23,56 @@ suite("Q# Notebook Tests", function suite() {
 
     // Test if the cell with the %%qsharp magic has been detected
     // and its language switched to qsharp. We can only expect this to happen
-    // after the notebook has happened, and the document handlers
+    // after the notebook has been opened, and the document handlers
     // have been invoked, but of course there is no callback for that.
     // So we just verify the notebook has been updated with the qsharp
     // language id within 50ms (If we start exceeding this timeout for some
     // reason, it's enough of a user-perceptible delay that we're probably
     // better off disabling this behavior, rather than suddenly change the
     // cell language from under the user after a delay).
-    await new Promise<void>((resolve, reject) => {
-      let done = false;
-      setTimeout(() => {
-        if (!done) {
-          reject(new Error("timed out waiting for a Q# code cell"));
-        }
-      }, 50);
-
-      vscode.workspace.onDidChangeNotebookDocument((event) => {
-        if (!done && hasQSharpCell(event.notebook)) {
-          done = true;
-          resolve();
-        }
-      });
-
-      // in case the notebook updates have already occurred by the time we get here
-      if (hasQSharpCell(notebook)) {
-        done = true;
-        resolve();
-      }
-
-      function hasQSharpCell(notebook) {
-        return notebook
+    await waitForCondition(
+      () =>
+        !!notebook
           .getCells()
-          .find((cell) => cell.document.languageId === "qsharp");
-      }
-    });
+          .find((cell) => cell.document.languageId === "qsharp"),
+      vscode.workspace.onDidChangeNotebookDocument,
+      50,
+      "timed out waiting for a Q# code cell",
+    );
+  });
+
+  test("Cell language is set back to Python", async () => {
+    const notebook = await vscode.workspace.openNotebookDocument(
+      vscode.Uri.joinPath(workspaceFolderUri, "test.ipynb"),
+    );
+
+    await vscode.window.showNotebookDocument(notebook);
+
+    assert.equal(
+      vscode.window.activeNotebookEditor?.notebook.uri.toString(),
+      notebook.uri.toString(),
+    );
+
+    const oldLength = notebook.getCells().length;
+
+    // Add a new cell at the bottom of the notebook
+    await vscode.commands.executeCommand("notebook.focusBottom");
+    await vscode.commands.executeCommand("notebook.cell.insertCodeCellBelow");
+
+    // There should be an additional cell in the notebook and it should be Python
+    await waitForCondition(
+      () => {
+        const cellsAfter = notebook.getCells();
+        return (
+          cellsAfter.length === oldLength + 1 &&
+          notebook.getCells()[cellsAfter.length - 1].document.languageId ===
+            "python"
+        );
+      },
+      vscode.workspace.onDidChangeNotebookDocument,
+      50,
+      "timed out waiting for a Python code cell",
+    );
   });
 
   test("Diagnostics", async () => {
