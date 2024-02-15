1dff590d29869eaae78938e9530b0170a569713f
Stefan J. Wernli <swernli@microsoft.com>
2024-02-15 09:06:23 -0800

    Test updates

--- compiler/qsc_codegen/src/qir_base/tests.rs
+++ compiler/qsc_codegen/src/qir_base/tests.rs
@@ -1000,7 +1000,7 @@ fn qubit_ids_properly_reused() {
 }
 
 #[test]
-fn custom_itrinsic_on_single_qubit() {
+fn custom_intrinsic_on_single_qubit() {
     check(
         indoc! {"
         namespace Test {
@@ -1058,6 +1058,93 @@ fn custom_itrinsic_on_single_qubit() {
             declare void @__quantum__rt__array_record_output(i64, i8*)
             declare void @__quantum__rt__tuple_record_output(i64, i8*)
             declare void @MyCustomGate(%Qubit*)
+
+            attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="2" "required_num_results"="2" }
+            attributes #1 = { "irreversible" }
+
+            ; module flags
+
+            !llvm.module.flags = !{!0, !1, !2, !3}
+
+            !0 = !{i32 1, !"qir_major_version", i32 1}
+            !1 = !{i32 7, !"qir_minor_version", i32 0}
+            !2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+            !3 = !{i32 1, !"dynamic_result_management", i1 false}
+        "#]],
+    );
+}
+
+#[test]
+fn multiple_custom_intrinsic_calls() {
+    check(
+        indoc! {"
+        namespace Test {
+            open Microsoft.Quantum.Measurement;
+            @EntryPoint()
+            operation Test() : (Result, Result) {
+                use (q0, q1) = (Qubit(), Qubit());
+                MySecondCustomGate(q0, q1);
+                MyCustomGate(q0);
+                MyCustomFunc(42);
+                return (MResetZ(q0), MResetZ(q1));
+            }
+
+            operation MyCustomGate(q : Qubit) : Unit {
+                body intrinsic;
+            }
+
+            operation MySecondCustomGate(q0 : Qubit, q1 : Qubit) : Unit {
+                body intrinsic;
+            }
+
+            function MyCustomFunc(n : Int) : Unit {
+                body intrinsic;
+            }
+        }
+        "},
+        None,
+        &expect![[r#"
+            %Result = type opaque
+            %Qubit = type opaque
+
+            define void @ENTRYPOINT__main() #0 {
+              call void @MySecondCustomGate(%Qubit* inttoptr (i64 0 to %Qubit*), %Qubit* inttoptr (i64 1 to %Qubit*))
+              call void @MyCustomGate(%Qubit* inttoptr (i64 0 to %Qubit*))
+              call void @MyCustomFunc(i64 42)
+              call void @__quantum__qis__mz__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*)) #1
+              call void @__quantum__qis__mz__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*)) #1
+              call void @__quantum__rt__tuple_record_output(i64 2, i8* null)
+              call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* null)
+              call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 1 to %Result*), i8* null)
+              ret void
+            }
+
+            declare void @__quantum__qis__ccx__body(%Qubit*, %Qubit*, %Qubit*)
+            declare void @__quantum__qis__cx__body(%Qubit*, %Qubit*)
+            declare void @__quantum__qis__cy__body(%Qubit*, %Qubit*)
+            declare void @__quantum__qis__cz__body(%Qubit*, %Qubit*)
+            declare void @__quantum__qis__rx__body(double, %Qubit*)
+            declare void @__quantum__qis__rxx__body(double, %Qubit*, %Qubit*)
+            declare void @__quantum__qis__ry__body(double, %Qubit*)
+            declare void @__quantum__qis__ryy__body(double, %Qubit*, %Qubit*)
+            declare void @__quantum__qis__rz__body(double, %Qubit*)
+            declare void @__quantum__qis__rzz__body(double, %Qubit*, %Qubit*)
+            declare void @__quantum__qis__h__body(%Qubit*)
+            declare void @__quantum__qis__s__body(%Qubit*)
+            declare void @__quantum__qis__s__adj(%Qubit*)
+            declare void @__quantum__qis__t__body(%Qubit*)
+            declare void @__quantum__qis__t__adj(%Qubit*)
+            declare void @__quantum__qis__x__body(%Qubit*)
+            declare void @__quantum__qis__y__body(%Qubit*)
+            declare void @__quantum__qis__z__body(%Qubit*)
+            declare void @__quantum__qis__swap__body(%Qubit*, %Qubit*)
+            declare void @__quantum__qis__mz__body(%Qubit*, %Result* writeonly) #1
+            declare void @__quantum__rt__result_record_output(%Result*, i8*)
+            declare void @__quantum__rt__array_record_output(i64, i8*)
+            declare void @__quantum__rt__tuple_record_output(i64, i8*)
+            declare void @MySecondCustomGate(%Qubit*, %Qubit*)
+            declare void @MyCustomGate(%Qubit*)
+            declare void @MyCustomFunc(i64)
 
             attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="2" "required_num_results"="2" }
             attributes #1 = { "irreversible" }
