4f17c962f091d8e4c3adeae8a10f1e201b6e3d6b
cesarzc <cesarzc@microsoft.com>
2024-02-14 16:39:13 -0800

    Add tests for binops.

---
+++ compiler/qsc_rca/tests/binops.rs
@@ -0,0 +1,128 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+pub mod common;
+
+use common::{check_last_statement_compute_propeties, write_fir_store_to_files, CompilationContext};
+use expect_test::expect;
+
+#[test]
+fn check_rca_for_bin_op_with_classical_lhs_and_classical_rhs() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(r#"1 + 1"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Classical
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
+
+#[test]
+fn check_rca_for_bin_op_with_dynamic_lhs_and_classical_rhs() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(
+        r#"
+        use q = Qubit();
+        M(q) != Zero"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicResult | UseOfDynamicBool)
+                    value_kind: Dynamic
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
+
+#[test]
+fn check_rca_for_bin_op_with_classical_lhs_and_dynamic_rhs() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(
+        r#"
+        use q = Qubit();
+        One == M(q)"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicResult | UseOfDynamicBool)
+                    value_kind: Dynamic
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
+
+#[test]
+fn check_rca_for_bin_op_with_dynamic_lhs_and_dynamic_rhs() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(
+        r#"
+        use (a, b) = (Qubit(), Qubit());
+        let (c, d) = (M(a) == Zero, M(b) == Zero);
+        c and d"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicResult | UseOfDynamicBool)
+                    value_kind: Dynamic
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
+
+#[test]
+fn check_rca_for_nested_bin_ops_with_classic_operands() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(
+        r#"
+        open Microsoft.Quantum.Math;
+        Sin(PI() / 2.0) ^ 2.0 + Cos(PI() / 2.0) ^ 2.0"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Classical
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
+
+#[test]
+fn check_rca_for_nested_bin_ops_with_a_dynamic_operand() {
+    let mut compilation_context = CompilationContext::new();
+    compilation_context.update(
+        r#"
+        use q = Qubit();
+        let i = M(q) == Zero ? 0 | 1;
+        i * 1 / 1"#);
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_propeties(
+        package_store_compute_properties,
+        &expect![
+            r#"
+            ApplicationsTable:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicResult | UseOfDynamicBool | UseOfDynamicInt)
+                    value_kind: Dynamic
+                dynamic_param_applications: <empty>"#
+        ],
+    );
+}
