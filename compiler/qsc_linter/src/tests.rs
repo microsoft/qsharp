781dd43b2c22b38e427e28b2632585b205ba98b5
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2024-02-28 11:57:15 -0800

    add unit tests

--- compiler/qsc_linter/src/tests.rs
+++ compiler/qsc_linter/src/tests.rs
@@ -1,37 +1,73 @@
+use crate::{linter::ast::run_ast_lints, Lint, LintLevel};
+use expect_test::{expect, Expect};
 use qsc_ast::{
     assigner::Assigner,
-    ast::{NodeId, Package},
+    ast::{self, NodeId},
     mut_visit::MutVisitor,
 };
-use qsc_data_structures::{language_features::LanguageFeatures, line_column};
+use qsc_data_structures::language_features::LanguageFeatures;
 
 #[test]
-fn linter() {
-    use crate::linter::ast::run_ast_lints;
-
-    let source = r"
-        namespace foo {
-            operation RunProgram(vector : Double[]) : Unit {
-                let x = ((1 + 2));
-            }
-        }
-    ";
-
-    let package = parse(source);
-    let lints = run_ast_lints(&package);
-
-    for lint in &lints {
-        let range = line_column::Range::from_span(line_column::Encoding::Utf8, source, &lint.span);
-        let chunk = get_lines(source, range);
-        for line in chunk {
-            println!("{line}");
-        }
-        println!("{lint:?}");
-    }
+fn double_parens() {
+    check_ast(
+        "let x = ((1 + 2));",
+        &expect![[r#"
+            [
+                SrcLint {
+                    source: "((1 + 2))",
+                    message: "unnecesary double parentheses",
+                    level: Warning,
+                },
+            ]
+        "#]],
+    );
 }
 
-fn parse(source: &str) -> Package {
-    let mut package = Package {
+#[test]
+fn multiple_lints() {
+    check_ast(
+        "let x = ((1 + 2)) / 0;",
+        &expect![[r#"
+        [
+            SrcLint {
+                source: "((1 + 2)) / 0",
+                message: "attempt to divide by zero",
+                level: Allow,
+            },
+            SrcLint {
+                source: "((1 + 2))",
+                message: "unnecesary double parentheses",
+                level: Warning,
+            },
+        ]
+    "#]],
+    );
+}
+
+/// Checks that the AST of the given source code generates the right Lints.
+fn check_ast(source: &str, expected: &Expect) {
+    let source = wrap_in_namespace(source);
+    let package = parse(&source);
+    let actual: Vec<SrcLint> = run_ast_lints(&package)
+        .into_iter()
+        .map(|lint| SrcLint::from(lint, &source))
+        .collect();
+    expected.assert_debug_eq(&actual);
+}
+
+/// Wraps some source code into a namespace, to make testing easier.
+fn wrap_in_namespace(source: &str) -> String {
+    format!(
+        "namespace foo {{
+        operation RunProgram(vector : Double[]) : Unit {{
+            {source}
+        }}
+    }}"
+    )
+}
+
+fn parse(source: &str) -> ast::Package {
+    let mut package = ast::Package {
         id: NodeId::FIRST,
         nodes: qsc_parse::top_level_nodes(source, LanguageFeatures::default())
             .0
@@ -45,12 +81,35 @@ fn parse(source: &str) -> Package {
     package
 }
 
-fn get_lines(source: &str, range: line_column::Range) -> Vec<&str> {
-    let mut lines_of_interest = Vec::new();
-    for (line_number, line) in source.lines().enumerate() {
-        if range.start.line as usize <= line_number && line_number <= range.end.line as usize {
-            lines_of_interest.push(line);
+/// A version of Lint that replaces the span by source code
+/// to make unit tests easier to write and verify.
+#[derive(Debug)]
+struct SrcLint {
+    source: String,
+    message: &'static str,
+    level: LintLevel,
+}
+
+impl SrcLint {
+    fn from(lint: Lint, source: &str) -> Self {
+        Self {
+            source: source[lint.span].into(),
+            message: lint.message,
+            level: lint.level,
         }
     }
-    lines_of_interest
+}
+
+impl std::fmt::Display for SrcLint {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Lint {{
+                source: {},
+                message: {},
+                level: {},
+            }}",
+            self.source, self.message, self.level
+        )
+    }
 }
