21e1eb54efcd781d74d251fb18ac2253f2f92786
Ian Davis <ian.f.davis@microsoft.com>
2024-03-12 15:43:15 -0700

    Simplifying compilation.

--- allocator/mimalloc-sys/build.rs
+++ allocator/mimalloc-sys/build.rs
@@ -5,7 +5,7 @@ use std::boxed::Box;
 use std::env;
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use cmake::Config;
 
@@ -15,37 +15,59 @@ use cmake::Config;
 static ALLOCATOR_MIMALLOC_TAG: &str = "43ce4bd7fd34bcc730c1c7471c99995597415488";
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let dst = download_mimalloc()?;
+    compile_mimalloc(&dst);
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=CMakeLists.txt");
-
-    compile_mimalloc()?;
-
     Ok(())
 }
 
-fn compile_mimalloc() -> Result<(), Box<dyn Error>> {
-    let build_dir = get_build_dir()?;
-    let mut config = Config::new(build_dir);
+// Compile mimalloc source code and link it to the crate.
+// The cc crate is used to compile the source code into a static library.
+// The cmake crate is used to download the source code and stage it in the build directory.
+// We don't use the cmake crate to compile the source code because the mimalloc build system
+// loads extra libraries, changes the name and path around, and does other things that are
+// difficult to handle. The cc crate is much simpler and more predictable.
+fn compile_mimalloc(dst: &Path) {
+    let src_dir = dst
+        .join("build")
+        .join("mimalloc-prefix")
+        .join("src")
+        .join("mimalloc");
 
-    config
-        //.build_target("mimalloc")
-        .no_build_target(true)
-        .define("CMAKE_BUILD_TYPE", "MinSizeRel")
-        .define("MI_INSTALL_TOPLEVEL", "ON")
-        .define("MI_OVERRIDE", "OFF")
-        .define("MI_WIN_REDIRECT", "OFF")
-        .env("ALLOCATOR_MIMALLOC_TAG", ALLOCATOR_MIMALLOC_TAG)
-        .static_crt(true)
-        .very_verbose(true);
+    let mut build = cc::Build::new();
 
-    let dst = config.build();
+    build.include(src_dir.join("include"));
+    build.include(src_dir.join("src"));
+    build.file(src_dir.join("src/static.c"));
+
+    if build.get_compiler().is_like_msvc() {
+        build.cpp(true);
+    }
+
+    build.compile("mimalloc");
+
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib").display()
     );
     println!("cargo:rustc-link-lib=static=mimalloc");
+}
 
-    Ok(())
+// Use cmake to download mimallloc source code and stage
+// it in the build directory.
+fn download_mimalloc() -> Result<PathBuf, Box<dyn Error>> {
+    let build_dir = get_build_dir()?;
+    let mut config = Config::new(build_dir);
+
+    config
+        .no_build_target(true)
+        .env("ALLOCATOR_MIMALLOC_TAG", ALLOCATOR_MIMALLOC_TAG)
+        .very_verbose(true);
+
+    let dst = config.build();
+
+    Ok(dst)
 }
 
 fn get_build_dir() -> Result<PathBuf, Box<dyn Error>> {
