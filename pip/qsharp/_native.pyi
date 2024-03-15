d6a8f8a71a71bf80168e846c940674595c413a5b
Mine Starks <16928427+minestarks@users.noreply.github.com>
2024-03-15 14:48:04 -0700

    reformat Python files with Black 24.2.0 (#1266)
    ```
    black .
    ```
    
    The extensive changes are mostly due to the 2024 updates to
    [Black](https://black.readthedocs.io/en/stable/change_log.html#highlights).

--- pip/qsharp/_native.pyi
+++ pip/qsharp/_native.pyi
@@ -45,6 +45,7 @@ class Interpreter:
         :param list_directory: A function that lists the contents of a directory.
         """
         ...
+
     def interpret(self, input: str, output_fn: Callable[[Output], None]) -> Any:
         """
         Interprets Q# source code.
@@ -57,9 +58,8 @@ class Interpreter:
         :raises QSharpError: If there is an error interpreting the input.
         """
         ...
-    def run(
-        self, entry_expr: str, output_fn: Callable[[Output], None]
-    ) -> Any:
+
+    def run(self, entry_expr: str, output_fn: Callable[[Output], None]) -> Any:
         """
         Runs the given Q# expression with an independent instance of the simulator.
 
@@ -71,6 +71,7 @@ class Interpreter:
         :raises QSharpError: If there is an error interpreting the input.
         """
         ...
+
     def qir(self, entry_expr: str) -> str:
         """
         Generates QIR from Q# source code.
@@ -80,6 +81,7 @@ class Interpreter:
         :returns qir: The QIR string.
         """
         ...
+
     def estimate(self, entry_expr: str, params: str) -> str:
         """
         Estimates resources for Q# source code.
@@ -90,6 +92,7 @@ class Interpreter:
         :returns resources: The estimated resources.
         """
         ...
+
     def set_quantum_seed(self, seed: Optional[int]) -> None:
         """
         Sets the seed for the quantum random number generator.
@@ -98,6 +101,7 @@ class Interpreter:
             the seed will be generated from entropy.
         """
         ...
+
     def set_classical_seed(self, seed: Optional[int]) -> None:
         """
         Sets the seed for the classical random number generator.
@@ -106,6 +110,7 @@ class Interpreter:
             the seed will be generated from entropy.
         """
         ...
+
     def dump_machine(self) -> StateDumpData:
         """
         Returns the sparse state vector of the simulator as a StateDump object.
