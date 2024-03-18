b1e7c8602589dfff54fe1dce7b38751a6b46bb18
Mine Starks <16928427+minestarks@users.noreply.github.com>
2024-03-18 21:17:09 +0000

    update colors in css

--- npm/ux/index.ts
+++ npm/ux/index.ts
@@ -4,6 +4,7 @@
 // By importing the CSS here, esbuild will by default bundle it up and copy it
 // to a CSS file adjacent to the JS bundle and with the same name.
 import "./qsharp-ux.css";
+import "./qsharp-circuit.css";
 
 export { CreateSingleEstimateResult, type ReData } from "./data.js";
 export { Histogram } from "./histogram.js";
