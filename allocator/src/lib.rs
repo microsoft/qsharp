9b1eeaad151bd5fb5fcc46e677571accc4dcf576
Stefan J. Wernli <swernli@microsoft.com>
2024-03-19 15:45:03 -0700

    Avoid mimalloc on Windows arm

--- allocator/src/lib.rs
+++ allocator/src/lib.rs
@@ -1,14 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#[cfg(not(target_family = "wasm"))]
+#[cfg(all(
+    not(target_family = "wasm"),
+    not(all(system = "windows", target_arch = "arm"))
+))]
 pub mod mimalloc;
 
 /// Declare a global allocator if the platform supports it.
 #[macro_export]
 macro_rules! assign_global {
     () => {
-        #[cfg(not(target_family = "wasm"))]
+        #[cfg(all(
+            not(target_family = "wasm"),
+            not(all(system = "windows", target_arch = "arm"))
+        ))]
         #[global_allocator]
         static GLOBAL: allocator::mimalloc::Mimalloc = allocator::mimalloc::Mimalloc;
     };
