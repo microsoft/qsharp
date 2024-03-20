713c241aa68d174be4dd261ce49929bbf2f0c503
Stefan J. Wernli <swernli@microsoft.com>
2024-03-19 23:18:28 -0700

    Update pattern

--- allocator/src/lib.rs
+++ allocator/src/lib.rs
@@ -1,20 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#[cfg(all(
-    not(target_family = "wasm"),
-    not(all(system = "windows", target_arch = "aarch64"))
-))]
+#[cfg(not(any(
+    target_family = "wasm",
+    all(target_family = "windows", target_arch = "aarch64")
+)))]
 pub mod mimalloc;
 
 /// Declare a global allocator if the platform supports it.
 #[macro_export]
 macro_rules! assign_global {
     () => {
-        #[cfg(all(
-            not(target_family = "wasm"),
-            not(all(system = "windows", target_arch = "aarch64"))
-        ))]
+        #[cfg(not(any(
+            target_family = "wasm",
+            all(target_family = "windows", target_arch = "aarch64")
+        )))]
         #[global_allocator]
         static GLOBAL: allocator::mimalloc::Mimalloc = allocator::mimalloc::Mimalloc;
     };
