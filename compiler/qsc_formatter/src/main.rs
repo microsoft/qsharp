2735f0879e998e9f447a980a72908de7ff444844
Scott Carda <Scott.Carda@microsoft.com>
2024-03-28 15:40:00 -0700

    skipped files

--- compiler/qsc_formatter/src/main.rs
+++ compiler/qsc_formatter/src/main.rs
@@ -3,6 +3,7 @@
 
 use std::{
     env,
+    fmt::Display,
     path::{Path, PathBuf},
 };
 mod formatter;
@@ -26,43 +27,158 @@ struct Cli {
     write: bool,
 }
 
-fn main() -> Result<(), i32> {
-    let cli = Cli::parse();
-    let mut file_count = 0;
-    let mut changed_files: Vec<String> = vec![];
+struct FileWalker {
+    root: PathBuf,
+    is_write: bool,
+    is_recursive: bool,
+    file_count: i32,
+    changed_files: Vec<String>,
+    skipped_files: Vec<String>,
+}
 
-    assert!(cli.path.exists(), "Given path can not found.");
-    if !cli.path.is_dir() && is_path_qs(&cli.path) {
-        panic!("Given path is not a folder or Q# file.");
+impl FileWalker {
+    pub fn new() -> Self {
+        let cli = Cli::parse();
+        Self {
+            root: cli.path,
+            is_write: cli.write,
+            is_recursive: cli.recursive,
+            file_count: 0,
+            changed_files: vec![],
+            skipped_files: vec![],
+        }
     }
 
-    format_file_or_dir(
-        &cli.path,
-        cli.write,
-        cli.recursive,
-        &mut file_count,
-        &mut changed_files,
-    );
+    fn format_from_root(&mut self) {
+        self.format_file_or_dir(&self.root.clone());
+    }
 
-    println!("Ran against {file_count} files.");
-    if cli.write {
-        println!("Updated {} files:", changed_files.len());
-        for f in changed_files.iter() {
+    fn format_file_or_dir(&mut self, path: &Path) {
+        use OutputFormatting::*;
+
+        if path.is_dir() {
+            let items = match path.read_dir() {
+                Ok(items) => items.flatten(),
+                Err(e) => {
+                    println!("\t{Skip}Could not read from directory: {e}{Reset}");
+                    self.skipped_files.push(path.display().to_string());
+                    return;
+                }
+            };
+            for item in items {
+                let subpath = &item.path();
+                if subpath.is_dir() && self.is_recursive {
+                    self.format_file_or_dir(subpath);
+                } else if is_path_qs(subpath) {
+                    self.format_file(subpath);
+                }
+            }
+        } else if is_path_qs(path) {
+            self.format_file(path);
+        }
+    }
+
+    fn format_file(&mut self, path: &Path) {
+        use OutputFormatting::*;
+
+        println!("Formatting {}", path.display());
+
+        let file_as_string = match std::fs::read_to_string(path) {
+            Ok(s) => s,
+            Err(e) => {
+                println!("\t{Skip}Could not read from file: {e}{Reset}");
+                self.skipped_files.push(path.display().to_string());
+                return; // don't count file in file_count
+            }
+        };
+        if self.is_write {
+            let formatted = format_str(&file_as_string);
+            if file_as_string != formatted {
+                match std::fs::write(path, formatted) {
+                    Ok(_) => {
+                        self.changed_files.push(path.display().to_string());
+                    }
+                    Err(e) => {
+                        println!("\t{Skip}Could not write to file: {e}{Reset}");
+                        self.skipped_files.push(path.display().to_string());
+                        return; // don't count file in file_count
+                    }
+                }
+            }
+        } else if !calculate_format_edits(&file_as_string).is_empty() {
+            self.changed_files.push(path.display().to_string());
+        }
+        self.file_count += 1;
+    }
+}
+
+enum OutputFormatting {
+    Error,
+    Skip,
+    Passing,
+    Reset,
+}
+
+impl Display for OutputFormatting {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            OutputFormatting::Error => "\x1B[1;91m", // Bold, Bright Red
+            OutputFormatting::Passing => "\x1B[1;92m", // Bold, Bright Green
+            OutputFormatting::Skip => "\x1B[1;93m",  // Bold, Bright Yellow
+            OutputFormatting::Reset => "\x1B[0m",
+        };
+        write!(f, "{output}")
+    }
+}
+
+fn main() -> Result<(), String> {
+    use OutputFormatting::*;
+
+    let mut file_walker = FileWalker::new();
+    if !file_walker.root.exists() {
+        return Err("Given path can not found.".to_string());
+    }
+    if !file_walker.root.is_dir() && is_path_qs(&file_walker.root) {
+        return Err("Given path is not a folder or Q# file.".to_string());
+    }
+
+    file_walker.format_from_root();
+
+    println!("Ran against {} files.", file_walker.file_count);
+
+    if !file_walker.skipped_files.is_empty() {
+        println!("{Skip}Skipped {} files:", file_walker.skipped_files.len());
+        for f in file_walker.skipped_files.iter() {
+            println!("\t{f}");
+        }
+        print!("{Reset}");
+    }
+
+    if file_walker.is_write {
+        println!("Updated {} files:", file_walker.changed_files.len());
+        for f in file_walker.changed_files.iter() {
             println!("\t{f}");
         }
         Ok(())
-    } else if !changed_files.is_empty() {
+    } else if !file_walker.changed_files.is_empty() {
         println!(
-            "\x1B[1;91m{} files are in need of formatting:",
-            changed_files.len()
+            "{Error}{} files are in need of formatting:",
+            file_walker.changed_files.len()
         );
-        for f in changed_files.iter() {
+        for f in file_walker.changed_files.iter() {
             println!("\t{f}");
         }
-        println!("Run the formatter to correct formatting for the above files.\x1B[0m");
-        Err(1)
+        println!("Run the formatter to correct formatting for the above files.{Reset}");
+        Err("Files are not formatted.".to_string())
+    } else if !file_walker.skipped_files.is_empty() {
+        println!(
+            "{Skip}{} files are correctly formatted. {} files skipped.{Reset}",
+            file_walker.file_count,
+            file_walker.skipped_files.len()
+        );
+        Ok(())
     } else {
-        println!("\x1B[1;92mAll files are correctly formatted.\x1B[0m");
+        println!("{Passing}All files are correctly formatted.{Reset}");
         Ok(())
     }
 }
@@ -76,46 +192,4 @@ fn is_path_qs(path: &Path) -> bool {
         }
     }
     false
-}
-
-fn format_file_or_dir(
-    path: &Path,
-    is_write: bool,
-    is_recursive: bool,
-    file_count: &mut i32,
-    changed_files: &mut Vec<String>,
-) {
-    if path.is_dir() {
-        for item in path
-            .read_dir()
-            .expect("unable to read from directory")
-            .flatten()
-        {
-            let subpath = item.path();
-            if subpath.is_dir() && is_recursive {
-                format_file_or_dir(&subpath, is_write, is_recursive, file_count, changed_files);
-            } else if is_path_qs(&subpath) {
-                println!("Formatting {}", subpath.display());
-                format_file(&subpath, is_write, changed_files);
-                *file_count += 1;
-            }
-        }
-    } else if is_path_qs(path) {
-        println!("Formatting {}", path.display());
-        format_file(path, is_write, changed_files);
-        *file_count += 1;
-    }
-}
-
-fn format_file(path: &Path, write: bool, changed_files: &mut Vec<String>) {
-    let file_as_string = std::fs::read_to_string(path).expect("file not found");
-    if write {
-        let formatted = format_str(&file_as_string);
-        if file_as_string != formatted {
-            std::fs::write(path, formatted).expect("could not write to file");
-            changed_files.push(path.display().to_string());
-        }
-    } else if !calculate_format_edits(&file_as_string).is_empty() {
-        changed_files.push(path.display().to_string());
-    }
 }
