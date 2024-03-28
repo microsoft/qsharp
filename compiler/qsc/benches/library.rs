d3bbc70dd1b4a27265cfd0f2261d629b6e0c5850
Stefan J. Wernli <swernli@microsoft.com>
2024-03-28 13:38:56 -0700

    Fix bench to actually include core

--- compiler/qsc/benches/library.rs
+++ compiler/qsc/benches/library.rs
@@ -9,8 +9,10 @@ use qsc_frontend::compile::{PackageStore, RuntimeCapabilityFlags};
 
 pub fn library(c: &mut Criterion) {
     c.bench_function("Core + Standard library compilation", |b| {
-        let store = PackageStore::new(compile::core());
-        b.iter(|| compile::std(&store, RuntimeCapabilityFlags::all()));
+        b.iter(|| {
+            let store = PackageStore::new(compile::core());
+            compile::std(&store, RuntimeCapabilityFlags::all())
+        });
     });
 }
 
