545212eda6ef7bf6337040fdc7baaab17466d674
Stefan J. Wernli <swernli@microsoft.com>
2024-04-03 14:46:50 -0700

    RIR SSA check pass
    This pass checks that the RIR is in Single Static Assignment (SSA) form. The check verifies that each variable is assigned exactly once before it is used, and that phi nodes are used correctly.
    
    The change also updates how successors are calculated for branch instructions that have constant conditions, and updates the tests that used constant conditions to use dynamic bool values instead.

--- compiler/qsc_rir/src/passes/build_dominator_graph/tests.rs
+++ compiler/qsc_rir/src/passes/build_dominator_graph/tests.rs
@@ -7,30 +7,16 @@ use expect_test::expect;
 use qsc_data_structures::index_map::IndexMap;
 
 use crate::{
+    builder::new_program,
     passes::remap_block_ids,
     rir::{
-        Block, BlockId, Callable, CallableId, CallableType, Instruction, Literal, Operand, Program,
+        Block, BlockId, Callable, CallableId, CallableType, Instruction, Operand, Ty, Variable,
+        VariableId,
     },
+    utils::build_predecessors_map,
 };
 
 use super::build_dominator_graph;
-
-/// Creates a new program with a single, entry callable that has block 0 as its body.
-fn new_program() -> Program {
-    let mut program = Program::new();
-    program.entry = CallableId(0);
-    program.callables.insert(
-        CallableId(0),
-        Callable {
-            name: "main".to_string(),
-            input_type: Vec::new(),
-            output_type: None,
-            body: Some(BlockId(0)),
-            call_type: CallableType::Regular,
-        },
-    );
-    program
-}
 
 fn display_dominator_graph(doms: &IndexMap<BlockId, BlockId>) -> String {
     let mut result = String::new();
@@ -51,7 +37,8 @@ fn test_dominator_graph_single_block_dominates_itself() {
         .insert(BlockId(0), Block(vec![Instruction::Return]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -73,7 +60,8 @@ fn test_dominator_graph_sequential_blocks_dominated_by_predecessor() {
         .insert(BlockId(2), Block(vec![Instruction::Return]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -86,13 +74,38 @@ fn test_dominator_graph_sequential_blocks_dominated_by_predecessor() {
 #[test]
 fn test_dominator_graph_branching_blocks_dominated_by_common_predecessor() {
     let mut program = new_program();
-    program
-        .blocks
-        .insert(BlockId(0), Block(vec![Instruction::Jump(BlockId(1))]));
+    program.callables.insert(
+        CallableId(1),
+        Callable {
+            name: "dynamic_bool".to_string(),
+            input_type: Vec::new(),
+            output_type: Some(Ty::Boolean),
+            body: None,
+            call_type: CallableType::Regular,
+        },
+    );
+
+    program.blocks.insert(
+        BlockId(0),
+        Block(vec![
+            Instruction::Call(
+                CallableId(1),
+                Vec::new(),
+                Some(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+            ),
+            Instruction::Jump(BlockId(1)),
+        ]),
+    );
     program.blocks.insert(
         BlockId(1),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(2),
             BlockId(3),
         )]),
@@ -105,7 +118,8 @@ fn test_dominator_graph_branching_blocks_dominated_by_common_predecessor() {
         .insert(BlockId(3), Block(vec![Instruction::Return]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -127,7 +141,8 @@ fn test_dominator_graph_infinite_loop() {
         .insert(BlockId(1), Block(vec![Instruction::Jump(BlockId(1))]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -139,13 +154,37 @@ fn test_dominator_graph_infinite_loop() {
 #[test]
 fn test_dominator_graph_branch_and_loop() {
     let mut program = new_program();
-    program
-        .blocks
-        .insert(BlockId(0), Block(vec![Instruction::Jump(BlockId(1))]));
+    program.callables.insert(
+        CallableId(1),
+        Callable {
+            name: "dynamic_bool".to_string(),
+            input_type: Vec::new(),
+            output_type: Some(Ty::Boolean),
+            body: None,
+            call_type: CallableType::Regular,
+        },
+    );
+    program.blocks.insert(
+        BlockId(0),
+        Block(vec![
+            Instruction::Call(
+                CallableId(1),
+                Vec::new(),
+                Some(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+            ),
+            Instruction::Jump(BlockId(1)),
+        ]),
+    );
     program.blocks.insert(
         BlockId(1),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(2),
             BlockId(3),
         )]),
@@ -161,7 +200,8 @@ fn test_dominator_graph_branch_and_loop() {
         .insert(BlockId(4), Block(vec![Instruction::Return]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -179,6 +219,17 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
     // by Cooper, Harvey, and Kennedy and uses the node numbering from the paper. However, the resulting dominator graph
     // is different due to the numbering of the blocks, such that each block is numbered in reverse postorder.
     let mut program = new_program();
+    program.callables.insert(
+        CallableId(1),
+        Callable {
+            name: "dynamic_bool".to_string(),
+            input_type: Vec::new(),
+            output_type: Some(Ty::Boolean),
+            body: None,
+            call_type: CallableType::Regular,
+        },
+    );
+
     program
         .callables
         .get_mut(CallableId(0))
@@ -186,11 +237,24 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
         .body = Some(BlockId(6));
     program.blocks.insert(
         BlockId(6),
-        Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
-            BlockId(5),
-            BlockId(4),
-        )]),
+        Block(vec![
+            Instruction::Call(
+                CallableId(1),
+                Vec::new(),
+                Some(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+            ),
+            Instruction::Branch(
+                Operand::Variable(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+                BlockId(5),
+                BlockId(4),
+            ),
+        ]),
     );
     program
         .blocks
@@ -198,7 +262,10 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
     program.blocks.insert(
         BlockId(4),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(2),
             BlockId(3),
         )]),
@@ -209,7 +276,10 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
     program.blocks.insert(
         BlockId(2),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(3),
             BlockId(1),
         )]),
@@ -219,7 +289,8 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
         .insert(BlockId(3), Block(vec![Instruction::Jump(BlockId(2))]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
@@ -235,18 +306,45 @@ fn test_dominator_graph_complex_structure_only_dominated_by_entry() {
 #[test]
 fn test_dominator_graph_with_node_having_many_predicates() {
     let mut program = new_program();
+    program.callables.insert(
+        CallableId(1),
+        Callable {
+            name: "dynamic_bool".to_string(),
+            input_type: Vec::new(),
+            output_type: Some(Ty::Boolean),
+            body: None,
+            call_type: CallableType::Regular,
+        },
+    );
+
     program.blocks.insert(
         BlockId(0),
-        Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
-            BlockId(1),
-            BlockId(2),
-        )]),
+        Block(vec![
+            Instruction::Call(
+                CallableId(1),
+                Vec::new(),
+                Some(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+            ),
+            Instruction::Branch(
+                Operand::Variable(Variable {
+                    variable_id: VariableId(0),
+                    ty: Ty::Boolean,
+                }),
+                BlockId(1),
+                BlockId(2),
+            ),
+        ]),
     );
     program.blocks.insert(
         BlockId(1),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(3),
             BlockId(4),
         )]),
@@ -254,7 +352,10 @@ fn test_dominator_graph_with_node_having_many_predicates() {
     program.blocks.insert(
         BlockId(2),
         Block(vec![Instruction::Branch(
-            Operand::Literal(Literal::Bool(true)),
+            Operand::Variable(Variable {
+                variable_id: VariableId(0),
+                ty: Ty::Boolean,
+            }),
             BlockId(5),
             BlockId(6),
         )]),
@@ -276,7 +377,8 @@ fn test_dominator_graph_with_node_having_many_predicates() {
         .insert(BlockId(7), Block(vec![Instruction::Return]));
 
     remap_block_ids(&mut program);
-    let doms = build_dominator_graph(&program);
+    let preds = build_predecessors_map(&program);
+    let doms = build_dominator_graph(&program, &preds);
 
     expect![[r#"
         Block 0 dominated by block 0,
--- compiler/qsc_rir/src/utils.rs
+++ compiler/qsc_rir/src/utils.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use crate::rir::{Block, BlockId, Instruction, Program};
+use crate::rir::{Block, BlockId, Instruction, Literal, Operand, Program};
 use qsc_data_structures::index_map::IndexMap;
 use rustc_hash::FxHashSet;
 
@@ -15,12 +15,14 @@ pub fn get_block_successors(block: &Block) -> Vec<BlockId> {
         .last()
         .expect("block should have at least one instruction")
     {
-        Instruction::Jump(target) => {
-            successors.push(*target);
-        }
-        Instruction::Branch(_, target1, target2) => {
+        Instruction::Branch(Operand::Variable(_), target1, target2) => {
             successors.push(*target1);
             successors.push(*target2);
+        }
+        Instruction::Jump(target)
+        | Instruction::Branch(Operand::Literal(Literal::Bool(true)), target, _)
+        | Instruction::Branch(Operand::Literal(Literal::Bool(false)), _, target) => {
+            successors.push(*target);
         }
         _ => {}
     }
