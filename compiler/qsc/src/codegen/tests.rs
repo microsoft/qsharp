8170faea23da24b1608969cad93049002037ed91
Ian Davis <ian.f.davis@microsoft.com>
2024-04-17 08:27:27 -0700

    More simple tests

--- compiler/qsc/src/codegen/tests.rs
+++ compiler/qsc/src/codegen/tests.rs
@@ -3,6 +3,11 @@
 
 mod samples {
     pub(crate) const BELL_STATE: &str = include_str!("../../../../samples/algorithms/BellState.qs");
+    pub(crate) const BERNSTEIN_VAZIRANI_NISQ: &str =
+        include_str!("../../../../samples/algorithms/BernsteinVaziraniNISQ.qs");
+    pub(crate) const DEUTSCH_JOZSA_NISQ: &str =
+        include_str!("../../../../samples/algorithms/DeutschJozsaNISQ.qs");
+    pub(crate) const GROVER: &str = include_str!("../../../../samples/algorithms/Grover.qs");
 }
 
 mod base_profile {
@@ -12,11 +17,75 @@ mod base_profile {
     use crate::codegen::get_qir;
 
     #[test]
+    fn simple() -> Result<(), String> {
+        let source = r#"namespace Test {
+            @EntryPoint()
+            operation Test() : Result {
+                use q = Qubit();
+                H(q);
+                MResetZ(q)
+            }
+        }"#;
+        let sources = SourceMap::new([("test.qs".into(), source.into())], None);
+        let language_features = LanguageFeatures::default();
+        let capabilities = TargetCapabilityFlags::empty();
+
+        let qir = get_qir(sources, language_features, capabilities)?;
+        assert!(qir.contains("ENTRYPOINT"));
+        Ok(())
+    }
+
+    #[test]
     fn bell_state_sample() -> Result<(), String> {
         let sources = SourceMap::new(
             [("BellState.qs".into(), super::samples::BELL_STATE.into())],
             None,
         );
+        let language_features = LanguageFeatures::default();
+        let capabilities = TargetCapabilityFlags::empty();
+
+        let qir = get_qir(sources, language_features, capabilities)?;
+        assert!(qir.contains("ENTRYPOINT"));
+        Ok(())
+    }
+
+    #[test]
+    fn bernstein_vazirani_nisq_sample() -> Result<(), String> {
+        let sources = SourceMap::new(
+            [(
+                "BernsteinVaziraniNISQ.qs".into(),
+                super::samples::BERNSTEIN_VAZIRANI_NISQ.into(),
+            )],
+            None,
+        );
+        let language_features = LanguageFeatures::default();
+        let capabilities = TargetCapabilityFlags::empty();
+
+        let qir = get_qir(sources, language_features, capabilities)?;
+        assert!(qir.contains("ENTRYPOINT"));
+        Ok(())
+    }
+
+    #[test]
+    fn deutsch_jozsa_nisq_sample() -> Result<(), String> {
+        let sources = SourceMap::new(
+            [(
+                "DeutschJozsaNISQ.qs".into(),
+                super::samples::DEUTSCH_JOZSA_NISQ.into(),
+            )],
+            None,
+        );
+        let language_features = LanguageFeatures::default();
+        let capabilities = TargetCapabilityFlags::empty();
+
+        let qir = get_qir(sources, language_features, capabilities)?;
+        assert!(qir.contains("ENTRYPOINT"));
+        Ok(())
+    }
+
+    #[test]
+    fn grover_sample() -> Result<(), String> {
+        let sources = SourceMap::new([("Grover.qs".into(), super::samples::GROVER.into())], None);
         let language_features = LanguageFeatures::default();
         let capabilities = TargetCapabilityFlags::empty();
 
