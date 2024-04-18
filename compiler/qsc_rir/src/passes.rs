4db51ce5e13d8e6586dd4daf9c7c014f205745e0
Stefan J. Wernli <swernli@microsoft.com>
2024-04-18 14:50:54 -0700

    Add RIR type check pass (#1397)
    This change introduces a type check pass for RIR that verifies and
    panics on any mismatches found in the types. It adds the check to the
    default passes and the SSA transform tests.

--- compiler/qsc_rir/src/passes.rs
+++ compiler/qsc_rir/src/passes.rs
@@ -7,6 +7,7 @@ mod reindex_qubits;
 mod remap_block_ids;
 mod ssa_check;
 mod ssa_transform;
+mod type_check;
 mod unreachable_code_check;
 
 use build_dominator_graph::build_dominator_graph;
@@ -15,6 +16,7 @@ use reindex_qubits::reindex_qubits;
 use remap_block_ids::remap_block_ids;
 use ssa_check::check_ssa_form;
 use ssa_transform::transform_to_ssa;
+pub use type_check::check_types;
 pub use unreachable_code_check::check_unreachable_code;
 
 use crate::{rir::Program, utils::build_predecessors_map};
@@ -27,11 +29,14 @@ use crate::{rir::Program, utils::build_predecessors_map};
 /// - Checking that the program is in SSA form
 pub fn check_and_transform(program: &mut Program) {
     check_unreachable_code(program);
+    check_types(program);
     remap_block_ids(program);
     let preds = build_predecessors_map(program);
     transform_to_ssa(program, &preds);
     let doms = build_dominator_graph(program, &preds);
     check_ssa_form(program, &preds, &doms);
+    check_unreachable_code(program);
+    check_types(program);
 }
 
 /// Run the RIR passes that are necessary for targets with no mid-program measurement.
