c9e3ff56f2f4b022f510014f8e13f9c444016a9b
Dmitry Vasilevsky <dmitryv@microsoft.com>
2024-05-09 11:14:05 -0700

    Moved concatenation to documentation component

--- vscode/src/documentation.ts
+++ vscode/src/documentation.ts
@@ -9,11 +9,11 @@ import { loadProject } from "./projectSystem";
 import { sendMessageToPanel } from "./webviewPanel";
 
 export async function showDocumentationCommand(extensionUri: Uri) {
-  // Reveal panel an show 'Loading...' for immediate feedback.
+  // Reveal panel and show 'Loading...' for immediate feedback.
   sendMessageToPanel(
     "documentation", // This is needed to route the message to the proper panel
-    true,
-    null,
+    true, // Reveal panel
+    null, // With no message
   );
   const editor = window.activeTextEditor;
   if (!editor || !isQsharpDocument(editor.document)) {
@@ -24,7 +24,7 @@ export async function showDocumentationCommand(extensionUri: Uri) {
   const program = await loadProject(docUri);
   const targetProfile = getTarget();
 
-  // Get std library documentation from compiler.
+  // Get API documentation from compiler.
   const compilerWorkerScriptPath = Uri.joinPath(
     extensionUri,
     "./out/compilerWorker.js",
@@ -36,19 +36,14 @@ export async function showDocumentationCommand(extensionUri: Uri) {
     program.languageFeatures,
   );
 
-  // Concatenate all documentation.
-  // The following adds an empty line and a horizontal line
-  // between documentation for different functions.
-  const content = documentation.join("<br/>\n\n---\n\n");
-
   const message = {
     command: "showDocumentationCommand", // This is handled in webview.tsx onMessage
-    contentToRender: content,
+    contentToRender: documentation,
   };
 
   sendMessageToPanel(
     "documentation", // This is needed to route the message to the proper panel
-    true,
-    message,
+    true, // Reveal panel
+    message, // And ask it to display documentation
   );
 }
--- vscode/src/webview/docview.tsx
+++ vscode/src/webview/docview.tsx
@@ -3,6 +3,12 @@
 
 import { Markdown } from "qsharp-lang/ux";
 
-export function DocumentationView(props: { contentToRender: string }) {
-  return <Markdown markdown={props.contentToRender} />;
+export function DocumentationView(props: { contentFragments: string[] }) {
+
+  // Concatenate all documentation.
+  // The following adds an empty line and a horizontal line
+  // between documentation for different functions.
+  const contentToRender = props.contentFragments.join("<br/>\n\n---\n\n");
+
+  return <Markdown markdown={contentToRender} />;
 }
