c2ed383766cb2c9ea71dad0a44d943246c4123bc
Stefan J. Wernli <swernli@microsoft.com>
2024-05-13 22:24:18 -0700

    Fix RIR transform for large unrolled loops, chained stores across branches
    This fixes two RIR transformation bugs:
    
     - Avoid revisiting same nodes back to back in node remapping pass, which can cause exponential increase in memory usage for programs with large loops containing nested branching.
     - Use variable map for operands in phi node arguments to avoid orphaned nodes when value comes from a store of a stored variable across branches.

--- compiler/qsc_rir/src/passes/remap_block_ids.rs
+++ compiler/qsc_rir/src/passes/remap_block_ids.rs
@@ -43,6 +43,17 @@ pub fn remap_block_ids(program: &mut Program) {
         }
         block_id_map.push(block_id);
 
+        let successors = get_block_successors(program.get_block(block_id));
+        if blocks_to_visit.len() >= successors.len()
+            && blocks_to_visit
+                .iter()
+                .skip(blocks_to_visit.len() - successors.len())
+                .eq(successors.iter())
+        {
+            // All successors are already in the queue in same order, so avoid adding them and reprocessing
+            // the same blocks back-to-back.
+            continue;
+        }
         blocks_to_visit.extend(get_block_successors(program.get_block(block_id)));
     }
 
