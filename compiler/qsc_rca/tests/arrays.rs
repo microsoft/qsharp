microsoft/qsharp

commit cc88c80f7db1585611eb06743a594b2dfd979cd9

Update RCA for mutable `Result`, array, and tuple variables (#1571)
This change strengthens RCA checks around use of mutable `Result`,
array, and tuple variables in a dynamic context to make them consistent
with how we verify the use of other dynamic value types like `Int` and
`Double`. This gives users compile time feedback when updating a mutable
value in a dynamic context will not produce QIR, rather than a codegen
failure with `an unexpected error occurred related to: re-assignment
within a dynamic branch is unsupported for type <type>` error.

compiler/qsc_rca/src/core.rs
@@ -176,9 +176,21 @@ impl<'a> Analyzer<'a> {
         // Since this is an assignment, the compute kind of the local variable (array var expression) needs to be updated.
         // The compute kind of the update is determined by the runtime features of the replacement value expression.
         let application_instance = self.get_current_application_instance();
-        let replacement_value_compute_kind =
+        let mut replacement_value_compute_kind =
             *application_instance.get_expr_compute_kind(replacement_value_expr_id);
-        let default_value_kind = ValueKind::Array(RuntimeKind::Static, RuntimeKind::Static);
+
+        let mut default_value_kind = ValueKind::Array(RuntimeKind::Static, RuntimeKind::Static);
+        // If we are within a dynamic scope, the compute kind of the assign index expression is dynamic and an additional
+        // runtime feature is used to mark the array itself as dynamically sized.
+        if !application_instance.active_dynamic_scopes.is_empty() {
+            default_value_kind = ValueKind::Array(RuntimeKind::Dynamic, RuntimeKind::Dynamic);
+            replacement_value_compute_kind =
+                replacement_value_compute_kind.aggregate(ComputeKind::new_with_runtime_features(
+                    RuntimeFeatureFlags::UseOfDynamicallySizedArray,
+                    default_value_kind,
+                ));
+        }
+
         let mut updated_compute_kind = ComputeKind::Classical;
         updated_compute_kind = updated_compute_kind
             .aggregate_runtime_features(replacement_value_compute_kind, default_value_kind);
@@ -684,12 +696,33 @@ impl<'a> Analyzer<'a> {
                 application_instance.get_expr_compute_kind(e).is_dynamic()
             });
         if is_any_sub_expr_dynamic {
-            let dynamic_value_kind = ValueKind::new_dynamic_from_type(expr_type);
-            let dynamic_runtime_features =
+            let dynamic_value_kind = if matches!(expr_type, Ty::Array(..)) {
+                // An array coming from a dynamic conditional should be treated as dynamic in length
+                // and content.
+                ValueKind::Array(
+                    RuntimeKind::Dynamic,
+                    if condition_expr_compute_kind.is_dynamic() {
+                        RuntimeKind::Dynamic
+                    } else {
+                        RuntimeKind::Static
+                    },
+                )
+            } else {
+                ValueKind::new_dynamic_from_type(expr_type)
+            };
+            let mut dynamic_runtime_features =
                 derive_runtime_features_for_value_kind_associated_to_type(
                     dynamic_value_kind,
                     expr_type,
                 );
+            if condition_expr_compute_kind.is_dynamic() {
+                if is_any_result(expr_type) {
+                    dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicResult;
+                }
+                if matches!(expr_type, Ty::Tuple(tup) if !tup.is_empty()) {
+                    dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicTuple;
+                }
+            }
             let dynamic_compute_kind = ComputeKind::Quantum(QuantumProperties {
                 runtime_features: dynamic_runtime_features,
                 value_kind: dynamic_value_kind,
@@ -1492,13 +1525,11 @@ impl<'a> Analyzer<'a> {
                             dynamic_value_kind,
                             local_type,
                         );
-                    if matches!(local_type, Ty::Array(..)) {
-                        // For arrays updated in a dynamic context, we also need to include the runtime feature
-                        // of dynamic arrays and change the value kind.
-                        dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicallySizedArray;
-                        dynamic_value_kind =
-                            ValueKind::Array(RuntimeKind::Dynamic, RuntimeKind::Dynamic);
-                    }
+                    update_features_for_type(
+                        local_type,
+                        &mut dynamic_runtime_features,
+                        &mut dynamic_value_kind,
+                    );
                     let dynamic_compute_kind = ComputeKind::new_with_runtime_features(
                         dynamic_runtime_features,
                         dynamic_value_kind,
@@ -1564,6 +1595,33 @@ impl<'a> Analyzer<'a> {
             }
             _ => panic!("expected a local variable or a tuple"),
         }
+    }
+}
+
+fn update_features_for_type(
+    local_type: &Ty,
+    dynamic_runtime_features: &mut RuntimeFeatureFlags,
+    dynamic_value_kind: &mut ValueKind,
+) {
+    match local_type {
+        Ty::Array(..) => {
+            // For arrays updated in a dynamic context, we also need to include the runtime feature
+            // of dynamic arrays and change the value kind.
+            *dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicallySizedArray;
+            *dynamic_value_kind = ValueKind::Array(RuntimeKind::Dynamic, RuntimeKind::Dynamic);
+        }
+        Ty::Tuple(tup) if !tup.is_empty() => {
+            // For tuples updated in a dynamic context, we also need to include the runtime feature
+            // of dynamic tuples and change the value kind.
+            *dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicTuple;
+            *dynamic_value_kind = ValueKind::Element(RuntimeKind::Dynamic);
+        }
+        Ty::Prim(Prim::Result) => {
+            // For result types updated in a dynamic context, we need to include the runtime
+            // feature of dynamic results.
+            *dynamic_runtime_features |= RuntimeFeatureFlags::UseOfDynamicResult;
+        }
+        _ => {}
     }
 }
 
@@ -2380,4 +2438,13 @@ fn split_controls_and_input(
         remainder_expr_id = pats[1];
     }
     (controls, remainder_expr_id)
+}
+
+fn is_any_result(t: &Ty) -> bool {
+    match t {
+        Ty::Prim(Prim::Result) => true,
+        Ty::Array(t) => is_any_result(t),
+        Ty::Tuple(ts) => ts.iter().any(is_any_result),
+        _ => false,
+    }
 }

compiler/qsc_rca/tests/arrays.rs
@@ -229,6 +229,119 @@ fn check_rca_for_mutable_array_dynamically_appended() {
 }
 
 #[test]
+fn check_rca_for_mutable_array_assignment_in_static_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [0, 1];
+        use q = Qubit();
+        if false {
+            set arr = [10];
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Classical
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
+fn check_rca_for_mutable_array_assignment_in_dynamic_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [0, 1];
+        use q = Qubit();
+        if M(q) == Zero {
+            set arr = [10];
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicInt | UseOfDynamicallySizedArray)
+                    value_kind: Array(Content: Dynamic, Size: Dynamic)
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
+fn check_rca_for_immutable_array_bound_to_dynamic_array() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        use q = Qubit();
+        let arr = M(q) == One ? [0, 1] | [2, 3];
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicBool | UseOfDynamicInt | UseOfDynamicallySizedArray)
+                    value_kind: Array(Content: Dynamic, Size: Dynamic)
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
+fn check_rca_for_mutable_array_assign_index_in_classical_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [0, 1];
+        use q = Qubit();
+        if false {
+            set arr w/= 0 <- 10;
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Classical
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
+fn check_rca_for_mutable_array_assign_index_in_dynamic_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [0, 1];
+        use q = Qubit();
+        if M(q) == Zero {
+            set arr w/= 0 <- 10;
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicallySizedArray)
+                    value_kind: Array(Content: Dynamic, Size: Dynamic)
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
 fn check_rca_for_access_using_classical_index() {
     let mut compilation_context = CompilationContext::default();
     compilation_context.update(

compiler/qsc_partial_eval/src/lib.rs
@@ -521,7 +521,12 @@ impl<'a> PartialEvaluator<'a> {
         self.get_current_rir_block_mut().0.push(instruction);
 
         // Return the variable as a value.
-        let value = Value::Var(map_rir_var_to_eval_var(rir_variable));
+        let value = Value::Var(map_rir_var_to_eval_var(rir_variable).map_err(|()| {
+            Error::Unexpected(
+                format!("{} type in binop", rir_variable.ty),
+                bin_op_expr_span,
+            )
+        })?);
         Ok(EvalControlFlow::Continue(value))
     }
 
@@ -598,7 +603,12 @@ impl<'a> PartialEvaluator<'a> {
                     _ => panic!("unsupported binary operation for bools: {bin_op:?}"),
                 };
                 self.get_current_rir_block_mut().0.push(bin_op_ins);
-                Value::Var(map_rir_var_to_eval_var(bin_op_rir_variable))
+                Value::Var(map_rir_var_to_eval_var(bin_op_rir_variable).map_err(|()| {
+                    Error::Unexpected(
+                        format!("{} type in binop", bin_op_rir_variable.ty),
+                        self.get_expr_package_span(rhs_expr_id),
+                    )
+                })?)
             }
             _ => panic!("unsupported binary operation for bools: {bin_op:?}"),
         };
@@ -664,7 +674,12 @@ impl<'a> PartialEvaluator<'a> {
                 let cmp_inst =
                     Instruction::Icmp(condition_code, lhs_operand, rhs_operand, rir_variable);
                 self.get_current_rir_block_mut().0.push(cmp_inst);
-                map_rir_var_to_eval_var(rir_variable)
+                map_rir_var_to_eval_var(rir_variable).map_err(|()| {
+                    Error::Unexpected(
+                        format!("{} type in comparison binop", rir_variable.ty),
+                        self.get_expr_package_span(rhs_expr_id),
+                    )
+                })?
             }
             (_, _) => panic!("invalid Boolean comparison operator {bin_op:?}"),
         };
@@ -736,7 +751,12 @@ impl<'a> PartialEvaluator<'a> {
         self.get_program_block_mut(current_block_node.id)
             .0
             .push(branch_ins);
-        let result_eval_var = map_rir_var_to_eval_var(result_rir_var);
+        let result_eval_var = map_rir_var_to_eval_var(result_rir_var).map_err(|()| {
+            Error::Unexpected(
+                format!("{} type in logical binop", result_rir_var.ty),
+                self.get_expr_package_span(rhs_expr_id),
+            )
+        })?;
         Ok(result_eval_var)
     }
 
@@ -787,7 +807,12 @@ impl<'a> PartialEvaluator<'a> {
                 rhs_operand,
                 bin_op_expr_span,
             )?;
-        let value = Value::Var(map_rir_var_to_eval_var(bin_op_rir_variable));
+        let value = Value::Var(map_rir_var_to_eval_var(bin_op_rir_variable).map_err(|()| {
+            Error::Unexpected(
+                format!("{} type in binop", bin_op_rir_variable.ty),
+                bin_op_expr_span,
+            )
+        })?);
         Ok(EvalControlFlow::Continue(value))
     }
 
@@ -1442,7 +1467,15 @@ impl<'a> PartialEvaluator<'a> {
 
         // Return the value of the if expression.
         let if_expr_value = if let Some(if_expr_var) = maybe_if_expr_var {
-            Value::Var(map_rir_var_to_eval_var(if_expr_var))
+            Value::Var(map_rir_var_to_eval_var(if_expr_var).map_err(|()| {
+                Error::Unexpected(
+                    format!(
+                        "dynamic value of type {} in conditional expression",
+                        if_expr_var.ty
+                    ),
+                    self.get_expr_package_span(if_expr_id),
+                )
+            })?)
         } else {
             Value::unit()
         };
@@ -1651,7 +1684,12 @@ impl<'a> PartialEvaluator<'a> {
 
         // Insert the instruction and return the corresponding evaluator variable.
         self.get_current_rir_block_mut().0.push(instruction);
-        let eval_variable = map_rir_var_to_eval_var(rir_variable);
+        let eval_variable = map_rir_var_to_eval_var(rir_variable).map_err(|()| {
+            Error::Unexpected(
+                format!("{} type in unop", rir_variable.ty),
+                self.get_expr_package_span(value_expr_id),
+            )
+        })?;
         Ok(EvalControlFlow::Continue(Value::Var(eval_variable)))
     }
 
@@ -2873,19 +2911,19 @@ fn map_rir_literal_to_eval_value(literal: rir::Literal) -> Value {
     }
 }
 
-fn map_rir_var_to_eval_var(var: rir::Variable) -> Var {
-    Var {
+fn map_rir_var_to_eval_var(var: rir::Variable) -> Result<Var, ()> {
+    Ok(Var {
         id: var.variable_id.into(),
-        ty: map_rir_type_to_eval_var_type(var.ty),
-    }
+        ty: map_rir_type_to_eval_var_type(var.ty)?,
+    })
 }
 
-fn map_rir_type_to_eval_var_type(ty: rir::Ty) -> VarTy {
+fn map_rir_type_to_eval_var_type(ty: rir::Ty) -> Result<VarTy, ()> {
     match ty {
-        rir::Ty::Boolean => VarTy::Boolean,
-        rir::Ty::Integer => VarTy::Integer,
-        rir::Ty::Double => VarTy::Double,
-        _ => panic!("cannot convert RIR type {ty} to evaluator varible type"),
+        rir::Ty::Boolean => Ok(VarTy::Boolean),
+        rir::Ty::Integer => Ok(VarTy::Integer),
+        rir::Ty::Double => Ok(VarTy::Double),
+        _ => Err(()),
     }
 }
 

compiler/qsc_partial_eval/tests/branching.rs
@@ -12,7 +12,9 @@ pub mod test_utils;
 use expect_test::expect;
 use indoc::indoc;
 use qsc_rir::rir::CallableId;
-use test_utils::{assert_blocks, assert_callable, get_rir_program};
+use test_utils::{
+    assert_blocks, assert_callable, assert_error, get_partial_evaluation_error, get_rir_program,
+};
 
 #[test]
 fn if_expression_with_true_condition() {
@@ -1287,5 +1289,27 @@ fn if_else_expression_with_dynamic_condition_and_subsequent_call_to_operation() 
             Block 3:Block:
                 Call id(4), args( Qubit(0), )
                 Jump(1)"#]],
+    );
+}
+
+#[test]
+fn if_else_expression_with_result_literal_fails() {
+    let error = get_partial_evaluation_error(indoc! {
+        r#"
+        namespace Test {
+            @EntryPoint()
+            operation Main() : Result {
+                use q = Qubit();
+                MResetZ(q) == One ? One | MResetZ(q)
+            }
+        }
+        "#,
+    });
+
+    assert_error(
+        &error,
+        &expect![[
+            r#"Unexpected("dynamic value of type Result in conditional expression", PackageSpan { package: PackageId(2), span: Span { lo: 101, hi: 137 } })"#
+        ]],
     );
 }

compiler/qsc_rca/src/errors.rs
@@ -52,6 +52,20 @@ pub enum Error {
     #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicQubit"))]
     UseOfDynamicQubit(#[label] Span),
 
+    #[error("cannot use a dynamic Result")]
+    #[diagnostic(help(
+        "using a Result variable whose value depends on a measurement result is not supported by the current target"
+    ))]
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicResult"))]
+    UseOfDynamicResult(#[label] Span),
+
+    #[error("cannot use a dynamic tuple")]
+    #[diagnostic(help(
+        "using a tuple whose members depend on a measurement result is not supported by the current target"
+    ))]
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicTuple"))]
+    UseOfDynamicTuple(#[label] Span),
+
     #[error("cannot use a dynamic big integer value")]
     #[diagnostic(help(
         "using a big integer value that depends on a measurement result is not supported by the configured target profile"
@@ -199,6 +213,9 @@ pub fn generate_errors_from_runtime_features(
     if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicQubit) {
         errors.push(Error::UseOfDynamicQubit(span));
     }
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicResult) {
+        errors.push(Error::UseOfDynamicResult(span));
+    }
     if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicBigInt) {
         errors.push(Error::UseOfDynamicBigInt(span));
     }
@@ -210,6 +227,9 @@ pub fn generate_errors_from_runtime_features(
     }
     if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicallySizedArray) {
         errors.push(Error::UseOfDynamicallySizedArray(span));
+    }
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicTuple) {
+        errors.push(Error::UseOfDynamicTuple(span));
     }
     if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicUdt) {
         errors.push(Error::UseOfDynamicUdt(span));

compiler/qsc_rca/src/lib.rs
@@ -777,6 +777,10 @@ bitflags! {
         const UseOfIntOutput = 1 << 24;
         // Use of a dynamic exponent in a computation.
         const UseOfDynamicExponent = 1 << 25;
+        // Use of a dynamic `Result` variable in a computation.
+        const UseOfDynamicResult = 1 << 26;
+        // Use of a dynamic tuple variable.
+        const UseOfDynamicTuple = 1 << 27;
     }
 }
 
@@ -872,6 +876,12 @@ impl RuntimeFeatureFlags {
         }
         if self.contains(RuntimeFeatureFlags::UseOfDynamicExponent) {
             capabilities |= TargetCapabilityFlags::BackwardsBranching;
+        }
+        if self.contains(RuntimeFeatureFlags::UseOfDynamicResult) {
+            capabilities |= TargetCapabilityFlags::HigherLevelConstructs;
+        }
+        if self.contains(RuntimeFeatureFlags::UseOfDynamicTuple) {
+            capabilities |= TargetCapabilityFlags::HigherLevelConstructs;
         }
         capabilities
     }

commit 157500ff1ee057c048de436e209757e4cac7aab9

Fix panic when updating array with dynamic value
This fixes a panic that was introduced in #1571, where if the content being used to update an array was a dynamic, non-array value it would panic when trying to aggreate a default array type into a element type. To make sure the update matches the chosen type, generate the `ValueKind` from the matching expression type. Added unit tests for both array and non-array dynamic update types.

compiler/qsc_rca/src/core.rs
@@ -184,10 +184,11 @@ impl<'a> Analyzer<'a> {
         // runtime feature is used to mark the array itself as dynamically sized.
         if !application_instance.active_dynamic_scopes.is_empty() {
             default_value_kind = ValueKind::Array(RuntimeKind::Dynamic, RuntimeKind::Dynamic);
+            let replacement_ty = &self.get_expr(replacement_value_expr_id).ty;
             replacement_value_compute_kind =
                 replacement_value_compute_kind.aggregate(ComputeKind::new_with_runtime_features(
                     RuntimeFeatureFlags::UseOfDynamicallySizedArray,
-                    default_value_kind,
+                    ValueKind::new_static_from_type(replacement_ty),
                 ));
         }
 

compiler/qsc_rca/tests/arrays.rs
@@ -342,6 +342,56 @@ fn check_rca_for_mutable_array_assign_index_in_dynamic_context() {
 }
 
 #[test]
+fn check_rca_for_mutable_array_assign_index_dynamic_content_in_dynamic_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [Zero];
+        use q = Qubit();
+        let r = M(q);
+        if r == One {
+            set arr w/= 0 <- r;
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicallySizedArray)
+                    value_kind: Array(Content: Dynamic, Size: Dynamic)
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
+fn check_rca_for_mutable_array_assign_index_dynamic_nested_array_content_in_dynamic_context() {
+    let mut compilation_context = CompilationContext::default();
+    compilation_context.update(
+        r#"
+        mutable arr = [[Zero]];
+        use q = Qubit();
+        let r = M(q);
+        if r == One {
+            set arr w/= 0 <- [r];
+        }
+        arr"#,
+    );
+    let package_store_compute_properties = compilation_context.get_compute_properties();
+    check_last_statement_compute_properties(
+        package_store_compute_properties,
+        &expect![[r#"
+            ApplicationsGeneratorSet:
+                inherent: Quantum: QuantumProperties:
+                    runtime_features: RuntimeFeatureFlags(UseOfDynamicallySizedArray)
+                    value_kind: Array(Content: Dynamic, Size: Dynamic)
+                dynamic_param_applications: <empty>"#]],
+    );
+}
+
+#[test]
 fn check_rca_for_access_using_classical_index() {
     let mut compilation_context = CompilationContext::default();
     compilation_context.update(