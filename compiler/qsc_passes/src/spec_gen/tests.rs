c00c60fa871f2e345b1bf3513d8c5aa4ed5ac2a3
Stefan J. Wernli <swernli@microsoft.com>
2024-06-10 15:41:04 -0700

    Fix test spans after name change

--- compiler/qsc_passes/src/spec_gen/tests.rs
+++ compiler/qsc_passes/src/spec_gen/tests.rs
@@ -2139,33 +2139,33 @@ fn codegen_intrinsic_succeeds_with_no_specializations() {
         "#},
         &expect![[r#"
             Package:
-                Item 0 [0-139] (Public):
+                Item 0 [0-143] (Public):
                     Namespace (Ident 16 [10-11] "A"): Item 1, Item 2
-                Item 1 [18-97] (Public):
+                Item 1 [18-101] (Public):
                     Parent: 0
                     SimulatableIntrinsic
-                    Callable 0 [42-97] (operation):
-                        name: Ident 1 [52-55] "Foo"
-                        input: Pat 2 [56-65] [Type Qubit]: Bind: Ident 3 [56-57] "q"
+                    Callable 0 [46-101] (operation):
+                        name: Ident 1 [56-59] "Foo"
+                        input: Pat 2 [60-69] [Type Qubit]: Bind: Ident 3 [60-61] "q"
                         output: Unit
                         functors: empty set
-                        body: SpecDecl 4 [42-97]: Impl:
-                            Block 5 [74-97] [Type Unit]:
-                                Stmt 6 [84-91]: Semi: Expr 7 [84-90] [Type Unit]: Call:
-                                    Expr 8 [84-87] [Type (Qubit => Unit)]: Var: Item 2
-                                    Expr 9 [88-89] [Type Qubit]: Var: Local 3
+                        body: SpecDecl 4 [46-101]: Impl:
+                            Block 5 [78-101] [Type Unit]:
+                                Stmt 6 [88-95]: Semi: Expr 7 [88-94] [Type Unit]: Call:
+                                    Expr 8 [88-91] [Type (Qubit => Unit)]: Var: Item 2
+                                    Expr 9 [92-93] [Type Qubit]: Var: Local 3
                         adj: <none>
                         ctl: <none>
                         ctl-adj: <none>
-                Item 2 [102-137] (Public):
+                Item 2 [106-141] (Public):
                     Parent: 0
-                    Callable 10 [102-137] (operation):
-                        name: Ident 11 [112-115] "Bar"
-                        input: Pat 12 [116-125] [Type Qubit]: Bind: Ident 13 [116-117] "q"
+                    Callable 10 [106-141] (operation):
+                        name: Ident 11 [116-119] "Bar"
+                        input: Pat 12 [120-129] [Type Qubit]: Bind: Ident 13 [120-121] "q"
                         output: Unit
                         functors: empty set
-                        body: SpecDecl 14 [102-137]: Impl:
-                            Block 15 [134-137]: <empty>
+                        body: SpecDecl 14 [106-141]: Impl:
+                            Block 15 [138-141]: <empty>
                         adj: <none>
                         ctl: <none>
                         ctl-adj: <none>"#]],
@@ -2188,8 +2188,8 @@ fn codegen_intrinsic_fails_with_adj_specialization() {
             [
                 SimulatableIntrinsic(
                     Span {
-                        lo: 42,
-                        hi: 104,
+                        lo: 46,
+                        hi: 108,
                     },
                 ),
             ]
@@ -2213,8 +2213,8 @@ fn codegen_intrinsic_fails_with_ctl_specialization() {
             [
                 SimulatableIntrinsic(
                     Span {
-                        lo: 42,
-                        hi: 104,
+                        lo: 46,
+                        hi: 108,
                     },
                 ),
             ]
