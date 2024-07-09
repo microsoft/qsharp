ecf6af9e8367d291c1a3320aee16dc32f2c504ab
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2024-07-09 11:48:42 -0700

    document tests

--- noisy-simulator/src/tests.rs
+++ noisy-simulator/src/tests.rs
@@ -8,18 +8,21 @@ use crate::{
 };
 use num_complex::Complex;
 
+/// Assert that two f64 are equal up to a `TOLERANCE`.
 fn assert_approx_eq(left: f64, right: f64) {
     if (left - right).abs() > TOLERANCE {
         panic!("aprox_equal failed, left = {left}, right = {right}");
     }
 }
 
+/// Returns an H gate.
 fn h() -> Operation {
     let f = 0.5_f64.sqrt();
     operation!([f,  f;
                 f, -f;])
 }
 
+/// Returns a CNOT gate.
 fn cnot() -> Operation {
     operation!([1., 0., 0., 0.;
                 0., 1., 0., 0.;
@@ -27,6 +30,7 @@ fn cnot() -> Operation {
                 0., 0., 1., 0.;])
 }
 
+/// Returns an MZ measurement.
 fn mz() -> Instrument {
     let m0 = operation!([1., 0.;
                          0., 0.;]);
@@ -59,19 +63,25 @@ pub fn measure_1<NS: NoisySimulator>() {
     assert_eq!(measurement, 1);
 }
 
+/// Check that both measurements in a Bell Pair yield the same result.
 pub fn bell_pair_sampling<NS: NoisySimulator>() {
     let (h, cnot, mz) = (h(), cnot(), mz());
     let mut sim = NS::new(2);
+
+    // Make a Bell Pair.
     sim.apply_operation(&h, &[0])
         .expect("operation should succeed");
     sim.apply_operation(&cnot, &[1, 0])
         .expect("operation should succeed");
+
+    // Measure both qubits.
     let m1 = sim
         .sample_instrument(&mz, &[0])
         .expect("measurement should succeed");
     let m2 = sim
         .sample_instrument(&mz, &[1])
         .expect("measurement should succeed");
+
     assert_eq!(m1, m2);
 }
 
@@ -79,6 +89,8 @@ pub fn bell_pair_projection<NS: NoisySimulator>(outcome: usize) -> Result<(), Er
     assert!((0..4).contains(&outcome));
     let (h, cnot, mz) = (h(), cnot(), mz());
     let mut sim = NS::new(2);
+
+    // Make a Bell Pair.
     sim.apply_operation(&h, &[0])
         .expect("operation should succeed");
     sim.apply_operation(&cnot, &[1, 0])
@@ -111,13 +123,10 @@ pub fn two_qubit_gate<NS: NoisySimulator>(outcome: usize) -> Result<(), Error> {
     let crx = |t: f64| {
         let c = t.cos();
         let s = t.sin() * Complex::I;
-        operation!(
-            [1., 0., 0., 0.;
-             0., 1., 0., 0.;
-             0., 0., c,  s;
-             0., 0., s,  c;
-            ]
-        )
+        operation!([1., 0., 0., 0.;
+                    0., 1., 0., 0.;
+                    0., 0., c,  s;
+                    0., 0., s,  c;])
     };
 
     for p in &probabilities {
@@ -147,6 +156,7 @@ pub fn two_qubit_gate<NS: NoisySimulator>(outcome: usize) -> Result<(), Error> {
     Ok(())
 }
 
+/// Check that two consecutive MZ on the same qubit yields the same outcome.
 pub fn repeated_mz<NS: NoisySimulator>() {
     let h = h();
     let mz = mz();
@@ -167,14 +177,10 @@ pub fn alternating_mz_and_mx<NS: NoisySimulator>() {
     let h = h();
     let mz = mz();
     let mx = Instrument::new(vec![
-        operation!([
-            0.5, 0.5;
-            0.5, 0.5;
-        ]),
-        operation!([
-             0.5, -0.5;
-            -0.5,  0.5;
-        ]),
+        operation!([0.5, 0.5;
+                    0.5, 0.5;]),
+        operation!([ 0.5, -0.5;
+                    -0.5,  0.5;]),
     ]);
 
     let mut sim = NS::new(1);
