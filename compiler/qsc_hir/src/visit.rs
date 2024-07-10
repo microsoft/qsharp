a787bfd1bfc0c8800e040c04999d89d4df10596b
sezna <alehansen@microsoft.com>
2024-07-10 16:40:58 -0700

    fix visitor impl

--- compiler/qsc_hir/src/visit.rs
+++ compiler/qsc_hir/src/visit.rs
@@ -65,7 +65,7 @@ pub fn walk_item<'a>(vis: &mut impl Visitor<'a>, item: &'a Item) {
         ItemKind::Callable(decl) => vis.visit_callable_decl(decl),
         ItemKind::Namespace(name, _) => vis.visit_idents(name),
         ItemKind::Ty(name, _) => vis.visit_ident(name),
-        ItemKind::Export(_, _) => (),
+        ItemKind::Export(name, _) => vis.visit_ident(name),
     }
 }
 
