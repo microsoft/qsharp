312af694dfd8af315cd6772c47ab0224c733c01d
orpuente-MS <156957451+orpuente-MS@users.noreply.github.com>
2024-07-12 13:12:25 -0700

    Add noisy simulator backend to qsharp (#1673)
    This PR does two things:
     - [x] Adds a noisy simulator backend to the qsharp repo.
     - [x] Exposes a thin PyO3 interface around the simulator.

---
+++ noisy_simulator/src/tests.rs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+pub mod noiseless_tests;
+pub mod noisy_tests;
+
+use crate::TOLERANCE;
+
+/// Assert that two f64 are equal up to a `TOLERANCE`.
+pub fn assert_approx_eq(left: f64, right: f64) {
+    assert_approx_eq_with_tolerance(left, right, TOLERANCE);
+}
+
+pub fn assert_approx_eq_with_tolerance(left: f64, right: f64, tolerance: f64) {
+    if (left - right).abs() > tolerance {
+        panic!("aprox_equal failed, left = {left}, right = {right}");
+    }
+}
