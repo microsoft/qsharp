22818e716a049e74b87af0f763f81dd1bad9c937
Moumita <69388410+moumita-halder@users.noreply.github.com>
2024-07-16 01:15:02 -0400

    Apply suggestions from code review
    Co-authored-by: César Zaragoza Cortés <cesarzc@microsoft.com>

--- samples/python_interop/teleportation_project/RunTeleport.py
+++ samples/python_interop/teleportation_project/RunTeleport.py
@@ -1,9 +1,10 @@
 import qsharp
 
 # set the root folder for the Q# project
-# make adjustments to the path depending on where your program is saved
+# the root folder of a Q# project is where the qsharp.json file is located
+# make adjustments to the path depending on the location of the qsharp.json file
 
-# this example assumes your program is in the same folder as the qsharp.json file
+# this example assumes your Python program is in the same folder as the qsharp.json file
 qsharp.init(project_root=".")
 
 print(qsharp.eval("RunTeleport.RunTeleportationExample()"))
