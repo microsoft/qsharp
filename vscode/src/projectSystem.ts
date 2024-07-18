284acd1eda88685ca07be9ab538a581b720bc677
sezna <alehansen@microsoft.com>
2024-07-18 10:14:08 -0700

    add comment

--- vscode/src/projectSystem.ts
+++ vscode/src/projectSystem.ts
@@ -248,6 +248,9 @@ export async function fetchGithubRaw(
   const uri = `${githubEndpoint}/${owner}/${repo}/${ref}/${pathNoLeadingSlash}`;
   log.info(`making request to ${uri}`);
   const response = await fetch(uri);
+  // note that if the above fetch fails, we will never send this telemetry event.
+  // however, this is okay, because if a network request to github is failing, it is likely
+  // that the user's network itself is suspect and the telemetry wouldn't send anyway.
   sendTelemetryEvent(
     EventType.FetchGitHub,
     { status: response.status.toString() },
