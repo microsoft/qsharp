ea2041a6045f58fcce3fa88c2f898f8a6547b4c8
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2024-07-18 15:05:59 -0700

    remove comment

--- noisy_simulator/src/kernel.rs
+++ noisy_simulator/src/kernel.rs
@@ -56,8 +56,6 @@ pub fn apply_kernel(
         if (s & mask) == 0 {
             // Extract relevant entries into a vector to make the gate application easier.
             for k in 0..num_elements {
-                // let idx = s | index_offsets[k];
-                // extracted_entries[k] = state[idx];
                 let idx = s | unsafe { index_offsets.get_unchecked(k) };
                 unsafe {
                     *extracted_entries.get_unchecked_mut(k) = *state.get_unchecked(idx);
@@ -74,8 +72,6 @@ pub fn apply_kernel(
 
             // Store accumulated result back into the state vector.
             for k in 0..num_elements {
-                // let idx = s | index_offsets[k];
-                // state[idx] = extracted_entries[k];
                 let idx = s | unsafe { index_offsets.get_unchecked(k) };
                 unsafe {
                     *state.get_unchecked_mut(idx) = *new_entries.get_unchecked(k);
