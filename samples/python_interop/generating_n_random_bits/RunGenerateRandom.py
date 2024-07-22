0781c925276cd385a59f4be0cf154b508cb67b0f
Moumita <69388410+moumita-halder@users.noreply.github.com>
2024-07-22 17:57:08 -0400

    Add a sample for modern QDK project with Python (#1721)
    This resolves [Issue
    1384](https://github.com/microsoft/qsharp/issues/1384).
    
    - Added [Teleportation project from the
    documentation](https://learn.microsoft.com/en-us/azure/quantum/user-guide/how-to-work-with-qsharp-projects?tabs=tabid-qsharp%2Ctabid-python-run#steps-for-creating-a-q-project)
    - Q# taking N, generating N random bits, and returning an array of them,
    and Python counting the number of 1s in the results
    
    ---------
    
    Co-authored-by: Mariia Mykhailova <michaylova@gmail.com>
    Co-authored-by: Bill Ticehurst <billti@hotmail.com>
    Co-authored-by: Manvi-Agrawal <40084144+Manvi-Agrawal@users.noreply.github.com>
    Co-authored-by: César Zaragoza Cortés <cesarzc@microsoft.com>

---
+++ samples/python_interop/generating_n_random_bits/RunGenerateRandom.py
@@ -0,0 +1,15 @@
+import qsharp
+
+qsharp.init(project_root=".")
+
+nQubits = input("Enter the number of random bits to be generated: ")
+(results, number) = qsharp.eval(f"GenerateRandom.GenerateRandomNumbers({nQubits})")
+
+count = 0
+for result in results:
+    if result == qsharp.Result.One:
+        count += 1
+
+print(f"Bits generated: {results}")
+print(f"Number of Ones: {count}")
+print(f"The integer representation of the generated {nQubits} bits: {number}")
---
+++ samples/python_interop/teleportation_project/RunTeleport.py
@@ -0,0 +1,10 @@
+import qsharp
+
+# set the root folder for the Q# project
+# the root folder of a Q# project is where the qsharp.json file is located
+# make adjustments to the path depending on the location of the qsharp.json file
+
+# this example assumes your Python program is in the same folder as the qsharp.json file
+qsharp.init(project_root=".")
+
+print(qsharp.eval("RunTeleport.RunTeleportationExample()"))
