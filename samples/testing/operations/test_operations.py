27800e931b3f6f9ee8aed7f29659df461f15a2a5
Manvi Agrawal <manvimeerut@gmail.com>
2024-07-23 14:07:17 -0700

    remove complex dtype in tests

--- samples/testing/operations/test_operations.py
+++ samples/testing/operations/test_operations.py
@@ -10,14 +10,14 @@ def setup():
     """Fixture to execute asserts before a test is run"""
     # You can set the target profile here if you want
     # By default, it uses "Unrestricted" profile
-    qsharp.init(target_profile=qsharp.TargetProfile.Unrestricted)
+    qsharp.init(target_profile=qsharp.TargetProfile.Base)
     yield # this is where the testing happens
 
 def test_empty_operation() -> None:
     res = dump_operation("qs => ()", 1)
     assert res == [
-        [complex(1.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(1.0, 0.0)],
+        [1.0, 0.0],
+        [0.0, 1.0],
     ]
 
 
@@ -26,8 +26,8 @@ def test_single_qubit_not_gate() -> None:
 
     res = dump_operation("qs => X(qs[0])", 1)
     assert res == [
-        [complex(0.0, 0.0), complex(1.0, 0.0)],
-        [complex(1.0, 0.0), complex(0.0, 0.0)],
+        [0.0, 1.0],
+        [1.0, 0.0],
     ]
 
 def test_single_qubit_hadamard_gate() -> None:
@@ -35,8 +35,8 @@ def test_single_qubit_hadamard_gate() -> None:
 
     res = dump_operation("qs => H(qs[0])", 1)
     assert res == [
-        [complex(0.707107, 0.0), complex(0.707107, 0.0)],
-        [complex(0.707107, 0.0), complex(-0.707107, 0.0)],
+        [0.707107, 0.707107],
+        [0.707107, -0.707107],
     ]
 
 def test_two_qubit_cnot_gate() -> None:
@@ -44,10 +44,10 @@ def test_two_qubit_cnot_gate() -> None:
 
     res = dump_operation("qs => CNOT(qs[0], qs[1])", 2)
     assert res == [
-        [complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0)],
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 0.0],
     ]
 
 def test_custom_operation() -> None:
@@ -59,10 +59,10 @@ def test_custom_operation() -> None:
 
     res = dump_operation("ApplySWAP", 2)
     assert res == [
-        [complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0)],
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
     ]
 
 def test_operation_no_args_in_qsharp_file() -> None:
@@ -71,10 +71,10 @@ def test_operation_no_args_in_qsharp_file() -> None:
 
     res = dump_operation("qs => CustomOperation.ApplySWAP(qs[0], qs[1])", 2)
     assert res == [
-        [complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(1.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.0, 0.0), complex(0.0, 0.0), complex(1.0, 0.0)],
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
     ]
 
 def test_operation_with_args_in_qsharp_file() -> None:
@@ -84,37 +84,37 @@ def test_operation_with_args_in_qsharp_file() -> None:
     res0 = dump_operation("BellState.AllBellStates(_, 0)", 2)
 
     assert res0 == [
-        [complex(0.707107, 0.0), complex(0.0,0.0), complex(0.707107, 0.0), complex(0.0,0.0)],
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(-0.707107, 0.0)],
-        [complex(0.707107, 0.0), complex(0.0, 0.0), complex(-0.707107, 0.0), complex(0.0, 0.0)],
+        [0.707107, 0.0, 0.707107, 0.0],
+        [0.0, 0.707107, 0.0, 0.707107],
+        [0.0, 0.707107, 0.0, -0.707107],
+        [0.707107, 0.0, -0.707107, 0.0],
     ]
 
     res1 = dump_operation("BellState.AllBellStates(_, 1)", 2)
 
     assert res1 == [
-        [complex(0.707107, 0.0), complex(0.0,0.0), complex(0.707107, 0.0), complex(0.0,0.0)],
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
-        [complex(0.0, 0.0), complex(-0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
-        [complex(-0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0)],
+        [0.707107, 0.0, 0.707107, 0.0],
+        [0.0, 0.707107, 0.0, 0.707107],
+        [0.0, -0.707107, 0.0, 0.707107],
+        [-0.707107, 0.0, 0.707107, 0.0],
     ]
 
     res2 = dump_operation("BellState.AllBellStates(_, 2)", 2)
 
     assert res2 == [
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
-        [complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0)],
-        [complex(0.707107, 0.0), complex(0.0, 0.0), complex(-0.707107, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(-0.707107, 0.0)],
+        [0.0, 0.707107, 0.0, 0.707107],
+        [0.707107, 0.0, 0.707107, 0.0],
+        [0.707107, 0.0, -0.707107, 0.0],
+        [0.0, 0.707107, 0.0, -0.707107],
     ]
 
     res3 = dump_operation("BellState.AllBellStates(_, 3)", 2)
 
     assert res3 == [
-        [complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
-        [complex(0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0)],
-        [complex(-0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0), complex(0.0, 0.0)],
-        [complex(0.0, 0.0), complex(-0.707107, 0.0), complex(0.0, 0.0), complex(0.707107, 0.0)],
+        [0.0, 0.707107, 0.0, 0.707107],
+        [0.707107, 0.0, 0.707107, 0.0],
+        [-0.707107, 0.0, 0.707107, 0.0],
+        [0.0, -0.707107, 0.0, 0.707107],
     ]
 
 
