3d71b8de225cb7ff73a776fdd0a43fc07c6392fd
sezna <alehansen@microsoft.com>
2024-07-29 07:50:37 -0400

    organize stdlib and update files list

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -23,74 +23,74 @@ pub const CORE_LIB: &[(&str, &str)] = &[
 pub const STD_LIB: &[(&str, &str)] = &[
     (
         "qsharp-library-source:arrays.qs",
-        include_str!("../qs_source/src/std/arrays.qs"),
+        include_str!("../qs_source/std/src/std/arrays.qs"),
     ),
     (
         "qsharp-library-source:canon.qs",
-        include_str!("../qs_source/src/std/canon.qs"),
+        include_str!("../qs_source/std/src/std/canon.qs"),
     ),
     (
         "qsharp-library-source:convert.qs",
-        include_str!("../qs_source/src/std/convert.qs"),
+        include_str!("../qs_source/std/src/std/convert.qs"),
     ),
     (
         "qsharp-library-source:core.qs",
-        include_str!("../qs_source/src/std/core.qs"),
+        include_str!("../qs_source/std/src/std/core.qs"),
     ),
     (
         "qsharp-library-source:diagnostics.qs",
-        include_str!("../qs_source/src/std/diagnostics.qs"),
+        include_str!("../qs_source/std/src/std/diagnostics.qs"),
     ),
     (
         "qsharp-library-source:internal.qs",
-        include_str!("../qs_source/src/std/internal.qs"),
+        include_str!("../qs_source/std/src/std/internal.qs"),
     ),
     (
         "qsharp-library-source:intrinsic.qs",
-        include_str!("../qs_source/src/std/intrinsic.qs"),
+        include_str!("../qs_source/std/src/std/intrinsic.qs"),
     ),
     (
         "qsharp-library-source:logical.qs",
-        include_str!("../qs_source/src/std/logical.qs"),
+        include_str!("../qs_source/std/src/std/logical.qs"),
     ),
     (
         "qsharp-library-source:math.qs",
-        include_str!("../qs_source/src/std/math.qs"),
+        include_str!("../qs_source/std/src/std/math.qs"),
     ),
     (
         "qsharp-library-source:measurement.qs",
-        include_str!("../qs_source/src/std/measurement.qs"),
+        include_str!("../qs_source/std/src/std/measurement.qs"),
     ),
     (
         "qsharp-library-source:qir.qs",
-        include_str!("../qs_source/src/std/qir.qs"),
+        include_str!("../qs_source/std/src/std/qir.qs"),
     ),
     (
         "qsharp-library-source:random.qs",
-        include_str!("../qs_source/src/std/random.qs"),
+        include_str!("../qs_source/std/src/std/random.qs"),
     ),
     (
         "qsharp-library-source:re.qs",
-        include_str!("../qs_source/src/std/re.qs"),
+        include_str!("../qs_source/std/src/std/re.qs"),
     ),
     (
         "qsharp-library-source:unstable_arithmetic.qs",
-        include_str!("../qs_source/src/std/unstable_arithmetic.qs"),
+        include_str!("../qs_source/std/src/std/unstable_arithmetic.qs"),
     ),
     (
         "qsharp-library-source:unstable_arithmetic_internal.qs",
-        include_str!("../qs_source/src/std/unstable_arithmetic_internal.qs"),
+        include_str!("../qs_source/std/src/std/unstable_arithmetic_internal.qs"),
     ),
     (
         "qsharp-library-source:unstable_state_preparation.qs",
-        include_str!("../qs_source/src/std/unstable_state_preparation.qs"),
+        include_str!("../qs_source/std/src/std/unstable_state_preparation.qs"),
     ),
     (
         "qsharp-library-source:unstable_table_lookup.qs",
-        include_str!("../qs_source/src/std/unstable_table_lookup.qs"),
+        include_str!("../qs_source/std/src/std/unstable_table_lookup.qs"),
     ),
     (
         "qsharp-library-source:modern_api.qs",
-        include_str!("../qs_source/src/std/modern_api.qs"),
+        include_str!("../qs_source/std/src/std/modern_api.qs"),
     ),
 ];
