bd11498ad959b165f77fb79cf17ec647bcb1c2a1
sezna <alehansen@microsoft.com>
2024-08-20 16:02:28 -0700

    wip

--- bench.sh
+++ bench.sh
@@ -3,7 +3,7 @@ commit_one_week_ago=$(git rev-list -n 1 --before="1 week ago" $branch)
 latest_commit=$(git rev-list -n 1 HEAD)
 
 echo "benching commit $commit_one_week_ago"
-cargo criterion --history-id $commit_one_week_ago
+cargo criterion --message-format=json --history-id $commit_one_week_ago > week_ago.json
 
 echo "benching commit $latest_commit"
-cargo criterion --history-id $latest_commit
+cargo criterion --message-format=json --history-id $latest_commit > latest.json
