aa73bc1a17aea3cec0f8b990e651c5f7f6d642dc
sezna <alehansen@microsoft.com>
2024-08-20 15:13:10 -0700

    only test two commits

--- bench.sh
+++ bench.sh
@@ -1,7 +1,9 @@
 branch="alex/benching"
-echo $(git rev-list --since="1 week ago" $branch)
-for commit in $(git rev-list --since="1 week ago" $branch)
-do
-  echo "benching commit" . $commit
-  cargo criterion --history-id $commit
-done
+commit_one_week_ago=$(git rev-list -n 1 --before="1 week ago" $branch)
+latest_commit=$(git rev-list -n 1 HEAD)
+
+echo "benching commit $commit_one_week_ago"
+cargo criterion --history-id $commit_one_week_ago
+
+echo "benching commit $latest_commit"
+cargo criterion --history-id $latest_commit
