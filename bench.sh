0c0eba677e9ba3e9c02480c1b8dd0c94afc5c8d1
sezna <alehansen@microsoft.com>
2024-08-21 10:11:19 -0700

    try using python for the benches

--- bench.sh
+++
@@ -1,8 +0,0 @@
-branch="alex/benching"
-echo $(git rev-list --since="1 day ago" --pretty='format:%ad__%h' --date=short $branch | awk 'NR%2==0')
-for date_and_commit in $(git rev-list --since="1 week ago" --pretty='format:%ad__%h' --date=short $branch | awk 'NR%2==0')
-do
-  echo "benching commit" $date_and_commit
-  cargo criterion --message-format=json --history-id $date_and_commit > $date_and_commit.json
-done
-
