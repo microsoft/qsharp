c9ee0cdb2fffc19f9be697abd18a0a5014363016
sezna <alehansen@microsoft.com>
2024-08-21 08:40:29 -0700

    bench with timestamps

--- bench.sh
+++ bench.sh
@@ -1,9 +1,8 @@
 branch="alex/benching"
-commit_one_week_ago=$(git rev-list -n 1 --before="1 week ago" $branch)
-latest_commit=$(git rev-list -n 1 HEAD)
+echo $(git rev-list --since="1 day ago" --pretty='format:%ad__%h' --date=short $branch | awk 'NR%2==0')
+for date_and_commit in $(git rev-list --since="1 week ago" --pretty='format:%ad__%h' --date=short $branch | awk 'NR%2==0')
+do
+  echo "benching commit" $date_and_commit
+  cargo criterion --message-format=json --history-id $date_and_commit > $date_and_commit.json
+done
 
-echo "benching commit $commit_one_week_ago"
-cargo criterion --message-format=json --history-id $commit_one_week_ago > week_ago.json
-
-echo "benching commit $latest_commit"
-cargo criterion --message-format=json --history-id $latest_commit > latest.json
