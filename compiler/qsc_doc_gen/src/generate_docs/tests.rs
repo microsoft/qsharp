d8c6b20f9eb82666f9e2eda8fb559dc584f17815
Scott Carda <Scott.Carda@microsoft.com>
2024-11-01 12:22:59 -0700

    test

--- compiler/qsc_doc_gen/src/generate_docs/tests.rs
+++ compiler/qsc_doc_gen/src/generate_docs/tests.rs
@@ -5,7 +5,7 @@ use super::generate_docs;
 use expect_test::expect;
 
 #[test]
-fn docs_generation() {
+fn generates_standard_function() {
     let files = generate_docs(None, None, None);
     let (_, metadata, contents) = files
         .iter()
@@ -48,6 +48,57 @@ fn docs_generation() {
         ```qsharp
         Message($"{ Length([0, 0, 0]) }"); // Prints 3
         ```
+    "#]]
+    .assert_eq(full_contents.as_str());
+}
+
+#[test]
+fn generates_unrestricted_function() {
+    let files = generate_docs(None, None, None);
+    let (_, metadata, contents) = files
+        .iter()
+        .find(|(file_name, _, _)| &**file_name == "Std.Diagnostics/CheckZero.md")
+        .expect("Could not file doc file for Length");
+    let full_contents = format!("{metadata}\n\n{contents}");
+
+    expect![[r#"
+        ---
+        uid: Qdk.Std.Diagnostics.CheckZero
+        title: CheckZero operation
+        ms.date: {TIMESTAMP}
+        ms.topic: managed-reference
+        qsharp.kind: operation
+        qsharp.package: __Std__
+        qsharp.namespace: Std.Diagnostics
+        qsharp.name: CheckZero
+        qsharp.summary: "Checks whether a qubit is in the |0⟩ state, returning true if it is."
+        ---
+
+        # CheckZero operation
+
+        Fully qualified name: Std.Diagnostics.CheckZero
+
+        ```qsharp
+        operation CheckZero(qubit : Qubit) : Bool
+        ```
+
+        ## Summary
+        Checks whether a qubit is in the |0⟩ state, returning true if it is.
+
+        ## Description
+        This operation checks whether a qubit is in the |0⟩ state. It will return true only
+        if the qubit is deterministically in the |0⟩ state, and will return false otherwise. This operation
+        does not change the state of the qubit.
+
+        ## Input
+        ### qubit
+        The qubit to check.
+        ## Output
+        True if the qubit is in the |0⟩ state, false otherwise.
+
+        ## Remarks
+        This operation is useful for checking whether a qubit is in the |0⟩ state during simulation. It is not possible to check
+        this on hardware without measuring the qubit, which could change the state.
     "#]]
     .assert_eq(full_contents.as_str());
 }
