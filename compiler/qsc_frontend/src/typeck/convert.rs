6965497a762e854614d6282862b8cc71f01361ae
Alexander Hansen <alex@alex-hansen.com>
2024-11-04 13:05:18 -0800

    Remove unnecessary recursive class check

--- compiler/qsc_frontend/src/typeck/convert.rs
+++ compiler/qsc_frontend/src/typeck/convert.rs
@@ -296,7 +296,7 @@ pub(crate) fn ast_pat_ty(names: &Names, pat: &Pat) -> (Ty, Vec<TyConversionError
     match &*pat.kind {
         PatKind::Bind(_, None) | PatKind::Discard(None) | PatKind::Elided => (
             Ty::Err,
-            vec![TyConversionError::MissingTy { span: pat.span }.into()],
+            vec![TyConversionError::MissingTy { span: pat.span }],
         ),
         PatKind::Bind(_, Some(ty)) | PatKind::Discard(Some(ty)) => ty_from_ast(names, ty),
         PatKind::Paren(inner) => ast_pat_ty(names, inner),
