3ab30341ae04c2a63190fae965df6e286e8ac57f
Scott Carda <Scott.Carda@microsoft.com>
2024-11-13 13:34:28 -0800

    fix regex match in playground

--- playground/src/docs.tsx
+++ playground/src/docs.tsx
@@ -32,7 +32,7 @@ export function processDocumentFiles(
     }
     // The next line contains "Zero-width space" unicode character
     // to allow line breaks before the period.
-    const newNamespace = "… " + match[1].replace(".", "​.");
+    const newNamespace = "… " + match[2].replace(".", "​.");
 
     if (contentByNamespace.has(newNamespace)) {
       const existingContent = contentByNamespace.get(newNamespace)!;
