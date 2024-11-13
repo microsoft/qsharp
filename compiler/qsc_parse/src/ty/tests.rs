fb159df3e1116a75e7a605d4d8a4e60836e2a916
sezna <alehansen@microsoft.com>
2024-11-13 11:24:55 -0800

    expect tests

--- compiler/qsc_parse/src/ty/tests.rs
+++ compiler/qsc_parse/src/ty/tests.rs
@@ -97,11 +97,7 @@ fn ty_unit() {
 
 #[test]
 fn ty_param() {
-    check(
-        ty,
-        "'T",
-        &expect!["Type _id_ [0-2]: Type Param: 'T"],
-    );
+    check(ty, "'T", &expect!["Type _id_ [0-2]: Type Param: 'T"]);
 }
 
 #[test]
