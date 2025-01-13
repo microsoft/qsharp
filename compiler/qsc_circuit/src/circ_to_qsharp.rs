9b7e59729f0135c24c793f0ed2180c0d54b1e82f
Scott Carda <Scott.Carda@microsoft.com>
2025-01-13 13:13:56 -0800

    very basic Q# code-gen

--- compiler/qsc_circuit/src/circ_to_qsharp.rs
+++ compiler/qsc_circuit/src/circ_to_qsharp.rs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+use rustc_hash::FxHashMap;
+
 use crate::Circuit;
 
 // pub fn circ_to_qsharp(circuit: Circuit) -> String {
@@ -25,9 +27,70 @@ use crate::Circuit;
 
 pub fn str_test(contents: String) -> String {
     match serde_json::from_str::<Circuit>(contents.as_str()) {
-        Ok(circuit) => test(circuit),
+        // Ok(circuit) => test(circuit),
+        Ok(circuit) => build_qsharp(circuit),
         Err(e) => format!("Error: {}", e),
     }
+}
+
+pub fn build_qsharp(circuit: Circuit) -> String {
+    let mut indentation_level = 0;
+    let qubits = circuit
+        .qubits
+        .iter()
+        .map(|q| (q.id, format!("q{}", q.id)))
+        .collect::<FxHashMap<_, _>>();
+
+    let mut parameters = qubits.iter().collect::<Vec<_>>();
+    parameters.sort_by_key(|(id, _)| *id);
+    let parameters = parameters
+        .iter()
+        .map(|(_, name)| format!("{} : Qubit", name))
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    let return_type = "Unit";
+
+    let mut qsharp_str = format!("operation PreparePsiMinus({parameters}) : {return_type} {{\n");
+    indentation_level += 1;
+
+    for op in circuit.operations {
+        let gate = op.gate.as_str();
+
+        let targets = op
+            .targets
+            .iter()
+            .map(|t| qubits.get(&t.q_id).unwrap().clone())
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        let controls = op
+            .controls
+            .iter()
+            .map(|t| qubits.get(&t.q_id).unwrap().clone())
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        let args = match (controls.is_empty(), targets.is_empty()) {
+            (false, false) => format!("[{controls}], {targets}"),
+            (false, true) => format!("[{controls}]"),
+            (true, false) => targets,
+            (true, true) => "".to_owned(),
+        };
+
+        let operation_str = match (op.is_controlled, op.is_adjoint) {
+            (false, false) => format!("{gate}({args})"),
+            (false, true) => format!("Adjoint {gate}({args})"),
+            (true, false) => format!("Controlled {gate}({args})"),
+            (true, true) => format!("Controlled Adjoint {gate}({args})"),
+        };
+
+        let indent = "    ".repeat(indentation_level);
+
+        qsharp_str.push_str(&format!("{indent}{operation_str};\n"));
+    }
+    qsharp_str.push_str("}\n");
+    qsharp_str
 }
 
 pub fn test(_x: Circuit) -> String {
