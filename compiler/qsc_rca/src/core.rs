68f7d1853d4115a06904ee56f376701b253a431b
Stefan J. Wernli <swernli@microsoft.com>
2025-01-13 12:41:51 -0800

    Remove extra panic import

--- compiler/qsc_rca/src/core.rs
+++ compiler/qsc_rca/src/core.rs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use core::panic;
-
 use crate::{
     applications::{ApplicationInstance, GeneratorSetsBuilder, LocalComputeKind},
     common::{
