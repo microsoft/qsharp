0d049cdd2ddbb2e5e6a4f119a1f0aaf6db147a5e
Ian Davis <Ian.F.Davis@microsoft.com>
2025-02-07 12:52:29 -0800

    Adding classical & io decls to the parser, stubbing out array decls (#2165)

--- compiler/qsc_qasm3/src/parser/stmt/tests.rs
+++ compiler/qsc_qasm3/src/parser/stmt/tests.rs
@@ -1,67 +1,8 @@
-use expect_test::expect;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-use crate::parser::tests::check;
-
-use super::parse;
-
-#[test]
-fn quantum_decl() {
-    check(
-        parse,
-        "qubit q;",
-        &expect![[r#"
-            Stmt [0-8]
-            StmtKind: QubitDeclaration [0-8]: Ident [6-7] "q""#]],
-    );
-}
-
-#[test]
-fn quantum_decl_missing_name() {
-    check(
-        parse,
-        "qubit;",
-        &expect![[r#"
-            Error(
-                Rule(
-                    "identifier",
-                    Semicolon,
-                    Span {
-                        lo: 5,
-                        hi: 6,
-                    },
-                ),
-            )
-        "#]],
-    );
-}
-
-#[test]
-fn quantum_decl_with_designator() {
-    check(
-        parse,
-        "qubit[5] qubits;",
-        &expect![[r#"
-            Stmt [0-16]
-            StmtKind: QubitDeclaration [0-16]: Ident [9-15] "qubits", ExprStmt [5-8]: Expr [6-7]: Lit: Int(5)"#]],
-    );
-}
-
-#[test]
-fn quantum_decl_with_designator_missing_name() {
-    check(
-        parse,
-        "qubit[5]",
-        &expect![[r#"
-            Error(
-                Rule(
-                    "identifier",
-                    Eof,
-                    Span {
-                        lo: 8,
-                        hi: 8,
-                    },
-                ),
-            )
-        "#]],
-    );
-}
+mod annotation;
+mod classical_decl;
+mod io_decl;
+mod pragma;
+mod quantum_decl;
