a56a806be1e993939747fd52fbb7c7e6992b8dda
Ian Davis <Ian.F.Davis@microsoft.com>
2025-02-12 15:01:24 -0800

    Add subroutine, return, and gate defs, externs, and old style decls (#2176)

---
+++ compiler/qsc_qasm3/src/parser/stmt/tests/old_style_decl.rs
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+use expect_test::expect;
+
+use crate::parser::tests::check;
+
+use crate::parser::stmt::parse;
+
+#[test]
+fn creg_decl() {
+    check(
+        parse,
+        "creg c;",
+        &expect![[r#"
+            Stmt [0-7]
+                StmtKind: ClassicalDeclarationStmt [0-7]: ClassicalType [0-7]: BitType, Ident [5-6] "c""#]],
+    );
+}
+
+#[test]
+fn creg_array_decl() {
+    check(
+        parse,
+        "creg c[n];",
+        &expect![[r#"
+            Stmt [0-10]
+                StmtKind: ClassicalDeclarationStmt [0-10]: ClassicalType [0-10]: BitType [0-10]: ExprStmt [6-9]: Expr [7-8]: Ident [7-8] "n", Ident [5-6] "c""#]],
+    );
+}
+
+#[test]
+fn qreg_decl() {
+    check(
+        parse,
+        "qreg q;",
+        &expect![[r#"
+            Stmt [0-7]
+                StmtKind: QubitDeclaration [0-7]: Ident [5-6] "q""#]],
+    );
+}
+
+#[test]
+fn qreg_array_decl() {
+    check(
+        parse,
+        "qreg q[n];",
+        &expect![[r#"
+            Stmt [0-10]
+                StmtKind: QubitDeclaration [0-10]: Ident [5-6] "q", ExprStmt [6-9]: Expr [7-8]: Ident [7-8] "n""#]],
+    );
+}
