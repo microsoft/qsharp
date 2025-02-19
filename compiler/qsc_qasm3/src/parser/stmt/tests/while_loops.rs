aa6826a50fc8c009377439da5c79996772a805e0
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2025-02-19 14:46:26 -0800

    parse if, for, and while statements

---
+++ compiler/qsc_qasm3/src/parser/stmt/tests/while_loops.rs
@@ -0,0 +1,39 @@
+use crate::parser::{stmt::parse, tests::check};
+use expect_test::expect;
+
+#[test]
+fn simple_while() {
+    check(
+        parse,
+        "
+    while (x != 2) {
+        int a = 0;
+    }",
+        &expect![[r#"
+            Stmt [5-46]
+                StmtKind: WhileLoop [5-46]: Expr [11-19]: Paren:
+                    Expr [12-18]: BinOp (Neq):
+                        Expr [12-13]: Ident [12-13] "x"
+                        Expr [17-18]: Lit: Int(2)
+                Stmt [30-40]
+                    StmtKind: ClassicalDeclarationStmt [30-40]: ClassicalType [30-33]: IntType [30-33], Ident [34-35] "a", ValueExpression ExprStmt [38-39]: Expr [38-39]: Lit: Int(0)"#]],
+    );
+}
+
+#[test]
+fn while_stmt_body() {
+    check(
+        parse,
+        "
+    while (x != 2)
+        int a = 0;",
+        &expect![[r#"
+            Stmt [5-38]
+                StmtKind: WhileLoop [5-38]: Expr [11-19]: Paren:
+                    Expr [12-18]: BinOp (Neq):
+                        Expr [12-13]: Ident [12-13] "x"
+                        Expr [17-18]: Lit: Int(2)
+                Stmt [28-38]
+                    StmtKind: ClassicalDeclarationStmt [28-38]: ClassicalType [28-31]: IntType [28-31], Ident [32-33] "a", ValueExpression ExprStmt [36-37]: Expr [36-37]: Lit: Int(0)"#]],
+    );
+}
