1ed793fe9b51937684747fa61a4d62af3d9d21d0
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2025-02-20 12:48:26 -0800

    format alias.rs file

--- compiler/qsc_qasm3/src/parser/stmt/tests/alias.rs
+++ compiler/qsc_qasm3/src/parser/stmt/tests/alias.rs
@@ -7,7 +7,10 @@ use expect_test::expect;
 
 #[test]
 fn alias_decl_stmt() {
-    check(parse, "let x = a[1:2] ++ b ++ c[1:2:3]", &expect![[r#"
+    check(
+        parse,
+        "let x = a[1:2] ++ b ++ c[1:2:3]",
+        &expect![[r#"
         Stmt [0-31]
             StmtKind: Alias [0-31]: Ident [4-5] "x"
                 Expr [8-14]: IndexExpr [9-14]: Expr [8-9]: Ident [8-9] "a", IndexElement:
@@ -20,5 +23,6 @@ fn alias_decl_stmt() {
                     Range: [25-30]
                         start: Expr [25-26]: Lit: Int(1)
                         step: Expr [27-28]: Lit: Int(2)
-                        end: Expr [29-30]: Lit: Int(3)"#]]);
+                        end: Expr [29-30]: Lit: Int(3)"#]],
+    );
 }
