17610440338a583c5162ac6f9b5a6db4d62775de
Oscar Puente <156957451+orpuente-MS@users.noreply.github.com>
2025-02-25 15:33:45 -0800

    add unit tests

---
+++ compiler/qsc_qasm3/src/parser/stmt/tests/cal_grammar.rs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+use crate::parser::stmt::parse;
+use crate::parser::tests::check;
+use expect_test::expect;
+
+#[test]
+fn defcalgrammar() {
+    check(parse, r#"defcalgrammar "openpulse";"#, &expect![[r#"
+        Stmt [0-26]
+            StmtKind: CalibrationGrammarStmt [0-26]: openpulse"#]]);
+}
