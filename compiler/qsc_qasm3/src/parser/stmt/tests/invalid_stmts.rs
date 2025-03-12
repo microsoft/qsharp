abc24544b2c2c0f78e517004001dbe14e4e8b3e5
orpuente-MS <156957451+orpuente-MS@users.noreply.github.com>
2025-03-12 07:30:32 -0700

    Add grammar docstrings to QASM3 parser (#2222)
    This PR:
     1. Fixes a bug with block statements
     2. Adds grammar docstrings to the QASM3 parser.
    3. Adds the invalid tests in the reference parser to make sure they are
    invalid for our parser as well.

--- compiler/qsc_qasm3/src/parser/stmt/tests/invalid_stmts.rs
+++ compiler/qsc_qasm3/src/parser/stmt/tests/invalid_stmts.rs
@@ -1,72 +1,11 @@
-use crate::parser::{stmt::parse, tests::check};
-use expect_test::expect;
-
-#[test]
-fn assignment_in_if_condition() {
-    check(
-        parse,
-        "if (x = 2) { 3; }",
-        &expect![[r#"
-            Stmt [0-17]:
-                annotations: <empty>
-                kind: IfStmt [0-17]:
-                    condition: Expr [4-5]: Ident [4-5] "x"
-                    if_block:
-                        Stmt [13-15]:
-                            annotations: <empty>
-                            kind: ExprStmt [13-15]:
-                                expr: Expr [13-14]: Lit: Int(3)
-                    else_block: <none>
-
-            [
-                Error(
-                    Token(
-                        Close(
-                            Paren,
-                        ),
-                        Eq,
-                        Span {
-                            lo: 6,
-                            hi: 7,
-                        },
-                    ),
-                ),
-            ]"#]],
-    );
-}
-
-#[test]
-fn binary_op_assignment_in_if_condition() {
-    check(
-        parse,
-        "if (x += 2) { 3; }",
-        &expect![[r#"
-            Stmt [0-18]:
-                annotations: <empty>
-                kind: IfStmt [0-18]:
-                    condition: Expr [4-5]: Ident [4-5] "x"
-                    if_block:
-                        Stmt [14-16]:
-                            annotations: <empty>
-                            kind: ExprStmt [14-16]:
-                                expr: Expr [14-15]: Lit: Int(3)
-                    else_block: <none>
-
-            [
-                Error(
-                    Token(
-                        Close(
-                            Paren,
-                        ),
-                        BinOpEq(
-                            Plus,
-                        ),
-                        Span {
-                            lo: 6,
-                            hi: 8,
-                        },
-                    ),
-                ),
-            ]"#]],
-    );
-}
+mod branch;
+mod cal;
+mod constant;
+mod decl;
+mod gate_calls;
+mod headers;
+mod io;
+mod loops;
+mod measure;
+mod switch;
+mod tokens;
---
+++ compiler/qsc_qasm3/src/parser/stmt/tests/invalid_stmts/measure.rs
@@ -0,0 +1,180 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+use crate::parser::{stmt::parse, tests::check};
+use expect_test::expect;
+
+#[test]
+fn measure_multiple_qubits() {
+    check(
+        parse,
+        "measure $0, $1;",
+        &expect![[r#"
+        Stmt [0-10]:
+            annotations: <empty>
+            kind: MeasureStmt [0-10]:
+                measurement: MeasureExpr [0-7]:
+                    operand: HardwareQubit [8-10]: 0
+                target: <none>
+
+        [
+            Error(
+                Token(
+                    Semicolon,
+                    Comma,
+                    Span {
+                        lo: 10,
+                        hi: 11,
+                    },
+                ),
+            ),
+        ]"#]],
+    );
+}
+
+#[test]
+fn assign_measure_multiple_qubits() {
+    check(
+        parse,
+        "a[0:1] = measure $0, $1;",
+        &expect![[r#"
+        Error(
+            Rule(
+                "expression",
+                Measure,
+                Span {
+                    lo: 9,
+                    hi: 16,
+                },
+            ),
+        )
+    "#]],
+    );
+}
+
+#[test]
+fn assign_arrow() {
+    check(
+        parse,
+        "a = measure $0 -> b;",
+        &expect![[r#"
+        Error(
+            Rule(
+                "expression",
+                Measure,
+                Span {
+                    lo: 4,
+                    hi: 11,
+                },
+            ),
+        )
+    "#]],
+    );
+}
+
+#[test]
+fn initialized_creg() {
+    check(
+        parse,
+        "creg a[1] = measure $0;",
+        &expect![[r#"
+        Stmt [0-9]:
+            annotations: <empty>
+            kind: ClassicalDeclarationStmt [0-9]:
+                type: ScalarType [0-9]: BitType [0-9]:
+                    size: Expr [7-8]: Lit: Int(1)
+                ident: Ident [5-6] "a"
+                init_expr: <none>
+
+        [
+            Error(
+                Token(
+                    Semicolon,
+                    Eq,
+                    Span {
+                        lo: 10,
+                        hi: 11,
+                    },
+                ),
+            ),
+        ]"#]],
+    );
+}
+
+#[test]
+fn invalid_arrow_target() {
+    check(
+        parse,
+        "measure $0 -> creg a[1];",
+        &expect![[r#"
+        Error(
+            Rule(
+                "identifier",
+                Keyword(
+                    CReg,
+                ),
+                Span {
+                    lo: 14,
+                    hi: 18,
+                },
+            ),
+        )
+    "#]],
+    );
+    check(
+        parse,
+        "measure $0 -> bit[1] a;",
+        &expect![[r#"
+        Error(
+            Rule(
+                "identifier",
+                Type(
+                    Bit,
+                ),
+                Span {
+                    lo: 14,
+                    hi: 17,
+                },
+            ),
+        )
+    "#]],
+    );
+}
+
+#[test]
+fn measure_cant_be_used_in_sub_expressions() {
+    check(
+        parse,
+        "a = 2 * measure $0;",
+        &expect![[r#"
+        Error(
+            Rule(
+                "expression",
+                Measure,
+                Span {
+                    lo: 8,
+                    hi: 15,
+                },
+            ),
+        )
+    "#]],
+    );
+    check(
+        parse,
+        "a = (measure $0) + (measure $1);",
+        &expect![[r#"
+        Error(
+            Token(
+                Close(
+                    Paren,
+                ),
+                Measure,
+                Span {
+                    lo: 5,
+                    hi: 12,
+                },
+            ),
+        )
+    "#]],
+    );
+}
