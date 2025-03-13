5ecebde7fcb80f58daff21e7246672227525c29d
orpuente-MS <156957451+orpuente-MS@users.noreply.github.com>
2025-03-13 11:26:07 -0700

    Make control flow stmts use Stmt instead of List<Stmt> for their bodies. (#2226)
    If/else blocks, while, and for loops in QASM introduce new scopes only
    when their body enclosed by curly braces. To make lowering easier, this
    PR changes their bodies from `List<Stmt>` to `Stmt`. In the body
    enclosed in curly braces case, the Stmt will be of kind Block.

--- compiler/qsc_qasm3/src/parser/stmt/tests/while_loops.rs
+++ compiler/qsc_qasm3/src/parser/stmt/tests/while_loops.rs
@@ -20,14 +20,16 @@ fn simple_while() {
                         op: Neq
                         lhs: Expr [12-13]: Ident [12-13] "x"
                         rhs: Expr [17-18]: Lit: Int(2)
-                    block:
-                        Stmt [30-36]:
-                            annotations: <empty>
-                            kind: AssignStmt [30-36]:
-                                lhs: IndexedIdent [30-31]:
-                                    name: Ident [30-31] "a"
-                                    indices: <empty>
-                                rhs: Expr [34-35]: Lit: Int(0)"#]],
+                    body: Stmt [20-42]:
+                        annotations: <empty>
+                        kind: Block [20-42]:
+                            Stmt [30-36]:
+                                annotations: <empty>
+                                kind: AssignStmt [30-36]:
+                                    lhs: IndexedIdent [30-31]:
+                                        name: Ident [30-31] "a"
+                                        indices: <empty>
+                                    rhs: Expr [34-35]: Lit: Int(0)"#]],
     );
 }
 
@@ -41,7 +43,9 @@ fn empty_while() {
                 annotations: <empty>
                 kind: WhileLoop [0-15]:
                     condition: Expr [7-11]: Lit: Bool(true)
-                    block: <empty>"#]],
+                    body: Stmt [13-15]:
+                        annotations: <empty>
+                        kind: Block [13-15]: <empty>"#]],
     );
 }
 
@@ -60,14 +64,13 @@ fn while_stmt_body() {
                         op: Neq
                         lhs: Expr [12-13]: Ident [12-13] "x"
                         rhs: Expr [17-18]: Lit: Int(2)
-                    block:
-                        Stmt [28-34]:
-                            annotations: <empty>
-                            kind: AssignStmt [28-34]:
-                                lhs: IndexedIdent [28-29]:
-                                    name: Ident [28-29] "a"
-                                    indices: <empty>
-                                rhs: Expr [32-33]: Lit: Int(0)"#]],
+                    body: Stmt [28-34]:
+                        annotations: <empty>
+                        kind: AssignStmt [28-34]:
+                            lhs: IndexedIdent [28-29]:
+                                name: Ident [28-29] "a"
+                                indices: <empty>
+                            rhs: Expr [32-33]: Lit: Int(0)"#]],
     );
 }
 
@@ -88,17 +91,19 @@ fn while_loop_with_continue_stmt() {
                         op: Neq
                         lhs: Expr [12-13]: Ident [12-13] "x"
                         rhs: Expr [17-18]: Lit: Int(2)
-                    block:
-                        Stmt [30-36]:
-                            annotations: <empty>
-                            kind: AssignStmt [30-36]:
-                                lhs: IndexedIdent [30-31]:
-                                    name: Ident [30-31] "a"
-                                    indices: <empty>
-                                rhs: Expr [34-35]: Lit: Int(0)
-                        Stmt [45-54]:
-                            annotations: <empty>
-                            kind: ContinueStmt [45-54]"#]],
+                    body: Stmt [20-60]:
+                        annotations: <empty>
+                        kind: Block [20-60]:
+                            Stmt [30-36]:
+                                annotations: <empty>
+                                kind: AssignStmt [30-36]:
+                                    lhs: IndexedIdent [30-31]:
+                                        name: Ident [30-31] "a"
+                                        indices: <empty>
+                                    rhs: Expr [34-35]: Lit: Int(0)
+                            Stmt [45-54]:
+                                annotations: <empty>
+                                kind: ContinueStmt [45-54]"#]],
     );
 }
 
@@ -119,16 +124,100 @@ fn while_loop_with_break_stmt() {
                         op: Neq
                         lhs: Expr [12-13]: Ident [12-13] "x"
                         rhs: Expr [17-18]: Lit: Int(2)
-                    block:
-                        Stmt [30-36]:
-                            annotations: <empty>
-                            kind: AssignStmt [30-36]:
-                                lhs: IndexedIdent [30-31]:
-                                    name: Ident [30-31] "a"
-                                    indices: <empty>
-                                rhs: Expr [34-35]: Lit: Int(0)
-                        Stmt [45-51]:
-                            annotations: <empty>
-                            kind: BreakStmt [45-51]"#]],
+                    body: Stmt [20-57]:
+                        annotations: <empty>
+                        kind: Block [20-57]:
+                            Stmt [30-36]:
+                                annotations: <empty>
+                                kind: AssignStmt [30-36]:
+                                    lhs: IndexedIdent [30-31]:
+                                        name: Ident [30-31] "a"
+                                        indices: <empty>
+                                    rhs: Expr [34-35]: Lit: Int(0)
+                            Stmt [45-51]:
+                                annotations: <empty>
+                                kind: BreakStmt [45-51]"#]],
+    );
+}
+
+#[test]
+fn single_stmt_while_stmt() {
+    check(
+        parse,
+        "while (x) z q;",
+        &expect![[r#"
+            Stmt [0-14]:
+                annotations: <empty>
+                kind: WhileLoop [0-14]:
+                    condition: Expr [7-8]: Ident [7-8] "x"
+                    body: Stmt [10-14]:
+                        annotations: <empty>
+                        kind: GateCall [10-14]:
+                            modifiers: <empty>
+                            name: Ident [10-11] "z"
+                            args: <empty>
+                            duration: <none>
+                            qubits:
+                                IndexedIdent [12-13]:
+                                    name: Ident [12-13] "q"
+                                    indices: <empty>"#]],
+    );
+}
+
+#[test]
+fn annotations_in_single_stmt_while_stmt() {
+    check(
+        parse,
+        "
+    while (x)
+        @foo
+        @bar
+        x = 5;",
+        &expect![[r#"
+            Stmt [5-55]:
+                annotations: <empty>
+                kind: WhileLoop [5-55]:
+                    condition: Expr [12-13]: Ident [12-13] "x"
+                    body: Stmt [23-55]:
+                        annotations:
+                            Annotation [23-27]:
+                                identifier: "foo"
+                                value: <none>
+                            Annotation [36-40]:
+                                identifier: "bar"
+                                value: <none>
+                        kind: AssignStmt [49-55]:
+                            lhs: IndexedIdent [49-50]:
+                                name: Ident [49-50] "x"
+                                indices: <empty>
+                            rhs: Expr [53-54]: Lit: Int(5)"#]],
+    );
+}
+
+#[test]
+fn nested_single_stmt_while_stmt() {
+    check(
+        parse,
+        "while (x) while (y) z q;",
+        &expect![[r#"
+            Stmt [0-24]:
+                annotations: <empty>
+                kind: WhileLoop [0-24]:
+                    condition: Expr [7-8]: Ident [7-8] "x"
+                    body: Stmt [10-24]:
+                        annotations: <empty>
+                        kind: WhileLoop [10-24]:
+                            condition: Expr [17-18]: Ident [17-18] "y"
+                            body: Stmt [20-24]:
+                                annotations: <empty>
+                                kind: GateCall [20-24]:
+                                    modifiers: <empty>
+                                    name: Ident [20-21] "z"
+                                    args: <empty>
+                                    duration: <none>
+                                    qubits:
+                                        IndexedIdent [22-23]:
+                                            name: Ident [22-23] "q"
+                                            indices: <empty>"#]],
     );
 }
