1778307756977cc846421d80ab66b4f77e7c39ee
Ian Davis <Ian.F.Davis@microsoft.com>
2025-03-13 17:06:44 -0700

    Enums used with Qiskit can now be deep copied (#2224)

--- pip/tests/test_qsharp.py
+++ pip/tests/test_qsharp.py
@@ -498,38 +498,6 @@ def test_run_with_invalid_shots_produces_error() -> None:
         assert False
 
 
-def test_target_profile_str_values_match_enum_values() -> None:
-    target_profile = qsharp.TargetProfile.Base
-    str_value = str(target_profile)
-    assert str_value == "Base"
-    target_profile = qsharp.TargetProfile.Adaptive_RI
-    str_value = str(target_profile)
-    assert str_value == "Adaptive_RI"
-    target_profile = qsharp.TargetProfile.Adaptive_RIF
-    str_value = str(target_profile)
-    assert str_value == "Adaptive_RIF"
-    target_profile = qsharp.TargetProfile.Unrestricted
-    str_value = str(target_profile)
-    assert str_value == "Unrestricted"
-
-
-def test_target_profile_from_str_match_enum_values() -> None:
-    target_profile = qsharp.TargetProfile.Base
-    str_value = str(target_profile)
-    assert qsharp.TargetProfile.from_str(str_value) == target_profile
-    target_profile = qsharp.TargetProfile.Adaptive_RI
-    str_value = str(target_profile)
-    assert qsharp.TargetProfile.from_str(str_value) == target_profile
-    target_profile = qsharp.TargetProfile.Adaptive_RIF
-    str_value = str(target_profile)
-    assert qsharp.TargetProfile.from_str(str_value) == target_profile
-    target_profile = qsharp.TargetProfile.Unrestricted
-    str_value = str(target_profile)
-    assert qsharp.TargetProfile.from_str(str_value) == target_profile
-    with pytest.raises(ValueError):
-        qsharp.TargetProfile.from_str("Invalid")
-
-
 def test_callables_exposed_into_env() -> None:
     qsharp.init()
     qsharp.eval("function Four() : Int { 4 }")
