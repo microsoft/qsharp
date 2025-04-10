af709baf12ac7b02848c018962a122e4ebe77284
Mine Starks <mineyalc@microsoft.com>
2025-04-10 14:01:09 -0700

    add updatingDocument flag

--- vscode/src/circuitEditor.ts
+++ vscode/src/circuitEditor.ts
@@ -6,7 +6,7 @@ import * as vscode from "vscode";
 
 export class CircuitEditorProvider implements vscode.CustomTextEditorProvider {
   private static readonly viewType = "qsharp-webview.circuit";
-  savedVersion: number | undefined;
+  updatingDocument: boolean = false;
 
   public static register(context: vscode.ExtensionContext): vscode.Disposable {
     log.info("Registering CircuitEditorProvider");
@@ -68,26 +68,23 @@ export class CircuitEditorProvider implements vscode.CustomTextEditorProvider {
     };
 
     // Update the webview when the text document changes
-    // const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(
-    //   (event) => {
-    //     if (event.document.uri.toString() === document.uri.toString()) {
-    //       if (
-    //         this.savedVersion !== undefined &&
-    //         event.document.version > this.savedVersion
-    //       ) {
-    //         // Update the webview with the new document content
-    //         updateWebview();
-    //       }
-    //     }
-    //   },
-    // );
+    const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(
+      (event) => {
+        if (event.document.uri.toString() === document.uri.toString()) {
+          if (!this.updatingDocument && event.contentChanges.length > 0) {
+            // Update the webview with the new document content
+            updateWebview();
+          }
+        }
+      },
+    );
+    // Dispose of the event listener when the webview is closed
+    webviewPanel.onDidDispose(() => {
+      changeDocumentSubscription.dispose();
+    });
 
-    // // Dispose of the event listener when the webview is closed
-    // webviewPanel.onDidDispose(() => {
-    //   changeDocumentSubscription.dispose();
-    // });
-
-    updateWebview();
+    // this initial call is unnecessary since the webview already calls "read" on load
+    // updateWebview();
   }
 
   private getHtmlForWebview(webview: vscode.Webview): string {
@@ -158,7 +155,8 @@ export class CircuitEditorProvider implements vscode.CustomTextEditorProvider {
       new vscode.Range(0, 0, document.lineCount, 0),
       circuit.trim(),
     );
+    this.updatingDocument = true;
     await vscode.workspace.applyEdit(edit);
-    this.savedVersion = document.version;
+    this.updatingDocument = false;
   }
 }
