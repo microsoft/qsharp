b80d3935b8fc740cb10104ad145463faef0121a7
Oscar Puente <oscarpuente@microsoft.com>
2025-04-10 14:27:29 -0700

    add 4 qubits benchmark

--- compiler/qsc_qasm3/benches/rgqft_multiplier.rs
+++ compiler/qsc_qasm3/benches/rgqft_multiplier.rs
@@ -7,11 +7,11 @@ use qsc_qasm3::{
     ProgramType, QubitSemantics,
 };
 
-const SOURCE: &str = include_str!("./rgqft_multiplier.qasm");
+pub fn rgqft_multiplier_1q(c: &mut Criterion) {
+    const SOURCE: &str = include_str!("./rgqft_multiplier_1q.qasm");
 
-pub fn rgqft_multiplier(c: &mut Criterion) {
-    c.bench_function("rgqft_multiplier sample compilation", |b| {
-        let all_sources = [("rgqft_multiplier.qasm".into(), SOURCE.into())];
+    c.bench_function("rgqft_multiplier_1q sample compilation", |b| {
+        let all_sources = [("rgqft_multiplier_1q.qasm".into(), SOURCE.into())];
         let mut resolver = InMemorySourceResolver::from_iter(all_sources);
 
         b.iter(move || {
@@ -22,10 +22,40 @@ pub fn rgqft_multiplier(c: &mut Criterion) {
                 Some("Test".into()),
                 None,
             );
-            black_box(compile_to_qsharp_ast_with_config(SOURCE, "", Some(&mut resolver), config))
+            black_box(compile_to_qsharp_ast_with_config(
+                SOURCE,
+                "",
+                Some(&mut resolver),
+                config,
+            ))
         });
     });
 }
 
-criterion_group!(benches, rgqft_multiplier);
+pub fn rgqft_multiplier_4q(c: &mut Criterion) {
+    const SOURCE: &str = include_str!("./rgqft_multiplier_4q.qasm");
+
+    c.bench_function("rgqft_multiplier_4q sample compilation", |b| {
+        let all_sources = [("rgqft_multiplier_4q.qasm".into(), SOURCE.into())];
+        let mut resolver = InMemorySourceResolver::from_iter(all_sources);
+
+        b.iter(move || {
+            let config = CompilerConfig::new(
+                QubitSemantics::Qiskit,
+                OutputSemantics::Qiskit,
+                ProgramType::File,
+                Some("Test".into()),
+                None,
+            );
+            black_box(compile_to_qsharp_ast_with_config(
+                SOURCE,
+                "",
+                Some(&mut resolver),
+                config,
+            ))
+        });
+    });
+}
+
+criterion_group!(benches, rgqft_multiplier_1q, rgqft_multiplier_4q);
 criterion_main!(benches);
