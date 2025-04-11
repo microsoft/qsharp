fe66bc3ef78a31e38c501df30cb8d5257a1cbfb6
Ian Davis <ian.f.davis@microsoft.com>
2025-04-11 10:58:10 -0700

    Adding more compilation and samples for fuzzing

--- fuzz/fuzz_targets/qasm3.rs
+++ fuzz/fuzz_targets/qasm3.rs
@@ -8,10 +8,57 @@ allocator::assign_global!();
 #[cfg(feature = "do_fuzz")]
 use libfuzzer_sys::fuzz_target;
 
+use qsc::{
+    compile::compile_ast,
+    hir::PackageId,
+    qasm::{
+        compile_to_qsharp_ast_with_config, io::InMemorySourceResolver, package_store_with_qasm,
+        CompilerConfig, OutputSemantics, ProgramType, QubitSemantics,
+    },
+    target::Profile,
+    PackageStore, PackageType,
+};
+
 fn compile(data: &[u8]) {
     if let Ok(fuzzed_code) = std::str::from_utf8(data) {
-        let mut resolver = qsc::qasm::io::InMemorySourceResolver::from_iter([]);
-        let _ = qsc::qasm::parser::parse_source(fuzzed_code, "fuzz.qasm", &mut resolver);
+        thread_local! {
+            static STORE_STD: (PackageId, PackageId, PackageStore) = {
+                package_store_with_qasm(Profile::Unrestricted.into())
+            };
+        }
+        STORE_STD.with(|(stdid, qasmid, store)| {
+            let mut resolver = InMemorySourceResolver::from_iter([]);
+            let config = CompilerConfig::new(
+                QubitSemantics::Qiskit,
+                OutputSemantics::OpenQasm,
+                ProgramType::File,
+                Some("Fuzz".into()),
+                None,
+            );
+
+            let unit = compile_to_qsharp_ast_with_config(
+                fuzzed_code,
+                "fuzz.qasm",
+                Some(&mut resolver),
+                config,
+            );
+            let (sources, _, package, _) = unit.into_tuple();
+
+            let dependencies = vec![
+                (PackageId::CORE, None),
+                (*stdid, None),
+                (*qasmid, Some("QasmStd".into())),
+            ];
+
+            let (mut _unit, _errors) = compile_ast(
+                store,
+                &dependencies,
+                package,
+                sources,
+                PackageType::Lib,
+                Profile::Unrestricted.into(),
+            );
+        });
     }
 }
 
