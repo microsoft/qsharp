6eb99dd66bd26c0c9be75c857e3f1b4e9af3103f
Oscar Puente <oscarpuente@microsoft.com>
2025-04-21 10:18:09 -0700

    add license header

--- compiler/qsc_linter/src/lint_groups.rs
+++ compiler/qsc_linter/src/lint_groups.rs
@@ -1,5 +1,7 @@
-use serde::{Deserialize, Serialize};
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
+use serde::{Deserialize, Serialize};
 use crate::{LintKind, LintLevel};
 
 /// End-user configuration for a lint group.
