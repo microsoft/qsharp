30fe6a69f85aa7b477ab5378f7b7e449c3ccbea8
Bill Ticehurst <billti@microsoft.com>
2025-04-24 12:23:03 -0700

    Use LaTeX fonts for math symbols in circuit diagrams (#2327)
    Circuit rendering now appears as per the below in VS Code (and have
    tested in JupyterLabs in both Safari and Edge browsers, and in light and
    dark themes).
    
    <img width="596" alt="image"
    src="https://github.com/user-attachments/assets/955d0304-9107-41a0-a874-765447c914a9"
    />

--- npm/qsharp/ux/circuit-vis/formatters/inputFormatter.ts
+++ npm/qsharp/ux/circuit-vis/formatters/inputFormatter.ts
@@ -10,6 +10,7 @@ import {
   classicalRegHeight,
 } from "../constants";
 import { group, text } from "./formatUtils";
+import { mathChars } from "../utils";
 
 /**
  * `formatInputs` takes in an array of Qubits and outputs the SVG string of formatted
@@ -71,38 +72,15 @@ const formatInputs = (
 const _qubitInput = (y: number, subscript?: string): SVGElement => {
   const el: SVGElement = text("", leftPadding, y, 16);
 
-  // Create the main text node
-  const mainText = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "tspan",
-  );
-  mainText.textContent = "|𝜓";
+  const subtext = subscript
+    ? `<tspan baseline-shift="sub" font-size="65%">${subscript}</tspan>`
+    : "";
 
-  // Create the subscript node if provided
-  if (subscript) {
-    const subscriptText = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "tspan",
-    );
-    subscriptText.textContent = subscript;
-    subscriptText.setAttribute("baseline-shift", "sub");
-    subscriptText.setAttribute("font-size", "65%");
-    mainText.appendChild(subscriptText);
-  }
-
-  // Add the closing part of the text
-  const closingText = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "tspan",
-  );
-  closingText.textContent = "⟩";
-
-  // Append all parts to the main SVG text element
-  el.appendChild(mainText);
-  el.appendChild(closingText);
+  el.innerHTML = `|<tspan class="qs-mathtext">${mathChars.psi}</tspan>${subtext}${mathChars.rangle}</tspan>`;
 
   el.setAttribute("text-anchor", "start");
   el.setAttribute("dominant-baseline", "middle");
+  el.classList.add("qs-maintext");
   return el;
 };
 
