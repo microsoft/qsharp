c8150772afce75621f2ccdb65d30135345f87b5e
DmitryVasilevsky <60718360+DmitryVasilevsky@users.noreply.github.com>
2025-06-03 01:11:53 -0700

    Added ApplyQPE and ApplyOperationPowerCA to Canon namespace (#2473)
    - Added ApplyQPE and ApplyOperationPowerCA to Canon namespace.
    - Added doc comments
    - Added tests
    
    ApplyQPE along with the previously implemented ApplyQFT are useful
    primitives for quantum algorithms. To keep these implementations simple
    and avoid performance penalty no swaps are performed on arguments or
    results. In most cases such results can be used without problems. Note,
    that ApplyQFT and ApplyQPE work with little-endian registers.
    
    ---------
    
    Co-authored-by: Dmitry Vasilevsky <dmitryv@microsoft.com>

--- compiler/qsc_partial_eval/src/tests/qubits.rs
+++ compiler/qsc_partial_eval/src/tests/qubits.rs
@@ -377,6 +377,6 @@ fn qubit_relabel_in_dynamic_block_triggers_capability_error() {
 
     assert_error(
         &error,
-        &expect!["CapabilityError(UseOfDynamicQubit(Span { lo: 60173, hi: 60186 }))"],
+        &expect!["CapabilityError(UseOfDynamicQubit(Span { lo: 62782, hi: 62795 }))"],
     );
 }
