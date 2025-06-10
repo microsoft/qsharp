7a1a6d910322b33eace52803568a82e2ebe2c397
Oscar Puente <oscarpuente@microsoft.com>
2025-06-10 14:25:19 -0700

    add tests/builtin/log.rs

--- compiler/qsc_qasm/src/semantic/tests/expression/builtin_functions.rs
+++ compiler/qsc_qasm/src/semantic/tests/expression/builtin_functions.rs
@@ -8,6 +8,7 @@ mod ceiling;
 mod cos;
 mod exp;
 mod floor;
+mod log;
 mod mod_;
 mod popcount;
 mod pow;
@@ -257,34 +258,5 @@ fn nested_builtin_call_succeeds() {
                                             const_value: Int(7)
                                             kind: SymbolId(9)
         "#]],
-    );
-}
-
-// ----------------------------------
-// log
-
-#[test]
-fn log() {
-    let source = "
-        log(2.);
-    ";
-
-    check_stmt_kinds(
-        source,
-        &expect![[r#"
-        ExprStmt [9-17]:
-            expr: Expr [9-16]:
-                ty: const float
-                const_value: Float(0.6931471805599453)
-                kind: BuiltinFunctionCall [9-16]:
-                    fn_name_span: [9-12]
-                    name: log
-                    function_ty: def (const float) -> const float
-                    args:
-                        Expr [13-15]:
-                            ty: const float
-                            const_value: Float(2.0)
-                            kind: Lit: Float(2.0)
-    "#]],
     );
 }
