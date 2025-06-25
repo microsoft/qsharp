3ed3478d4c5c4fc6e3a32a6f19b3cdff04d094b6
Oscar Puente <oscarpuente@microsoft.com>
2025-06-25 10:14:36 -0700

    allow clippy::large_stack_arrays in openqasm crate

--- source/compiler/qsc_qasm/src/lib.rs
+++ source/compiler/qsc_qasm/src/lib.rs
@@ -1,6 +1,19 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+// When running `build.py` on the repo, clippy fails in this module with
+// `clippy::large_stack_arrays`. Note that the `build.py` script runs the command
+// `cargo clippy --all-targets --all-features -- -D warnings`. Just running
+// `cargo clippy` won't trigger the failure. If you want to reproduce the failure
+// with the minimal command possible, you can run `cargo clippy --test -- -D warnings`.
+//
+// We tried to track down the error, but it is non-deterministic. Our assumpution
+// is that clippy is running out of stack memory because of how many and how large
+// the static strings in the test modules are.
+//
+// Decision: Based on this, we decided to disable the `clippy::large_stack_arrays` lint.
+#![allow(clippy::large_stack_arrays)]
+
 mod ast_builder;
 pub mod compiler;
 mod convert;
