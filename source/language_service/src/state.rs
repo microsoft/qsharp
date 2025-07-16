e63b31fe8bc1d6933e036d9276d759d671a88aeb
Scott Carda <Scott.Carda@microsoft.com>
2025-07-16 13:50:49 -0700

    fist attempt, remove target_profile as a passed in param, recognize param, get profile from params

--- source/language_service/src/state.rs
+++ source/language_service/src/state.rs
@@ -309,7 +309,6 @@ impl<'a> CompilationStateUpdater<'a> {
             let compilation = match loaded_project.project_type {
                 ProjectType::OpenQASM(sources) => Compilation::new_qasm(
                     configuration.package_type,
-                    configuration.target_profile,
                     sources,
                     loaded_project.errors,
                     &loaded_project.name,
