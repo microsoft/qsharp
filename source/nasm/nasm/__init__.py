ca21f2f0c0283eff7deaa56257dcf825beb50352
Bill Ticehurst <billti@microsoft.com>
2025-07-30 00:34:00 -0700

    Initial noisy qir run

---
+++ source/nasm/nasm/__init__.py
@@ -0,0 +1,104 @@
+from functools import lru_cache
+from qsharp.noisy_simulator import StateVectorSimulator, Instrument, Operation
+from qsharp._qsharp import QirInputData
+from .noise_model import (
+    NoiseModel,
+    create_default_noise_model,
+    amplitude_damping_kraus,
+    rz_for_theta,
+)
+from .ops import QirOps
+
+
+# This function is used to get the RZ operator for a given angle.
+# It uses lru_cache to cache the results for performance.
+# rev_list is used for cache busting when the noise model is updated.
+@lru_cache(maxsize=64)
+def get_rz_operator(model: NoiseModel, rev: int, theta: float) -> Operation:
+    op_matrix = rz_for_theta(theta)
+    noise_matrices = model.get_noise_matrices_for_gate("rz")
+    combined_matrices = model.apply_unitary_to_kraus(op_matrix, noise_matrices)
+    return Operation(combined_matrices)
+
+
+class Simulator:
+    def __init__(self, noise_model: NoiseModel = create_default_noise_model()):
+        self.noise_model = noise_model
+
+    def run(self, code: QirInputData | str, shots: int = 1):
+        seed = 0
+        if isinstance(code, QirInputData):
+            code = str(code)
+
+        ops = QirOps(code)
+        self.shots = []
+
+        for shot in range(shots):
+            seed += 1
+            self.simulator = StateVectorSimulator(ops.qubits, seed)
+
+            # Do this here rather than init, so the noise model can be modified between runs.
+            (gates, instruments) = self.noise_model.get_noisy_gates_and_instruments()
+
+            self.operations: dict[str, Operation] = {}
+            self.measurements: dict[str, Instrument] = {}
+            self.outcomes: dict[str, list[str]] = {}
+            self.results = {}
+            self.reported_results = []
+
+            for gate_name in gates.keys():
+                self.operations[gate_name.lower()] = Operation(gates[gate_name])
+
+            for measurement_name in instruments.keys():
+                projectors = []
+                results_str: list[str] = []
+                for choice in instruments[measurement_name]:
+                    projectors.append(Operation(choice[0]))
+                    results_str.append(choice[1])
+                self.measurements[measurement_name.lower()] = Instrument(projectors)
+                self.outcomes[measurement_name.lower()] = results_str
+
+            for op in ops.ops:
+                if op.name == "initialize":
+                    pass
+                elif op.name == "rz":
+                    angle = op.args[0]
+                    sim_op = get_rz_operator(
+                        self.noise_model, self.noise_model.rev, angle
+                    )
+                    self.simulator.apply_operation(sim_op, [op.args[1]])
+                elif op.name in self.operations:
+                    self.apply_operation(op.name, op.args)
+                elif op.name in self.measurements:
+                    # We only support a single qubit and result for now
+                    self.apply_instrument(op.name, [op.args[0]], [op.args[1]])
+                elif op.name == "array_record_output":
+                    pass
+                elif op.name == "result_record_output":
+                    self.reported_results.append(self.results.get(op.args[0]))
+                else:
+                    raise ValueError(f"Unsupported operation {op.name} in QIR code")
+
+            result_list = [self.results.get(i) for i in range(ops.results)]
+            self.shots.append("".join(result_list))
+        return self.shots
+
+    def apply_operation(self, op_name: str, qubits: list[int]):
+        noisy_gate = self.operations.get(op_name)
+        if noisy_gate is None:
+            raise f"Unsupported operation {op_name}"
+
+        self.simulator.apply_operation(noisy_gate, qubits[::-1])
+
+    def apply_instrument(self, op_name: str, qubits: list[int], resultReg: list[int]):
+        # TODO: Handle multiple result registers (e.g. gadget to measure 2 qubits in X basis)
+        instrument = self.measurements.get(op_name)
+        outcomes = self.outcomes.get(op_name)
+        if instrument is not None:
+            outcome_idx = self.simulator.sample_instrument(instrument, qubits[::-1])
+            outcome = outcomes[outcome_idx]
+            if outcome is None:
+                raise f"No outcome for index {outcome_idx} returned by instrument {op_name}"
+            self.results[resultReg[0]] = outcome
+        else:
+            raise "Unsupported instrument"
---
+++ source/nasm/nasm/ops.py
@@ -0,0 +1,136 @@
+import re
+
+
+class Op:
+    # type will be 'qir' or 'rt'
+    # name will be the operation name, such as 'sx', 'rz', 'cx', 'mz', 'result_record_output', etc.
+    # args will be a list of arguments, such as qubit or result ids (int), rangles (float), etc.
+    def __init__(self, type: str, name: str, args: list):
+        self.type = type
+        self.name = name
+        self.args = args
+
+
+class QirOps:
+    def __init__(self, ir_text: str):
+        self.ir = ir_text
+
+        # We expect attribute 0 to have the required attributes, and be formatted something like:
+        #    attributes #0 = { "entry_point" "output_labeling_schema" "qir_profiles"="base_profile" "required_num_qubits"="2" "required_num_results"="2" }
+
+        attribute_0 = re.search(
+            r'^\s*attributes #0 = \{.*"qir_profiles"="(.+?)".*"required_num_qubits"="(\d+)".*"required_num_results"="(\d+)".*\}\s*$',
+            ir_text,
+            re.MULTILINE,
+        )
+        if not attribute_0:
+            raise ValueError("QIR does not contain the required attributes")
+
+        self.profile = attribute_0.group(1)
+        self.qubits = int(attribute_0.group(2))
+        self.results = int(attribute_0.group(3))
+
+        if self.profile != "base_profile":
+            raise ValueError(f"Profile is not base_profile: {self.profile}")
+
+        if self.qubits < 0 or self.results < 0:
+            raise ValueError(f"Qubit and result count must be greater than 0")
+
+        # Extract the body of the entry point function
+        #   First non-capture group is the #0 attribute and <block_name>: label
+        #   With no branching, end at the 'ret' instruction
+        entry_point_body = re.search(
+            r"(?:define void @\S+\(\) #0\s+{\n\w+:\n)((.|\n)*)(?:^\s*ret (i64|void))",
+            ir_text,
+            re.MULTILINE,
+        )
+        if not entry_point_body:
+            raise ValueError("QIR does not contain entry point function body")
+
+        self.entry_point_body = entry_point_body.group(1)
+
+        self.ops = [
+            self.parse_line(line)
+            for line in self.entry_point_body.splitlines()
+            if line.strip()
+        ]
+
+    """
+    A program such as
+
+      operation Main() : Result[] {
+        use q = Qubit[2];
+        SX(q[0]);
+        Rz(0.5, q[1]);
+        CZ(q[0], q[1]);
+        return [M(q[0]), M(q[1])];
+      }
+
+    Results in QIR such as:
+
+      call void @__quantum__qis__sx__body(%Qubit* inttoptr (i64 0 to %Qubit*))
+      call void @__quantum__qis__rz__body(double 0.5, %Qubit* inttoptr (i64 1 to %Qubit*))
+      call void @__quantum__qis__cz__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Qubit* inttoptr (i64 1 to %Qubit*))
+      call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 0 to %Qubit*), %Result* inttoptr (i64 0 to %Result*))
+      call void @__quantum__qis__m__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
+      call void @__quantum__rt__array_record_output(i64 2, i8* null)
+      call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 0 to %Result*), i8* null)
+      call void @__quantum__rt__result_record_output(%Result* inttoptr (i64 1 to %Result*), i8* null)
+
+    Note: It may (should) also have an initial "call void @__quantum__rt__initialize(i8* null)" call
+    """
+
+    def parse_line(self, line: str) -> Op:
+        call_groups = re.search(
+            r"^\s*call\s+void\s+@__quantum__(qis|rt)__(\w+?)\((.*)\)\s*$", line
+        )
+        if not call_groups or len(call_groups.groups()) != 3:
+            raise ValueError(f"Invalid QIR line: {line}")
+
+        name = call_groups.group(2)
+        if name.endswith("__body"):
+            name = name[:-6]
+
+        # Normalize 'm' to 'mz' for measurement operations
+        if name == "m":
+            name = "mz"
+
+        return Op(
+            type=call_groups.group(1),
+            name=name,
+            args=self.parse_args(call_groups.group(3)),
+        )
+
+    def parse_args(self, args: str):
+        # Input will be something like:
+        #    double 0.5, %Qubit* inttoptr (i64 1 to %Qubit*)
+        #    %Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*)
+        #    %Result* inttoptr (i64 0 to %Result*), i8* null
+        results = []
+        split_args = [arg.strip() for arg in args.split(",")]
+        for arg in split_args:
+            if arg.startswith("%Qubit* inttoptr (i64 ") or arg.startswith(
+                "%Result* inttoptr (i64 "
+            ):
+                # Extract the qubit or result id from the inttoptr
+                match = re.search(r"\(i64 (\d+) to \%", arg)
+                if match:
+                    results.append(int(match.group(1)))
+                else:
+                    raise ValueError(f"Invalid QIR argument: {arg}")
+            elif arg.startswith("double ") or arg.startswith("i64 "):
+                # Extract the number from the double or i64 argument
+                match = re.search(r"^(?:double|i64)\s+(.*)$", arg)
+                if match:
+                    if arg.startswith("double "):
+                        results.append(float(match.group(1)))
+                    else:
+                        results.append(int(match.group(1)))
+                else:
+                    raise ValueError(f"Invalid QIR argument: {arg}")
+            elif arg == "i8* null":
+                # Shouldn't get non null i8* arguments in base QIR (i.e. no strings)
+                results.append(None)
+            else:
+                raise ValueError(f"Invalid QIR argument: {arg}")
+        return results
