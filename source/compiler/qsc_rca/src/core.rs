d8e7c1a9b6b9419744bea421dead160168e87577
Stefan J. Wernli <swernli@microsoft.com>
2025-08-12 15:55:34 -0700

    Allow multi-op cycles rather than just strict recursion

--- source/compiler/qsc_rca/src/core.rs
+++ source/compiler/qsc_rca/src/core.rs
@@ -504,7 +504,6 @@ impl<'a> Analyzer<'a> {
             if quantum_properties
                 .runtime_features
                 .contains(RuntimeFeatureFlags::CyclicOperationSpec)
-                && Some(&callee.item) != self.get_current_context().get_current_item_id()
             {
                 quantum_properties
                     .runtime_features
@@ -565,37 +564,24 @@ impl<'a> Analyzer<'a> {
             return CallComputeKind::Regular(compute_kind);
         };
 
-        if Some(&callee.item) == self.get_current_context().get_current_item_id() {
+        if self.callee_in_active_contexts(&callee) {
             assert_eq!(
                 expr_type,
                 &Ty::UNIT,
                 "output type for allowed recursive call should be Unit"
             );
 
-            if Some(callee.functor_app.functor_set_value())
-                == self.get_current_context().get_current_functor_set()
-            {
-                // This is a recursive call to the current item specialization, which we allow with some deferred
-                // capabilities checks at runtime. We treat the call as an unresolved callee, like above,
-                // such that partial evaluation will perform extra validation on the capabilities at runtime.
-                // This covers the corner case where a recursive call is made with a dynamic argument whose
-                // type is allowed to be dynamic but whose usage in later recursion could require additional
-                // capabilities.
-                self.get_current_application_instance_mut()
-                    .unresolved_callee_exprs
-                    .push(callee_expr_id);
-                return CallComputeKind::Regular(ComputeKind::Quantum(QuantumProperties {
-                    runtime_features: RuntimeFeatureFlags::CallToUnresolvedCallee,
-                    value_kind: ValueKind::Element(RuntimeKind::Static),
-                }));
-            }
-        }
-
-        // This is a call into a different specialization of the same item or a different item. Check the context
-        // to see if the current specialization is already present, in which case this is a cycle.
-        if self.in_cyclic_context() {
+            // This is a recursive call, which we allow with some deferred capabilities checks at runtime.
+            // We treat the call as an unresolved callee, like above, such that partial evaluation will perform
+            // extra validation on the capabilities at runtime.
+            // This covers the corner case where a recursive call is made with a dynamic argument whose
+            // type is allowed to be dynamic but whose usage in later recursion could require additional
+            // capabilities.
+            self.get_current_application_instance_mut()
+                .unresolved_callee_exprs
+                .push(callee_expr_id);
             return CallComputeKind::Regular(ComputeKind::Quantum(QuantumProperties {
-                runtime_features: RuntimeFeatureFlags::CyclicOperationSpec,
+                runtime_features: RuntimeFeatureFlags::CallToUnresolvedCallee,
                 value_kind: ValueKind::Element(RuntimeKind::Static),
             }));
         }
@@ -1730,33 +1716,14 @@ impl<'a> Analyzer<'a> {
         }
     }
 
-    /// Checks if the analyzer is currently in a cyclic context by
-    /// examining the active contexts stack. Returns true if the current
-    /// item specialization appears in the stack.
-    fn in_cyclic_context(&self) -> bool {
-        let (current_context, rest_context) = self
-            .active_contexts
-            .split_last()
-            .expect("should have at least one context");
-        for context in rest_context.iter().rev() {
-            match (context, current_context) {
-                (AnalysisContext::Item(item), AnalysisContext::Item(current_item))
-                    if item.id == current_item.id
-                        && item
-                            .current_spec_context
-                            .as_ref()
-                            .map(|s| s.functor_set_value)
-                            == current_item
-                                .current_spec_context
-                                .as_ref()
-                                .map(|s| s.functor_set_value) =>
-                {
-                    return true;
-                }
-                _ => {}
-            }
-        }
-        false
+    /// Checks whether the given callee is present in any of the active contexts, which indicates we are about
+    /// to perform a call into a cycle.
+    fn callee_in_active_contexts(&self, callee: &Callee) -> bool {
+        self.active_contexts.iter().any(|context| {
+            matches!(context, AnalysisContext::Item(item)
+                if item.id == callee.item
+                    && item.current_spec_context.as_ref().map(|s| s.functor_set_value) == Some(callee.functor_app.functor_set_value()))
+        })
     }
 }
 
@@ -2137,22 +2104,6 @@ impl AnalysisContext {
             Self::TopLevel(top_level_context) => {
                 top_level_context.get_current_application_instance_mut()
             }
-        }
-    }
-
-    pub fn get_current_item_id(&self) -> Option<&StoreItemId> {
-        match self {
-            Self::Item(item_context) => Some(&item_context.id),
-            Self::TopLevel(_) => None,
-        }
-    }
-
-    pub fn get_current_functor_set(&self) -> Option<FunctorSetValue> {
-        match self {
-            Self::Item(item_context) => {
-                Some(item_context.get_current_spec_context().functor_set_value)
-            }
-            Self::TopLevel(_) => None,
         }
     }
 }
