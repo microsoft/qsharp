fdb27feca3c9cdbdfaef8e1f791b0e7650f26b34
Ian Davis <ian.f.davis@microsoft.com>
2025-08-18 16:02:39 -0700

    Adding tests and fixup

--- source/compiler/qsc_qasm/src/semantic/types/tests.rs
+++ source/compiler/qsc_qasm/src/semantic/types/tests.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use crate::tests::check_qasm_to_qsharp;
+use crate::tests::{check_qasm_to_qsharp, compile_qasm_to_qsharp_operation};
 use expect_test::expect;
 
 #[test]
@@ -36,4 +36,66 @@ fn sliced_type_has_right_dimensions() {
         mutable arr_2 = arr_1[1..3];
     "#]],
     );
+}
+
+#[test]
+fn bigint_output_is_initially_zero_l() {
+    let source = "
+        input uint[128] input_var;
+        output uint[128] output_var;
+        output_var = input_var;
+    ";
+
+    let expect = expect![[r#"
+        operation Test(input_var : BigInt) : BigInt {
+            import Std.OpenQASM.Intrinsic.*;
+            mutable output_var : BigInt = 0L;
+            set output_var = input_var;
+            output_var
+        }
+    "#]];
+    match compile_qasm_to_qsharp_operation(source) {
+        Ok(qsharp) => {
+            expect.assert_eq(&qsharp);
+        }
+        Err(errors) => {
+            let buffer = errors
+                .iter()
+                .map(|e| format!("{e:?}"))
+                .collect::<Vec<_>>()
+                .join("\n");
+            expect.assert_eq(&buffer);
+        }
+    }
+}
+
+#[test]
+fn complex_input_and_output_is_mapped_correctly() {
+    let source = "
+        input complex input_var;
+        output complex output_var;
+        output_var = input_var;
+    ";
+
+    let expect = expect![[r#"
+        operation Test(input_var : Std.Math.Complex) : Std.Math.Complex {
+            import Std.OpenQASM.Intrinsic.*;
+            mutable output_var = Std.Math.Complex(0., 0.);
+            set output_var = input_var;
+            output_var
+        }
+    "#]];
+    match compile_qasm_to_qsharp_operation(source) {
+        Ok(qsharp) => {
+            expect.assert_eq(&qsharp);
+        }
+        Err(errors) => {
+            let buffer = errors
+                .iter()
+                .map(|e| format!("{e:?}"))
+                .collect::<Vec<_>>()
+                .join("\n");
+            expect.assert_eq(&buffer);
+        }
+    }
 }
--- source/pip/tests/test_qasm.py
+++ source/pip/tests/test_qasm.py
@@ -163,6 +163,205 @@ def test_import_can_declare_callables_from_fragments() -> None:
     assert qsharp_eval("Foo()") == 42
 
 
+def test_import_supported_bit_input_and_output_types() -> None:
+    source = """
+        input bit input_var;
+        output bit output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = Result.One
+    assert code.program(input) == input
+    input = Result.Zero
+    assert code.program(input) == input
+
+    input = True
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = False
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = 1
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = 0
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+
+
+def test_import_supported_bool_input_and_output_types() -> None:
+    source = """
+        input bool input_var;
+        output bool output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = True
+    assert code.program(input) == input
+    input = False
+    assert code.program(input) == input
+
+    input = Result.One
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = Result.Zero
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = 1
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+    input = 0
+    with pytest.raises(TypeError):
+        assert code.program(input) == input
+
+
+def test_import_supported_complex_input_and_output_types() -> None:
+    source = """
+        input complex input_var;
+        output complex output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1.0
+    output = code.program(input)
+    assert output == input
+    input = 1.0j
+    assert code.program(input) == input
+    input = 1.0 + 2.0j
+    assert code.program(input) == input
+
+
+def test_import_supported_float_input_and_output_types() -> None:
+    source = """
+        input float input_var;
+        output float output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1.0
+    assert code.program(input) == input
+    input = 0.0
+    assert code.program(input) == input
+
+    input = 1
+    code.program(input) == float(input)
+    input = 0
+    code.program(input) == float(input)
+
+
+def test_import_supported_int_input_and_output_types() -> None:
+    source = """
+        input int input_var;
+        output int output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1
+    assert code.program(input) == input
+    input = 0
+    assert code.program(input) == input
+    input = -1
+    assert code.program(input) == input
+
+
+def test_import_supported_bigint_input_and_output_types() -> None:
+    source = """
+        input int[128] input_var;
+        output int[128] output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1
+    assert code.program(input) == input
+    input = 0
+    assert code.program(input) == input
+    input = -1
+    assert code.program(input) == input
+    input = (1 << 64) - 1
+    assert code.program(input) == input
+    input = -(1 << 63)
+    assert code.program(input) == input
+    input = (1 << 128) - 1
+    assert code.program(input) == input
+    input = -(1 << 127)
+    assert code.program(input) == input
+
+
+def test_import_supported_uint_input_and_output_types() -> None:
+    source = """
+        input uint input_var;
+        output uint output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1
+    assert code.program(input) == input
+    input = 0
+    assert code.program(input) == input
+    input = -1
+    # we don't have uint, so it goes in as-is
+    assert code.program(input) == input
+
+
+def test_import_supported_biguint_input_and_output_types() -> None:
+    source = """
+        input uint[128] input_var;
+        output uint[128] output_var;
+        output_var = input_var;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    input = 1
+    assert code.program(input) == input
+    input = 0
+    assert code.program(input) == input
+    input = -1
+    assert code.program(input) == input
+    input = (1 << 64) - 1
+    assert code.program(input) == input
+    # we don't have uint, so it goes in as-is
+    input = -(1 << 63)
+    assert code.program(input) == input
+    input = (1 << 128) - 1
+    assert code.program(input) == input
+    # we don't have uint, so it goes in as-is
+    input = -(1 << 128)
+    assert code.program(input) == input
+
+
+def test_import_supported_bitarray_input_and_output_types() -> None:
+    source = """
+        input bit[5] i_0;
+        output bit[5] o_0;
+        o_0 = i_0;
+        """
+    init(target_profile=TargetProfile.Base)
+    import_openqasm(source, program_type=ProgramType.Operation, name="program")
+
+    with pytest.raises(TypeError):
+        code.program([1, 0, 1, 0])
+    with pytest.raises(TypeError):
+        code.program([True, False, True, False])
+
+    input = [Result.One, Result.Zero, Result.One, Result.Zero]
+    assert code.program(input) == input
+
+
 def test_import_can_declare_files_with_namespaces() -> None:
     init(target_profile=TargetProfile.Adaptive_RI)
     import_openqasm("output int x; x = 42;", program_type=ProgramType.File)
@@ -510,8 +709,6 @@ def test_callable_with_bool_exposed_into_env_fails_incorrect_types() -> None:
         code.Identity([4])
 
 
-# mark this test xfail until we support arrays as arguments
-@pytest.mark.xfail(reason="Arrays as arguments are not supported yet")
 def test_callable_with_array_exposed_into_env_fails_incorrect_types() -> None:
     init()
     import_openqasm(
@@ -519,18 +716,19 @@ def test_callable_with_array_exposed_into_env_fails_incorrect_types() -> None:
         program_type=ProgramType.Fragments,
     )
     assert code.fst([4, 5, 6]) == 4
+    assert code.fst((5, 6, 7)) == 5
+    with pytest.raises(QSharpError):  # out of range
+        code.fst([])
     with pytest.raises(TypeError):
-        code.Identity([])
+        code.fst((4, 5.0, 6))
     with pytest.raises(TypeError):
-        code.Identity((4, 5, 6))
+        code.fst(4)
     with pytest.raises(TypeError):
-        code.Identity(4)
+        code.fst("4")
     with pytest.raises(TypeError):
-        code.Identity("4")
+        code.fst(4.0)
     with pytest.raises(TypeError):
-        code.Identity(4.0)
-    with pytest.raises(TypeError):
-        code.Identity([1, 2, 3.0])
+        code.fst([1, 2, 3.0])
 
 
 @pytest.mark.xfail(
