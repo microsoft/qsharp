5305156cee8fcd202659c46b2a000aff5e32f883
Stefan J. Wernli <swernli@microsoft.com>
2025-09-03 14:58:48 -0700

    Update to latest simulator, new benchmark (#2690)
    This change updates to the latest sparse simulator, which includes
    performance improvements. The change also adds a new benchmark taken
    from the old qsharp-runtime repo to help with evaluating simulator
    updates on a more complex program.
    
    Here are how the execution benchmarks (including the new one) change
    with the latest simulator:
    <img width="643" height="452" alt="image"
    src="https://github.com/user-attachments/assets/18ca8a8e-d7e9-43f3-8c46-e650232bcba9"
    />
    Smaller programs like Teleport receive a minor boost, slightly bigger
    programs like Deutsch-Jozsa get a bigger boost, and the new benchmark
    shows a significant improvement. Certain programs like the Large File
    Parity sample incurr a small penalty, but the trade-offs seem well
    worthwhile.
    
    For very large programs, like the Ising samples on 25 qubits, this has
    shown significant performance boost, taking only 25% of the time after
    update.

--- source/compiler/qsc/benches/eval.rs
+++ source/compiler/qsc/benches/eval.rs
@@ -14,6 +14,7 @@ const TELEPORT: &str = include_str!("../../../../samples/algorithms/Teleportatio
 const DEUTSCHJOZSA: &str = include_str!("../../../../samples/algorithms/DeutschJozsa.qs");
 const LARGE: &str = include_str!("./large.qs");
 const ARRAY_LITERAL: &str = include_str!("./array_literal");
+const BENCH5X5: &str = include_str!("./bench5x5.qs");
 
 pub fn teleport(c: &mut Criterion) {
     c.bench_function("Teleport evaluation", |b| {
@@ -60,6 +61,28 @@ pub fn deutsch_jozsa(c: &mut Criterion) {
 pub fn large_file(c: &mut Criterion) {
     c.bench_function("Large file parity evaluation", |b| {
         let sources = SourceMap::new([("large.qs".into(), LARGE.into())], None);
+        let (std_id, store) = qsc::compile::package_store_with_stdlib(TargetCapabilityFlags::all());
+        let mut evaluator = Interpreter::new(
+            sources,
+            PackageType::Exe,
+            TargetCapabilityFlags::all(),
+            LanguageFeatures::default(),
+            store,
+            &[(std_id, None)],
+        )
+        .expect("code should compile");
+
+        b.iter(move || {
+            let mut out = Vec::new();
+            let mut rec = GenericReceiver::new(&mut out);
+            assert!(evaluator.eval_entry(&mut rec).is_ok());
+        });
+    });
+}
+
+pub fn bench5x5(c: &mut Criterion) {
+    c.bench_function("Bench 5x5 evaluation", |b| {
+        let sources = SourceMap::new([("bench5x5.qs".into(), BENCH5X5.into())], None);
         let (std_id, store) = qsc::compile::package_store_with_stdlib(TargetCapabilityFlags::all());
         let mut evaluator = Interpreter::new(
             sources,
@@ -213,6 +236,7 @@ criterion_group!(
     teleport,
     deutsch_jozsa,
     large_file,
+    bench5x5,
     array_append,
     array_update,
     array_literal,
